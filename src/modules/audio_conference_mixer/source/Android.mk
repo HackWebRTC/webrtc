694019b9aac4226b759140fc12df32bf98272ee3
leozwang@google.com <leozwang@google.com@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-07-26 05:04:12 +0000

    Clean up ANDROID macro definitions [audio_conf] Review URL: http://webrtc-codereview.appspot.com/94002
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@255 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/modules/audio_conference_mixer/source/Android.mk
+++ src/modules/audio_conference_mixer/source/Android.mk
@@ -8,43 +8,28 @@ LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 LOCAL_MODULE := libwebrtc_audio_conference_mixer
 LOCAL_MODULE_TAGS := optional
 LOCAL_CPP_EXTENSION := .cc
-LOCAL_GENERATED_SOURCES :=
-LOCAL_SRC_FILES := audio_frame_manipulator.cc \
+LOCAL_SRC_FILES := \
+    audio_frame_manipulator.cc \
     level_indicator.cc \
     audio_conference_mixer_impl.cc \
     time_scheduler.cc
 
 # Flags passed to both C and C++ files.
-MY_CFLAGS :=  
-MY_CFLAGS_C :=
-MY_DEFS := '-DNO_TCMALLOC' \
-    '-DNO_HEAPCHECKER' \
-    '-DWEBRTC_TARGET_PC' \
-    '-DWEBRTC_LINUX' \
-    '-DWEBRTC_THREAD_RR' \
-    '-DWEBRTC_ANDROID' \
-    '-DANDROID' 
-LOCAL_CFLAGS := $(MY_CFLAGS_C) $(MY_CFLAGS) $(MY_DEFS)
+LOCAL_CFLAGS := \
+    $(MY_WEBRTC_COMMON_DEFS)
 
-# Include paths placed before CFLAGS/CPPFLAGS
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../.. \
+LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../interface \
     $(LOCAL_PATH)/../../interface \
+    $(LOCAL_PATH)/../../.. \
     $(LOCAL_PATH)/../../../system_wrappers/interface 
 
-# Flags passed to only C++ (and not C) files.
-LOCAL_CPPFLAGS := 
-
-LOCAL_LDFLAGS :=
-
-LOCAL_STATIC_LIBRARIES :=
-# Duplicate the static libraries to fix circular references
-LOCAL_STATIC_LIBRARIES += $(LOCAL_STATIC_LIBRARIES)
-
-LOCAL_SHARED_LIBRARIES := libcutils \
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
     libdl \
     libstlport
-LOCAL_ADDITIONAL_DEPENDENCIES :=
 
+ifndef NDK_ROOT
 include external/stlport/libstlport.mk
+endif
 include $(BUILD_STATIC_LIBRARY)
