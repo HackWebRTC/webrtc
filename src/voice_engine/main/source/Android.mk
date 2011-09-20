ec5e87614ec9c5cb56fbb64ece8961a67d22a1b1
leozwang@google.com <leozwang@google.com@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-09-20 16:41:09 +0000

    Enable OPENELSE defination when compile voice engine Review URL: http://webrtc-codereview.appspot.com/150005
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@629 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/voice_engine/main/source/Android.mk
+++ src/voice_engine/main/source/Android.mk
@@ -49,7 +49,8 @@ LOCAL_SRC_FILES := \
 
 # Flags passed to both C and C++ files.
 LOCAL_CFLAGS := \
-    $(MY_WEBRTC_COMMON_DEFS)
+    $(MY_WEBRTC_COMMON_DEFS) \
+   '-DWEBRTC_ANDROID_OPENSLES'
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../interface \
