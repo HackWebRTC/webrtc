49d025f2625056494cc545f730c3360585679554
xians@google.com <xians@google.com@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-09-27 14:43:06 +0000

    Get the right guid str for GetRecordingDeviceName
    Bug=http://code.google.com/p/webrtc/issues/detail?id=99
    Test=none
    Review URL: http://webrtc-codereview.appspot.com/183002
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@652 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/voice_engine/main/source/voe_hardware_impl.cc
+++ src/voice_engine/main/source/voe_hardware_impl.cc
@@ -271,7 +271,7 @@ int VoEHardwareImpl::GetRecordingDeviceName(int index,
 
     if (strGuidUTF8 != NULL)
     {
-        strncpy(strGuidUTF8, name, strLen);
+        strncpy(strGuidUTF8, guid, strLen);
         WEBRTC_TRACE(kTraceStateInfo, kTraceVoice, VoEId(_instanceId, -1),
                      "  Output: strGuidUTF8=%s", strGuidUTF8);
     }
