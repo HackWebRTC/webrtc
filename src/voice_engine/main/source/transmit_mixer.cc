e33a102eee68b2163d21548551e876fc7525684e
niklas.enbom@webrtc.org <niklas.enbom@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-11-16 10:33:53 +0000

    Resubmitting http://webrtc-codereview.appspot.com/269007/ Review URL: http://webrtc-codereview.appspot.com/268012
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@955 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/voice_engine/main/source/transmit_mixer.cc
+++ src/voice_engine/main/source/transmit_mixer.cc
@@ -1155,9 +1155,13 @@ TransmitMixer::GenerateAudioFrame(const WebRtc_Word16 audioSamples[],
                  "samplesPerSec=%u, mixingFrequency=%u)",
                  nSamples, samplesPerSec, mixingFrequency);
 
+    ResamplerType resampType = (nChannels == 1) ? 
+            kResamplerSynchronous : kResamplerSynchronousStereo;
+    
+
     if (_audioResampler.ResetIfNeeded(samplesPerSec,
                                         mixingFrequency,
-                                        kResamplerSynchronous) != 0)
+                                        resampType) != 0)
     {
         WEBRTC_TRACE(kTraceError, kTraceVoice, VoEId(_instanceId, -1),
                      "TransmitMixer::GenerateAudioFrame() unable to resample");
@@ -1165,7 +1169,7 @@ TransmitMixer::GenerateAudioFrame(const WebRtc_Word16 audioSamples[],
     }
     if (_audioResampler.Push(
         (WebRtc_Word16*) audioSamples,
-        nSamples,
+        nSamples * nChannels,
         _audioFrame._payloadData,
         AudioFrame::kMaxAudioFrameSizeSamples,
         (int&) _audioFrame._payloadDataLengthInSamples) == -1)
@@ -1175,6 +1179,7 @@ TransmitMixer::GenerateAudioFrame(const WebRtc_Word16 audioSamples[],
         return -1;
     }
 
+    _audioFrame._payloadDataLengthInSamples /= nChannels;
     _audioFrame._id = _instanceId;
     _audioFrame._timeStamp = -1;
     _audioFrame._frequencyInHz = mixingFrequency;
