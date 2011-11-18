543611a77a53e97df8495c2c3ac9a3a7d5b63520
kjellander@webrtc.org <kjellander@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-11-18 13:25:13 +0000

    Reverting r972 due to compilation error on Windows Release build.
    TBR=kma
    Review URL: http://webrtc-codereview.appspot.com/282003
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@976 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/modules/audio_coding/codecs/iSAC/fix/source/filters.c
+++ src/modules/audio_coding/codecs/iSAC/fix/source/filters.c
@@ -22,44 +22,50 @@
 #include "lpc_masking_model.h"
 #include "codec.h"
 
-#if !(defined(WEBRTC_ANDROID) && defined(WEBRTC_ARCH_ARM_NEON))
-// Autocorrelation function in fixed point.
-// NOTE! Different from SPLIB-version in how it scales the signal.
+
+/* Autocorrelation function in fixed point. NOTE! Different from SPLIB-version in how it scales the signal. */
 int WebRtcIsacfix_AutocorrFix(
-    WebRtc_Word32* __restrict__ r,
-    const WebRtc_Word16* __restrict__ x,
-    WebRtc_Word16 N,
-    WebRtc_Word16 order,
-    WebRtc_Word16* __restrict__ scale) {
+    WebRtc_Word32          *r,
+    const WebRtc_Word16 *x,
+    WebRtc_Word16          N,
+    WebRtc_Word16          order,
+    WebRtc_Word16          *scale)
+{
+  int  j, i;
+  WebRtc_Word16  scaling;
+  WebRtc_Word32 sum, prod, newsum;
+  G_CONST WebRtc_Word16    *xptr1;
+  G_CONST WebRtc_Word16    *xptr2;
 
-  int i = 0;
-  int j = 0;
-  int16_t scaling = 0;
-  int32_t sum = 0;
-  uint32_t temp = 0;
-  int64_t prod = 0;
-
-  // Calculate r[0].
-  for (i = 0; i < N; i++) {
-    prod += WEBRTC_SPL_MUL_16_16(x[i], x[i]);
-  }
-
-  // Calculate scaling (the value of shifting).
-  temp = (uint32_t)(prod >> 31);
-  if(temp == 0) {
-    scaling = 0;
-  } else {
-    scaling = 32 - WebRtcSpl_NormU32(temp);
-  }
-  r[0] = (int32_t)(prod >> scaling);
-
-  // Perform the actual correlation calculation.
-  for (i = 1; i < order + 1; i++) {
-    prod = 0;
-    for (j = 0; j < N - i; j++) {
-      prod += WEBRTC_SPL_MUL_16_16(x[j], x[i + j]);
+  sum=0;
+  scaling=0;
+  /* Calculate r[0] and how much scaling is needed */
+  for (i=0; i < N; i++) {
+    prod = WEBRTC_SPL_MUL_16_16_RSFT(x[i],x[i],scaling);
+    newsum = sum+prod;
+    /* If sum gets less than 0 we have overflow and need to scale the signal */
+    if(newsum<0) {
+      scaling++;
+      sum=WEBRTC_SPL_RSHIFT_W32(sum, 1);
+      prod=WEBRTC_SPL_RSHIFT_W32(prod, 1);
     }
-    sum = (int32_t)(prod >> scaling);
+    sum += prod;
+  }
+  r[0]=sum;
+
+  /* Perform the actual correlation calculation */
+  for (i = 1; i < order + 1; i++)
+  {
+    int loops=(N-i);
+    sum = 0;
+    xptr1=(G_CONST WebRtc_Word16 *)x;
+    xptr2=(G_CONST WebRtc_Word16 *)&x[i];
+
+    for (j = loops;j > 0; j--)
+    {
+      sum += WEBRTC_SPL_MUL_16_16_RSFT(*xptr1++,*xptr2++,scaling);
+    }
+
     r[i] = sum;
   }
 
@@ -67,7 +73,6 @@ int WebRtcIsacfix_AutocorrFix(
 
   return(order + 1);
 }
-#endif // !(defined(WEBRTC_ANDROID) && defined(WEBRTC_ARCH_ARM_NEON))
 
 static const WebRtc_Word32 kApUpperQ15[ALLPASSSECTIONS] = { 1137, 12537 };
 static const WebRtc_Word32 kApLowerQ15[ALLPASSSECTIONS] = { 5059, 24379 };
