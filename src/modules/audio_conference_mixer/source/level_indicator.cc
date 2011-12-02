d3eac4158c5f303f0f3e0254233ac3b7c39cdfb4
perkj@webrtc.org <perkj@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-12-02 09:34:01 +0000

    Fixed webrtc::perm variable.
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@1085 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/modules/audio_conference_mixer/source/level_indicator.cc
+++ src/modules/audio_conference_mixer/source/level_indicator.cc
@@ -12,7 +12,7 @@
 
 namespace webrtc {
 // Array for adding smothing to level changes (ad-hoc).
-WebRtc_UWord32 perm[] =
+const WebRtc_UWord32 perm[] =
     {0,1,2,3,4,4,5,5,5,5,6,6,6,6,6,7,7,7,7,8,8,8,9,9,9,9,9,9,9,9,9,9,9};
 
 LevelIndicator::LevelIndicator()
