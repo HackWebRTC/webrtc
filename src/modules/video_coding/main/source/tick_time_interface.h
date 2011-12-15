0a10e3c4b2c29e52e6fed063cf60939c953cce47
henrik.lundin@webrtc.org <henrik.lundin@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-12-15 16:08:36 +0000

    Fix order of include and guard in tick_time_interface.h
    Review URL: http://webrtc-codereview.appspot.com/331002
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@1207 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/modules/video_coding/main/source/tick_time_interface.h
+++ src/modules/video_coding/main/source/tick_time_interface.h
@@ -8,10 +8,10 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "system_wrappers/interface/tick_util.h"
-
 #ifndef WEBRTC_MODULES_VIDEO_CODING_MAIN_SOURCE_TICK_TIME_WRAPPER_H_
 #define WEBRTC_MODULES_VIDEO_CODING_MAIN_SOURCE_TICK_TIME_WRAPPER_H_
+
+#include "system_wrappers/interface/tick_util.h"
 
 namespace webrtc {
 
