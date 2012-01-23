2442de1351a8d9bee0912a09317122bb5bdb08bc
andrew@webrtc.org <andrew@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-01-23 17:45:41 +0000

    Clean up PRESUBMIT.py, and enable license check.
    Fix the license header based on the example in:
    http://src.chromium.org/viewvc/chrome/trunk/tools/depot_tools/presubmit_canned_checks.py?revision=107590&view=markup
    
    BUG=None
    TEST=Run presubmit checks on a dummy CL with Python and C code.
    
    Review URL: https://webrtc-codereview.appspot.com/369002
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@1511 4adac7df-926f-26a2-2b94-8c16560cd09d

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1,35 +1,38 @@
-
-	
-
-
-webrtc_license_header = (
-      r'.*? Copyright \(c\) 2012 The WebRTC project authors'
-      r'.*?Use of this source code is governed by a BSD-style license\n'
-      r'.*? that can be found in the LICENSE file in the root of the source\n'
-      r'.*? tree. An additional intellectual property rights grant can be found\n'
-r'.*? in the file PATENTS.  All contributing project authors may\n'
-r'.*? be found in the AUTHORS file in the root of the source tree\n'
-  ) 
-
+# Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
+#
+# Use of this source code is governed by a BSD-style license
+# that can be found in the LICENSE file in the root of the source
+# tree. An additional intellectual property rights grant can be found
+# in the file PATENTS.  All contributing project authors may
+# be found in the AUTHORS file in the root of the source tree.
 
 def CheckChangeOnUpload(input_api, output_api):
+  webrtc_license_header = (
+      r'.*? Copyright \(c\) %(year)s The WebRTC project authors\. '
+        r'All Rights Reserved\.\n'
+      r'.*?\n'
+      r'.*? Use of this source code is governed by a BSD-style license\n'
+      r'.*? that can be found in the LICENSE file in the root of the source\n'
+      r'.*? tree\. An additional intellectual property rights grant can be '
+        r'found\n'
+      r'.*? in the file PATENTS\.  All contributing project authors may\n'
+      r'.*? be found in the AUTHORS file in the root of the source tree\.\n'
+  ) % {
+      'year': input_api.time.strftime('%Y'),
+  }
+
   results = []
-  results.extend(input_api.canned_checks.CheckLongLines(input_api, output_api,maxlen=95))
-  results.extend(input_api.canned_checks.CheckChangeHasNoTabs(input_api, output_api))
+  # Ideally, maxlen would be 80.
+  results.extend(input_api.canned_checks.CheckLongLines(
+      input_api, output_api, maxlen=95))
+  results.extend(input_api.canned_checks.CheckChangeHasNoTabs(
+      input_api, output_api))
+  results.extend(input_api.canned_checks.CheckLicense(
+      input_api, output_api, webrtc_license_header))
+
   return results
 
-
- #results.extend(CheckChangeLintsClean(input_api, output_api))
- #results.extend(input_api.canned_checks.CheckLicense(input_api, output_api, license_re=webrtc_license_header))
-  
-
-
-def CheckChangeOnCommit (input_api, output_api):
+def CheckChangeOnCommit(input_api, output_api):
   results = []
   results.extend(input_api.canned_checks.CheckOwners(input_api, output_api))
   return results
-
-
-
-
-#sources = lambda x: input_api.FilterSourceFile(x, black_list=black_list)
