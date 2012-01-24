2fc722cedab2ed4bf10cd3257632a3bf240f0592
leozwang@webrtc.org <leozwang@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-01-24 17:46:04 +0000

    Enable building library and test app Review URL: https://webrtc-codereview.appspot.com/369007
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@1537 4adac7df-926f-26a2-2b94-8c16560cd09d

--- Android.mk
+++ Android.mk
@@ -38,14 +38,14 @@ include $(MY_WEBRTC_ROOT_PATH)/src/voice_engine/main/source/Android.mk
 
 # video
 include $(MY_WEBRTC_ROOT_PATH)/src/common_video/jpeg/main/source/Android.mk
-#include $(MY_WEBRTC_ROOT_PATH)/src/common_video/libyuv/Android.mk
+include $(MY_WEBRTC_ROOT_PATH)/src/common_video/libyuv/Android.mk
 include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_capture/main/source/Android.mk
 include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_coding/codecs/i420/main/source/Android.mk
-#include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_coding/codecs/vp8/main/source/Android.mk
+include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_coding/codecs/vp8/main/source/Android.mk
 include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_coding/main/source/Android.mk
 include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_processing/main/source/Android.mk
 include $(MY_WEBRTC_ROOT_PATH)/src/modules/video_render/main/source/Android.mk
-#include $(MY_WEBRTC_ROOT_PATH)/src/video_engine/Android.mk
+include $(MY_WEBRTC_ROOT_PATH)/src/video_engine/Android.mk
 
 # third party
 include $(MY_WEBRTC_ROOT_PATH)/libvpx.mk
@@ -156,12 +156,12 @@ LOCAL_PRELINK_MODULE := false
 ifndef NDK_ROOT
 include external/stlport/libstlport.mk
 endif
-#include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 
 # test apps, they're for test only; all these test apps have LOCAL_MODULE_TAGS:=tests
 # voice engine test apps
-#include $(MY_WEBRTC_ROOT_PATH)/src/voice_engine/main/test/cmd_test/Android.mk
+include $(MY_WEBRTC_ROOT_PATH)/src/voice_engine/main/test/cmd_test/Android.mk
 #include $(MY_WEBRTC_ROOT_PATH)/src/voice_engine/main/test/auto_test/Android.mk
 # video engien test apps
-#include $(MY_WEBRTC_ROOT_PATH)/src/video_engine/main/test/android_test/Android.mk
+include $(MY_WEBRTC_ROOT_PATH)/src/video_engine/main/test/android_test/Android.mk
 #include $(MY_WEBRTC_ROOT_PATH)/src/video_engine/test/auto_test/android/Android.mk
