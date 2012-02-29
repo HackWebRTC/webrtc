a368f86ff6a4e59001a12e5f9b3dccae8619335f
leozwang@webrtc.org <leozwang@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-02-29 18:53:10 +0000

    Add libvpx configuration files for arm Review URL: https://webrtc-codereview.appspot.com/416003
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@1801 4adac7df-926f-26a2-2b94-8c16560cd09d

--- third_party/libvpx/source/config/linux/arm/vpx_config.c
+++ third_party/libvpx/source/config/linux/arm/vpx_config.c
@@ -1,2 +1,9 @@
-static const char* const cfg = "--target=armv6-linux-gcc --enable-pic --enable-error-concealment --disable-install-docs --disable-install-bins --disable-install-libs --enable-realtime-only --disable-install-srcs --disable-examples";
+/* Copyright (c) 2011 The WebM project authors. All Rights Reserved. */
+/*  */
+/* Use of this source code is governed by a BSD-style license */
+/* that can be found in the LICENSE file in the root of the source */
+/* tree. An additional intellectual property rights grant can be found */
+/* in the file PATENTS.  All contributing project authors may */
+/* be found in the AUTHORS file in the root of the source tree. */
+static const char* const cfg = "--sdk-path=/usr/local/google/bin/android-ndk-r7 --target=armv5te-android-gcc --enable-pic --enable-error-concealment --disable-install-docs --disable-install-srcs --disable-examples --disable-internal-stats --disable-install-libs --disable-install-bins --enable-realtime-only";
 const char *vpx_codec_build_config(void) {return cfg;}
