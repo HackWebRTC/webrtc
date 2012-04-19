3d48b09bb47b322fa3f081df69ec754957c6b6e0
tommi@webrtc.org <tommi@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-04-19 15:07:11 +0000

    Fix android build.
    TBR=kjellander
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@2067 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/system_wrappers/source/Android.mk
+++ src/system_wrappers/source/Android.mk
@@ -20,7 +20,7 @@ LOCAL_SRC_FILES := \
     map.cc \
     sort.cc \
     aligned_malloc.cc \
-    atomic32.cc \
+    atomic32_linux.cc \
     condition_variable.cc \
     cpu_no_op.cc \
     cpu_features.cc \
