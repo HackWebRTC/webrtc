03b2b85b19af8599f0c4bb9b3d736c52db017cbe
stefan@webrtc.org <stefan@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-09-03 06:57:01 +0000

    Enable multi-res encoding feature in libvpx.
    BUG=
    
    Review URL: https://webrtc-codereview.appspot.com/761005
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@2697 4adac7df-926f-26a2-2b94-8c16560cd09d

--- third_party/libvpx/source/config/win/x64/vpx_config.c
+++ third_party/libvpx/source/config/win/x64/vpx_config.c
@@ -5,5 +5,5 @@
 /* tree. An additional intellectual property rights grant can be found */
 /* in the file PATENTS.  All contributing project authors may */
 /* be found in the AUTHORS file in the root of the source tree. */
-static const char* const cfg = "--target=x86_64-win64-vs9 --enable-pic --enable-error-concealment --disable-install-docs --disable-install-srcs --disable-examples --disable-internal-stats --disable-install-libs --disable-install-bins --enable-realtime-only --enable-static-msvcrt --as=yasm";
+static const char* const cfg = "--target=x86_64-win64-vs9 --enable-pic --enable-error-concealment --disable-install-docs --disable-install-srcs --disable-examples --disable-internal-stats --disable-install-libs --disable-install-bins --enable-realtime-only --enable-static-msvcrt --as=yasm --enable-multi-res-encoding";
 const char *vpx_codec_build_config(void) {return cfg;}
