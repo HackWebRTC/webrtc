4a2639a210957bdd027c73370cc0460e5afbb3b9
phoglund@webrtc.org <phoglund@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-09-10 13:28:39 +0000

    Disabled one more test on Linux that was probably flaky. 42 remaining.
    BUG=
    
    Review URL: https://webrtc-codereview.appspot.com/786004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@2727 4adac7df-926f-26a2-2b94-8c16560cd09d

--- src/modules/audio_device/main/test/audio_device_test_api.cc
+++ src/modules/audio_device/main/test/audio_device_test_api.cc
@@ -1688,7 +1688,8 @@ TEST_F(AudioDeviceAPITest, CPULoad) {
 }
 
 // TODO(kjellander): Fix flakiness causing failures on Windows.
-#if !defined(_WIN32)
+// TODO(phoglund):  Fix flakiness causing failures on Linux.
+#if !defined(_WIN32) && !defined(WEBRTC_LINUX)
 TEST_F(AudioDeviceAPITest, StartAndStopRawOutputFileRecording) {
   // NOTE: this API is better tested in a functional test
   CheckInitialPlayoutStates();
@@ -1724,8 +1725,6 @@ TEST_F(AudioDeviceAPITest, StartAndStopRawOutputFileRecording) {
   // - size of raw_output_playing.pcm shall be > 0
 }
 
-// TODO(phoglund): The following test is flaky on Linux.
-#if !defined(WEBRTC_LINUX)
 TEST_F(AudioDeviceAPITest, StartAndStopRawInputFileRecording) {
   // NOTE: this API is better tested in a functional test
   CheckInitialRecordingStates();
@@ -1759,8 +1758,7 @@ TEST_F(AudioDeviceAPITest, StartAndStopRawInputFileRecording) {
   // - size of raw_input_not_recording.pcm shall be 0
   // - size of raw_input_not_recording.pcm shall be > 0
 }
-#endif  // !WEBRTC_LINUX
-#endif  // !WIN32
+#endif  // !WIN32 && !WEBRTC_LINUX
 
 TEST_F(AudioDeviceAPITest, RecordingSampleRate) {
   WebRtc_UWord32 sampleRate(0);
