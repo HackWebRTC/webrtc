28afee04ae74e708168cfe974f8239e932b400c3
fischman@webrtc.org <fischman@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-11-29 01:09:44 +0000

    Enable building WebRTCDemo apk using Release webrtc libs.
    Now passing BUILDTYPE=Release to both the make that builds the libs and the
    ndk-build that builds the app makes the app use non-Debug libs.
    
    Review URL: https://webrtc-codereview.appspot.com/972007
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@3190 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/video_engine/test/android/jni/Android.mk
+++ webrtc/video_engine/test/android/jni/Android.mk
@@ -10,7 +10,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
 
-MY_LIBS_PATH := ../../../../../out/Debug/obj.target
+MY_LIBS_PATH := ../../../../../out/$(BUILDTYPE)/obj.target
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvoice_engine_core
