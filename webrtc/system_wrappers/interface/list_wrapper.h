52d981f60c64b155982c1f9d1d0d42f0cb5908f3
phoglund@webrtc.org <phoglund@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-12-14 09:52:34 +0000

    Reformatted list classes.
    BUG=
    TEST=Trybots
    
    Review URL: https://webrtc-codereview.appspot.com/995004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@3291 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/system_wrappers/interface/list_wrapper.h
+++ webrtc/system_wrappers/interface/list_wrapper.h
@@ -11,97 +11,97 @@
 #ifndef WEBRTC_SYSTEM_WRAPPERS_INTERFACE_LIST_WRAPPER_H_
 #define WEBRTC_SYSTEM_WRAPPERS_INTERFACE_LIST_WRAPPER_H_
 
-#include "constructor_magic.h"
+#include "webrtc/system_wrappers/interface/constructor_magic.h"
 
 namespace webrtc {
+
 class CriticalSectionWrapper;
 
-class ListItem
-{
-friend class ListWrapper;
+class ListItem {
+  friend class ListWrapper;
 
-public:
-    ListItem(const void* ptr);
-    ListItem(const unsigned int item);
-    virtual ~ListItem();
-    void* GetItem() const;
-    unsigned int GetUnsignedItem() const;
+ public:
+  ListItem(const void* ptr);
+  ListItem(const unsigned int item);
+  virtual ~ListItem();
+  void* GetItem() const;
+  unsigned int GetUnsignedItem() const;
 
-protected:
-    ListItem* next_;
-    ListItem* prev_;
+ protected:
+  ListItem* next_;
+  ListItem* prev_;
 
-private:
-    const void*         item_ptr_;
-    const unsigned int  item_;
+ private:
+  const void*         item_ptr_;
+  const unsigned int  item_;
 };
 
-class ListWrapper
-{
-public:
-    ListWrapper();
-    virtual ~ListWrapper();
+class ListWrapper {
+ public:
+  ListWrapper();
+  virtual ~ListWrapper();
 
-    // Returns the number of elements stored in the list.
-    unsigned int GetSize() const;
+  // Returns the number of elements stored in the list.
+  unsigned int GetSize() const;
 
-    // Puts a pointer to anything last in the list.
-    int PushBack(const void* ptr);
-    // Puts a pointer to anything first in the list.
-    int PushFront(const void* ptr);
+  // Puts a pointer to anything last in the list.
+  int PushBack(const void* ptr);
+  // Puts a pointer to anything first in the list.
+  int PushFront(const void* ptr);
 
-    // Puts a copy of the specified integer last in the list.
-    int PushBack(const unsigned int item_id);
-    // Puts a copy of the specified integer first in the list.
-    int PushFront(const unsigned int item_id);
+  // Puts a copy of the specified integer last in the list.
+  int PushBack(const unsigned int item_id);
+  // Puts a copy of the specified integer first in the list.
+  int PushFront(const unsigned int item_id);
 
-    // Pops the first ListItem from the list
-    int PopFront();
+  // Pops the first ListItem from the list
+  int PopFront();
 
-    // Pops the last ListItem from the list
-    int PopBack();
+  // Pops the last ListItem from the list
+  int PopBack();
 
-    // Returns true if the list is empty
-    bool Empty() const;
+  // Returns true if the list is empty
+  bool Empty() const;
 
-    // Returns a pointer to the first ListItem in the list.
-    ListItem* First() const;
+  // Returns a pointer to the first ListItem in the list.
+  ListItem* First() const;
 
-    // Returns a pointer to the last ListItem in the list.
-    ListItem* Last() const;
+  // Returns a pointer to the last ListItem in the list.
+  ListItem* Last() const;
 
-    // Returns a pointer to the ListItem stored after item in the list.
-    ListItem* Next(ListItem* item) const;
+  // Returns a pointer to the ListItem stored after item in the list.
+  ListItem* Next(ListItem* item) const;
 
-    // Returns a pointer to the ListItem stored before item in the list.
-    ListItem* Previous(ListItem* item) const;
+  // Returns a pointer to the ListItem stored before item in the list.
+  ListItem* Previous(ListItem* item) const;
 
-    // Removes item from the list.
-    int Erase(ListItem* item);
+  // Removes item from the list.
+  int Erase(ListItem* item);
 
-    // Insert list item after existing_previous_item. Please note that new_item
-    // must be created using new ListItem(). The map will take ownership of
-    // new_item following a successfull insert. If insert fails new_item will
-    // not be released by the List
-    int Insert(ListItem* existing_previous_item,
-               ListItem* new_item);
+  // Insert list item after existing_previous_item. Please note that new_item
+  // must be created using new ListItem(). The map will take ownership of
+  // new_item following a successfull insert. If insert fails new_item will
+  // not be released by the List
+  int Insert(ListItem* existing_previous_item,
+             ListItem* new_item);
 
-    // Insert list item before existing_next_item. Please note that new_item
-    // must be created using new ListItem(). The map will take ownership of
-    // new_item following a successfull insert. If insert fails new_item will
-    // not be released by the List
-    int InsertBefore(ListItem* existing_next_item,
-                     ListItem* new_item);
+  // Insert list item before existing_next_item. Please note that new_item
+  // must be created using new ListItem(). The map will take ownership of
+  // new_item following a successfull insert. If insert fails new_item will
+  // not be released by the List
+  int InsertBefore(ListItem* existing_next_item,
+                   ListItem* new_item);
 
-private:
-    void PushBackImpl(ListItem* item);
-    void PushFrontImpl(ListItem* item);
+ private:
+  void PushBackImpl(ListItem* item);
+  void PushFrontImpl(ListItem* item);
 
-    CriticalSectionWrapper* critical_section_;
-    ListItem* first_;
-    ListItem* last_;
-    unsigned int size_;
+  CriticalSectionWrapper* critical_section_;
+  ListItem* first_;
+  ListItem* last_;
+  unsigned int size_;
 };
-} //namespace webrtc
 
-#endif // WEBRTC_SYSTEM_WRAPPERS_INTERFACE_LIST_WRAPPER_H_
+}  // namespace webrtc
+
+#endif  // WEBRTC_SYSTEM_WRAPPERS_INTERFACE_LIST_WRAPPER_H_
--- webrtc/system_wrappers/source/list_stl.h
+++ webrtc/system_wrappers/source/list_stl.h
@@ -13,54 +13,53 @@
 
 #include <list>
 
-#include "constructor_magic.h"
+#include "webrtc/system_wrappers/interface/constructor_magic.h"
 
 namespace webrtc {
-class ListItem
-{
-friend class ListWrapper;
 
-public:
-    ListItem(const void* ptr);
-    ListItem(const unsigned int item);
-    virtual ~ListItem();
-    void* GetItem() const;
-    unsigned int GetUnsignedItem() const;
+class ListItem {
+ public:
+  ListItem(const void* ptr);
+  ListItem(const unsigned int item);
+  virtual ~ListItem();
+  void* GetItem() const;
+  unsigned int GetUnsignedItem() const;
 
-private:
-    mutable std::list<ListItem*>::iterator this_iter_;
-    const void*         item_ptr_;
-    const unsigned int  item_;
-    DISALLOW_COPY_AND_ASSIGN(ListItem);
+ private:
+  friend class ListWrapper;
+  mutable std::list<ListItem*>::iterator this_iter_;
+  const void*         item_ptr_;
+  const unsigned int  item_;
+  DISALLOW_COPY_AND_ASSIGN(ListItem);
 };
 
-class ListWrapper
-{
-public:
-    ListWrapper();
-    ~ListWrapper();
+class ListWrapper {
+ public:
+  ListWrapper();
+  ~ListWrapper();
 
-    // ListWrapper functions
-    unsigned int GetSize() const;
-    int PushBack(const void* ptr);
-    int PushBack(const unsigned int item_id);
-    int PushFront(const void* ptr);
-    int PushFront(const unsigned int item_id);
-    int PopFront();
-    int PopBack();
-    bool Empty() const;
-    ListItem* First() const;
-    ListItem* Last() const;
-    ListItem* Next(ListItem* item) const;
-    ListItem* Previous(ListItem* item) const;
-    int Erase(ListItem* item);
-    int Insert(ListItem* existing_previous_item, ListItem* new_item);
-    int InsertBefore(ListItem* existing_next_item, ListItem* new_item);
+  // ListWrapper functions
+  unsigned int GetSize() const;
+  int PushBack(const void* ptr);
+  int PushBack(const unsigned int item_id);
+  int PushFront(const void* ptr);
+  int PushFront(const unsigned int item_id);
+  int PopFront();
+  int PopBack();
+  bool Empty() const;
+  ListItem* First() const;
+  ListItem* Last() const;
+  ListItem* Next(ListItem* item) const;
+  ListItem* Previous(ListItem* item) const;
+  int Erase(ListItem* item);
+  int Insert(ListItem* existing_previous_item, ListItem* new_item);
+  int InsertBefore(ListItem* existing_next_item, ListItem* new_item);
 
-private:
-    mutable std::list<ListItem*> list_;
-    DISALLOW_COPY_AND_ASSIGN(ListWrapper);
+ private:
+  mutable std::list<ListItem*> list_;
+  DISALLOW_COPY_AND_ASSIGN(ListWrapper);
 };
-} // namespace webrtc
 
-#endif // WEBRTC_SYSTEM_WRAPPERS_SOURCE_LIST_STL_H_
+}  // namespace webrtc
+
+#endif  // WEBRTC_SYSTEM_WRAPPERS_SOURCE_LIST_STL_H_
