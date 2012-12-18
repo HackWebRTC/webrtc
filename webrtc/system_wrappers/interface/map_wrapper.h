6e0ce73741aaa8db739c5b49849b5d5974fa3900
phoglund@webrtc.org <phoglund@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2012-12-18 17:18:35 +0000

    Reformatted map classes.
    BUG=
    TEST=Trybots.
    
    Review URL: https://webrtc-codereview.appspot.com/1006004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@3308 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/system_wrappers/interface/map_wrapper.h
+++ webrtc/system_wrappers/interface/map_wrapper.h
@@ -13,63 +13,62 @@
 
 #include <map>
 
-#include "constructor_magic.h"
+#include "webrtc/system_wrappers/interface/constructor_magic.h"
 
 namespace webrtc {
-class MapItem
-{
-friend class MapWrapper;
+class MapItem {
+  friend class MapWrapper;
 
-public:
-    MapItem(int id, void* ptr);
-    virtual ~MapItem();
-    void* GetItem();
-    int GetId();
-    unsigned int GetUnsignedId();
-    void SetItem(void* ptr);
+ public:
+  MapItem(int id, void* ptr);
+  virtual ~MapItem();
+  void* GetItem();
+  int GetId();
+  unsigned int GetUnsignedId();
+  void SetItem(void* ptr);
 
-private:
-    int   item_id_;
-    void* item_pointer_;
+ private:
+  int   item_id_;
+  void* item_pointer_;
 };
 
-class MapWrapper
-{
-public:
-    MapWrapper();
-    ~MapWrapper();
+class MapWrapper {
+ public:
+  MapWrapper();
+  ~MapWrapper();
 
-    // Puts a pointer to anything in the map and associates it with id. Note, id
-    // needs to be unique for all items in the map.
-    int Insert(int id, void* ptr);
+  // Puts a pointer to anything in the map and associates it with id. Note, id
+  // needs to be unique for all items in the map.
+  int Insert(int id, void* ptr);
 
-    // Removes item from map.
-    int Erase(MapItem* item);
+  // Removes item from map.
+  int Erase(MapItem* item);
 
-    // Finds item with associated with id and removes it from the map.
-    int Erase(int id);
+  // Finds item with associated with id and removes it from the map.
+  int Erase(int id);
 
-    // Returns the number of elements stored in the map.
-    int Size() const;
+  // Returns the number of elements stored in the map.
+  int Size() const;
 
-    // Returns a pointer to the first MapItem in the map.
-    MapItem* First() const;
+  // Returns a pointer to the first MapItem in the map.
+  MapItem* First() const;
 
-    // Returns a pointer to the last MapItem in the map.
-    MapItem* Last() const;
+  // Returns a pointer to the last MapItem in the map.
+  MapItem* Last() const;
 
-    // Returns a pointer to the MapItem stored after item in the map.
-    MapItem* Next(MapItem* item) const;
+  // Returns a pointer to the MapItem stored after item in the map.
+  MapItem* Next(MapItem* item) const;
 
-    // Returns a pointer to the MapItem stored before item in the map.
-    MapItem* Previous(MapItem* item) const;
+  // Returns a pointer to the MapItem stored before item in the map.
+  MapItem* Previous(MapItem* item) const;
 
-    // Returns a pointer to the MapItem associated with id from the map.
-    MapItem* Find(int id) const;
+  // Returns a pointer to the MapItem associated with id from the map.
+  MapItem* Find(int id) const;
 
-private:
-    std::map<int, MapItem*>    map_;
+ private:
+  std::map<int, MapItem*>    map_;
 };
+
 } // namespace webrtc
 
 #endif  // WEBRTC_SYSTEM_WRAPPERS_INTERFACE_MAP_WRAPPER_H_
--- webrtc/system_wrappers/source/map_no_stl.h
+++ webrtc/system_wrappers/source/map_no_stl.h
@@ -11,60 +11,60 @@
 #ifndef WEBRTC_SYSTEM_WRAPPERS_SOURCE_MAP_NO_STL_H_
 #define WEBRTC_SYSTEM_WRAPPERS_SOURCE_MAP_NO_STL_H_
 
-#include "constructor_magic.h"
+#include "webrtc/system_wrappers/interface/constructor_magic.h"
 
 namespace webrtc {
+
 class CriticalSectionWrapper;
 
-class MapNoStlItem
-{
-friend class Map;
+class MapNoStlItem {
+  friend class Map;
 
-public:
-    MapNoStlItem(int id, void* ptr);
-    virtual ~MapNoStlItem();
-    void* GetItem();
-    int GetId();
-    unsigned int GetUnsignedId();
-    void SetItem(void* ptr);
+ public:
+  MapNoStlItem(int id, void* ptr);
+  virtual ~MapNoStlItem();
+  void* GetItem();
+  int GetId();
+  unsigned int GetUnsignedId();
+  void SetItem(void* ptr);
 
-protected:
-    MapNoStlItem* next_;
-    MapNoStlItem* prev_;
+ protected:
+  MapNoStlItem* next_;
+  MapNoStlItem* prev_;
 
-private:
-    int item_id_;
-    void* item_ptr_;
-    DISALLOW_COPY_AND_ASSIGN(MapNoStlItem);
+ private:
+  int item_id_;
+  void* item_ptr_;
+  DISALLOW_COPY_AND_ASSIGN(MapNoStlItem);
 };
 
-class MapNoStl
-{
-public:
-    MapNoStl();
-    virtual ~MapNoStl();
+class MapNoStl {
+ public:
+  MapNoStl();
+  virtual ~MapNoStl();
 
-    // MapWrapper functions.
-    int Insert(int id, void* ptr);
-    int Erase(MapNoStlItem* item);
-    int Erase(int id);
-    int Size() const;
-    MapNoStlItem* First() const;
-    MapNoStlItem* Last() const;
-    MapNoStlItem* Next(MapNoStlItem* item) const;
-    MapNoStlItem* Previous(MapNoStlItem* item) const;
-    MapNoStlItem* Find(int id) const;
+  // MapWrapper functions.
+  int Insert(int id, void* ptr);
+  int Erase(MapNoStlItem* item);
+  int Erase(int id);
+  int Size() const;
+  MapNoStlItem* First() const;
+  MapNoStlItem* Last() const;
+  MapNoStlItem* Next(MapNoStlItem* item) const;
+  MapNoStlItem* Previous(MapNoStlItem* item) const;
+  MapNoStlItem* Find(int id) const;
 
-private:
-    MapNoStlItem* Locate(int id) const;
-    int Remove(MapNoStlItem* item);
+ private:
+  MapNoStlItem* Locate(int id) const;
+  int Remove(MapNoStlItem* item);
 
-    CriticalSection* critical_section_;
-    MapNoStlItem* first_;
-    MapNoStlItem* last_;
-    int size_;
-    DISALLOW_COPY_AND_ASSIGN(MapNoStl);
+  CriticalSection* critical_section_;
+  MapNoStlItem* first_;
+  MapNoStlItem* last_;
+  int size_;
+  DISALLOW_COPY_AND_ASSIGN(MapNoStl);
 };
+
 } // namespace webrtc
 
-#endif // WEBRTC_SYSTEM_WRAPPERS_SOURCE_MAP_NO_STL_H_
+#endif  // WEBRTC_SYSTEM_WRAPPERS_SOURCE_MAP_NO_STL_H_
