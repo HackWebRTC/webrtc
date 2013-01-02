201d4b61d1f0447916970a19c494956a50b14ff1
andrew@webrtc.org <andrew@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-01-02 19:59:53 +0000

    Fix implicit conversion error in mixing test.
    TBR=mikhal
    
    Review URL: https://webrtc-codereview.appspot.com/1020004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@3328 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/voice_engine/test/auto_test/standard/mixing_test.cc
+++ webrtc/voice_engine/test/auto_test/standard/mixing_test.cc
@@ -194,8 +194,9 @@ class MixingTest : public AfterInitializationFixture {
 TEST_F(MixingTest, FourChannelsWithOnlyThreeMixed) {
   const int16_t kInputValue = 1000;
   const int16_t kExpectedOutput = kInputValue * 3;
-  RunMixingTest(4, 0, 4, kInputValue, 1.15 * kExpectedOutput,
-                0.85 * kExpectedOutput);
+  RunMixingTest(4, 0, 4, kInputValue,
+                static_cast<int16_t>(1.15 * kExpectedOutput),
+                static_cast<int16_t>(0.85 * kExpectedOutput));
 }
 
 // Ensure the mixing saturation protection is working. We can do this because
@@ -207,8 +208,9 @@ TEST_F(MixingTest, VerifySaturationProtection) {
   // If this isn't satisfied, we're not testing anything.
   ASSERT_GT(kInputValue * 3, kInt16Max);
   ASSERT_LT(1.1 * kExpectedOutput, kInt16Max);
-  RunMixingTest(3, 0, 3, kInputValue, 1.15 * kExpectedOutput,
-               0.85 * kExpectedOutput);
+  RunMixingTest(3, 0, 3, kInputValue,
+                static_cast<int16_t>(1.15 * kExpectedOutput),
+                static_cast<int16_t>(0.85 * kExpectedOutput));
 }
 
 TEST_F(MixingTest, SaturationProtectionHasNoEffectOnOneChannel) {
@@ -218,28 +220,31 @@ TEST_F(MixingTest, SaturationProtectionHasNoEffectOnOneChannel) {
   ASSERT_GT(0.95 * kExpectedOutput, kLimiterHeadroom);
   // Tighter constraints are required here to properly test this.
   RunMixingTest(1, 0, 1, kInputValue, kExpectedOutput,
-                0.95 * kExpectedOutput);
+                static_cast<int16_t>(0.95 * kExpectedOutput));
 }
 
 TEST_F(MixingTest, VerifyAnonymousAndNormalParticipantMixing) {
   const int16_t kInputValue = 1000;
   const int16_t kExpectedOutput = kInputValue * 2;
-  RunMixingTest(1, 1, 1, kInputValue, 1.15 * kExpectedOutput,
-                0.85 * kExpectedOutput);
+  RunMixingTest(1, 1, 1, kInputValue,
+                static_cast<int16_t>(1.15 * kExpectedOutput),
+                static_cast<int16_t>(0.85 * kExpectedOutput));
 }
 
 TEST_F(MixingTest, AnonymousParticipantsAreAlwaysMixed) {
   const int16_t kInputValue = 1000;
   const int16_t kExpectedOutput = kInputValue * 4;
-  RunMixingTest(3, 1, 3, kInputValue, 1.15 * kExpectedOutput,
-                0.85 * kExpectedOutput);
+  RunMixingTest(3, 1, 3, kInputValue,
+                static_cast<int16_t>(1.15 * kExpectedOutput),
+                static_cast<int16_t>(0.85 * kExpectedOutput));
 }
 
 TEST_F(MixingTest, VerifyStereoAndMonoMixing) {
   const int16_t kInputValue = 1000;
   const int16_t kExpectedOutput = kInputValue * 2;
-  RunMixingTest(2, 0, 1, kInputValue, 1.15 * kExpectedOutput,
-                0.85 * kExpectedOutput);
+  RunMixingTest(2, 0, 1, kInputValue,
+                static_cast<int16_t>(1.15 * kExpectedOutput),
+                static_cast<int16_t>(0.85 * kExpectedOutput));
 }
 
 }  // namespace webrtc
