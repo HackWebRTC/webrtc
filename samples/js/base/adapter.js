a856db26a69f91dd1026240a95d72fe5b15db143
vikasmarwaha@webrtc.org <vikasmarwaha@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-03-05 03:35:26 +0000

    Moved trace function to adapter.js and removed from pc1 & multiple.html. Review URL: https://webrtc-codereview.appspot.com/1156005
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@3608 4adac7df-926f-26a2-2b94-8c16560cd09d

--- samples/js/base/adapter.js
+++ samples/js/base/adapter.js
@@ -4,6 +4,14 @@ var attachMediaStream = null;
 var reattachMediaStream = null;
 var webrtcDetectedBrowser = null;
 
+function trace(text) {
+  // This function is used for logging.
+  if (text[text.length - 1] == '\n') {
+    text = text.substring(0, text.length - 1);
+  }
+  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+}
+
 if (navigator.mozGetUserMedia) {
   console.log("This appears to be Firefox");
 
