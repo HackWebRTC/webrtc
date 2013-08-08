e2703314816bad1c39736501cbb1a74062890244
niklas.enbom@webrtc.org <niklas.enbom@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-08-08 22:23:48 +0000

    Fix duplicate code
    R=pwestin@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/1993004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@4507 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
+++ webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.cc
@@ -934,22 +934,8 @@ int32_t ModuleRtpRtcpImpl::SendOutgoingData(
                                    rtp_video_hdr);
   } else {
     CriticalSectionScoped lock(critical_section_module_ptrs_.get());
-
     std::list<ModuleRtpRtcpImpl*>::iterator it = child_modules_.begin();
-    if (it != child_modules_.end()) {
-      if ((*it)->SendingMedia()) {
-        ret_val = (*it)->SendOutgoingData(frame_type,
-                                          payload_type,
-                                          time_stamp,
-                                          capture_time_ms,
-                                          payload_data,
-                                          payload_size,
-                                          fragmentation,
-                                          rtp_video_hdr);
-      }
-      it++;
-    }
-    // Send to all remaining "child" modules
+    // Send to all "child" modules
     while (it != child_modules_.end()) {
       if ((*it)->SendingMedia()) {
         ret_val = (*it)->SendOutgoingData(frame_type,
