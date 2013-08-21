fbf0f69bf82dcf5da4ef144dc5f201e23d1f3001
pbos@webrtc.org <pbos@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-08-21 16:00:15 +0000

    Call SetExecutablePath from test_main.cc
    Fixes crash in video_engine_tests on bots, that were unabled to locate
    the resource file.
    
    BUG=
    R=mflodman@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/2083004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@4581 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/video_engine/test/test_main.cc
+++ webrtc/video_engine/test/test_main.cc
@@ -10,12 +10,14 @@
 
 #include "testing/gtest/include/gtest/gtest.h"
 
+#include "webrtc/test/testsupport/fileutils.h"
 #include "webrtc/video_engine/test/common/flags.h"
 #include "webrtc/video_engine/test/common/run_tests.h"
 
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   webrtc::test::flags::Init(&argc, &argv);
+  webrtc::test::SetExecutablePath(argv[0]);
 
   return webrtc::test::RunAllTests();
 }
