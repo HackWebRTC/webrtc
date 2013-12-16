e7b1e112833517c334a12aac16be17a27d798944
asapersson@webrtc.org <asapersson@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-12-16 14:40:36 +0000

    Revert 5285 "Revert 5228 "Use the RTT from RtcpRttStats class if..."
    > Revert 5228 "Use the RTT from RtcpRttStats class if provided whe..."
    > 
    > > Use the RTT from RtcpRttStats class if provided when sending/receiving NACK.
    > > 
    > > R=holmer@google.com
    > > 
    > > Review URL: https://webrtc-codereview.appspot.com/5049004
    > 
    > TBR=asapersson@webrtc.org
    > 
    > Review URL: https://webrtc-codereview.appspot.com/5799004
    
    TBR=mflodman@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/5989004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@5299 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.h
+++ webrtc/modules/rtp_rtcp/source/rtp_rtcp_impl.h
@@ -410,6 +410,7 @@ class ModuleRtpRtcpImpl : public RtpRtcp {
   Clock*                    clock_;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(RtpRtcpImplTest, Rtt);
   FRIEND_TEST_ALL_PREFIXES(RtpRtcpImplTest, RttForReceiverOnly);
   int64_t RtcpReportInterval();
   void SetRtcpReceiverSsrcs(uint32_t main_ssrc);
