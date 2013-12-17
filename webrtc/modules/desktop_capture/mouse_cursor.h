8fd1d265361eda8424d39e58b20dd8184420423e
sergeyu@chromium.org <sergeyu@chromium.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2013-12-17 22:19:12 +0000

    Make MouseCursor mutable
    MouseCursor objects were previous immutable which makes it harder to
    implement deserializers when MouseCursor is sent over IPC in Chromium.
    
    R=dcaiafa@chromium.org
    
    Review URL: https://webrtc-codereview.appspot.com/6059005
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@5310 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/desktop_capture/mouse_cursor.h
+++ webrtc/modules/desktop_capture/mouse_cursor.h
@@ -21,13 +21,19 @@ class DesktopFrame;
 
 class MouseCursor {
  public:
+  MouseCursor();
+
   // Takes ownership of |image|. |hotspot| must be within |image| boundaries.
   MouseCursor(DesktopFrame* image, const DesktopVector& hotspot);
+
   ~MouseCursor();
 
   static MouseCursor* CopyOf(const MouseCursor& cursor);
 
-  const DesktopFrame& image() const { return *image_; }
+  void set_image(DesktopFrame* image) { image_.reset(image); }
+  const DesktopFrame* image() const { return image_.get(); }
+
+  void set_hotspot(const DesktopVector& hotspot ) { hotspot_ = hotspot; }
   const DesktopVector& hotspot() const { return hotspot_; }
 
  private:
