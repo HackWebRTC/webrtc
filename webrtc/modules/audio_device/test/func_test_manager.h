79cf3acc79475ef05cfc984dc166463de79cc44a
henrike@webrtc.org <henrike@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-01-13 15:21:30 +0000

    Removes usage of ListWrapper from several files.
    BUG=2164
    R=andrew@webrtc.org, pbos@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/6269004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@5373 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/audio_device/test/func_test_manager.h
+++ webrtc/modules/audio_device/test/func_test_manager.h
@@ -13,13 +13,13 @@
 
 #include "webrtc/modules/audio_device/audio_device_utility.h"
 
+#include <list>
 #include <string>
 
 #include "webrtc/common_audio/resampler/include/resampler.h"
 #include "webrtc/modules/audio_device/include/audio_device.h"
 #include "webrtc/modules/audio_device/test/audio_device_test_defines.h"
 #include "webrtc/system_wrappers/interface/file_wrapper.h"
-#include "webrtc/system_wrappers/interface/list_wrapper.h"
 #include "webrtc/typedefs.h"
 
 #if defined(WEBRTC_IOS) || defined(ANDROID)
@@ -58,6 +58,15 @@ enum TestType
     TTDeviceRemoval = 13,
     TTMobileAPI = 14,
     TTTest = 66,
+};
+
+struct AudioPacket
+{
+    uint8_t dataBuffer[4 * 960];
+    uint16_t nSamples;
+    uint16_t nBytesPerSample;
+    uint8_t nChannels;
+    uint32_t samplesPerSec;
 };
 
 class ProcessThread;
@@ -165,6 +174,7 @@ public:
     ;
 
 private:
+    typedef std::list<AudioPacket*> AudioPacketList;
     AudioDeviceModule* _audioDevice;
 
     bool _playFromFile;
@@ -181,8 +191,7 @@ private:
 
     uint32_t _recCount;
     uint32_t _playCount;
-
-    ListWrapper _audioList;
+    AudioPacketList _audioList;
 
     Resampler _resampler;
 };
