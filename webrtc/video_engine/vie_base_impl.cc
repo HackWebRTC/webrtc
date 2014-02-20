3f170dd309b2093acde35fb2fc38e015850e4713
elham@webrtc.org <elham@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-02-20 22:31:07 +0000

    Updated WebRTC version to 3.50 TBR= wu@webrtc.org
    Review URL: https://webrtc-codereview.appspot.com/8979004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@5589 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/video_engine/vie_base_impl.cc
+++ webrtc/video_engine/vie_base_impl.cc
@@ -389,7 +389,7 @@ int ViEBaseImpl::GetVersion(char version[1024]) {
 
   // Add WebRTC Version.
   std::stringstream version_stream;
-  version_stream << "VideoEngine 3.49.0" << std::endl;
+  version_stream << "VideoEngine 3.50.0" << std::endl;
 
   // Add build info.
   version_stream << "Build: " << BUILDINFO << std::endl;
