ca9d038ac895d21716d62353f753444bfcfc2859
andrew@webrtc.org <andrew@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-04-01 01:19:08 +0000

    Fix ARM64 detection.
    Use only __aarch64__ and don't look for __arm64__ at all.
    It turns out that clang defines both and GCC only the former.
    Hence, looking only for __aarch64__ should be safe.
    
    BUG=chromium:354405,chromium:358092
    R=andrew@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/10939004
    
    Patch from Primiano Tucci <primiano@chromium.org>.
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@5821 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/typedefs.h
+++ webrtc/typedefs.h
@@ -23,7 +23,7 @@
 #define WEBRTC_ARCH_X86_64
 #define WEBRTC_ARCH_64_BITS
 #define WEBRTC_ARCH_LITTLE_ENDIAN
-#elif defined(__arm64__) || defined(__aarch64__)
+#elif defined(__aarch64__)
 #define WEBRTC_ARCH_64_BITS
 #define WEBRTC_ARCH_LITTLE_ENDIAN
 #elif defined(_M_IX86) || defined(__i386__)
