b235c56017a3b5bacccef1a994c1d91edef8330a
jiayl@webrtc.org <jiayl@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-05-03 00:16:29 +0000

    Implement the Windows screen capturer using the Magnification API. The original ScreenCapturerWin is renamed ScreenCapturerWinGdi.
    BUG=2789
    TESTED=full desktop cast and single monitor cast works on win7 and win8 desktop mode. Have to use GDI capturer on win8 metro mode. Changing display configuration work on the fly.
    R=sergeyu@chromium.org, wez@chromium.org
    
    Review URL: https://webrtc-codereview.appspot.com/12149004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6048 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/desktop_capture/desktop_capture_options.cc
+++ webrtc/modules/desktop_capture/desktop_capture_options.cc
@@ -19,6 +19,10 @@ DesktopCaptureOptions::DesktopCaptureOptions()
   // XDamage is often broken, so don't use it by default.
   use_update_notifications_ = false;
 #endif
+
+#if defined(WEBRTC_WIN)
+  allow_use_magnification_api_ = false;
+#endif
 }
 
 DesktopCaptureOptions::~DesktopCaptureOptions() {}
