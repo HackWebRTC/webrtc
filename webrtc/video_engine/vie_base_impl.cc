0720758f9f7440886c02091fcb160863e051dbff
tnakamura@webrtc.org <tnakamura@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-05-22 17:51:18 +0000

    Bump WebRTC version number to 3.54 TBR=wu@webrtc.org
    Review URL: https://webrtc-codereview.appspot.com/17619006
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6222 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/video_engine/vie_base_impl.cc
+++ webrtc/video_engine/vie_base_impl.cc
@@ -335,7 +335,7 @@ int ViEBaseImpl::GetVersion(char version[1024]) {
 
   // Add WebRTC Version.
   std::stringstream version_stream;
-  version_stream << "VideoEngine 3.53.0" << std::endl;
+  version_stream << "VideoEngine 3.54.0" << std::endl;
 
   // Add build info.
   version_stream << "Build: " << BUILDINFO << std::endl;
