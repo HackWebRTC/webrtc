40b45fc07a214c4102d83883bbd8bb7521de11e2
buildbot@webrtc.org <buildbot@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-06-04 14:48:33 +0000

    (Auto)update libjingle 68506654-> 68507189
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6328 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/media/webrtc/webrtcvideoengine.cc
+++ talk/media/webrtc/webrtcvideoengine.cc
@@ -69,6 +69,10 @@
 #if !defined(LIBPEERCONNECTION_LIB)
 #include "talk/media/webrtc/webrtcmediaengine.h"
 
+#ifdef _MSC_VER
+#define strtok_r strtok_s
+#endif  // _MSC_VER
+
 WRME_EXPORT
 cricket::MediaEngineInterface* CreateWebRtcMediaEngine(
     webrtc::AudioDeviceModule* adm, webrtc::AudioDeviceModule* adm_sc,
@@ -145,8 +149,9 @@ static int GetExternalVideoPayloadType(int index) {
 
 static void LogMultiline(talk_base::LoggingSeverity sev, char* text) {
   const char* delim = "\r\n";
-  // TODO(fbarchard): Fix strtok lint warning.
-  for (char* tok = strtok(text, delim); tok; tok = strtok(NULL, delim)) {
+  char* strtok_save;
+  for (char* tok = strtok_r(text, delim, &strtok_save);
+       tok; tok = strtok_r(NULL, delim, &strtok_save)) {
     LOG_V(sev) << tok;
   }
 }
