9c55f0f957534144d2b8a64154f0a479249b34be
henrik.lundin@webrtc.org <henrik.lundin@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-06-09 08:10:28 +0000

    Rename neteq4 folder to neteq
    Keep the old neteq4/audio_decoder_unittests.isolate while waiting for
    a hard-coded reference to change.
    
    This CL effectively reverts r6257 "Rename neteq4 folder to neteq".
    
    BUG=2996
    TBR=tina.legrand@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/21629004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6367 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/audio_coding/main/acm2/acm_receiver_unittest.cc
+++ webrtc/modules/audio_coding/main/acm2/acm_receiver_unittest.cc
@@ -16,7 +16,7 @@
 #include "webrtc/modules/audio_coding/main/interface/audio_coding_module.h"
 #include "webrtc/modules/audio_coding/main/acm2/audio_coding_module_impl.h"
 #include "webrtc/modules/audio_coding/main/acm2/acm_codec_database.h"
-#include "webrtc/modules/audio_coding/neteq4/tools/rtp_generator.h"
+#include "webrtc/modules/audio_coding/neteq/tools/rtp_generator.h"
 #include "webrtc/system_wrappers/interface/clock.h"
 #include "webrtc/system_wrappers/interface/scoped_ptr.h"
 #include "webrtc/test/test_suite.h"
