ddeec048c093da6e8e3dc17e599672681fa4def7
jiayl@webrtc.org <jiayl@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-06-12 21:42:46 +0000

    Revert r6390 "Adds end to end DataChannel tests." Flaky on linux_memcheck
    This reverts commit c3272a942f04f9dd0db3f6bf0d201bcf47c3fa08.
    
    TBR=wu@webrtc.org
    BUG=2626
    
    Review URL: https://webrtc-codereview.appspot.com/13689004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6420 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/app/webrtc/test/peerconnectiontestwrapper.cc
+++ talk/app/webrtc/test/peerconnectiontestwrapper.cc
@@ -103,13 +103,6 @@ bool PeerConnectionTestWrapper::CreatePc(
   return peer_connection_.get() != NULL;
 }
 
-talk_base::scoped_refptr<webrtc::DataChannelInterface>
-PeerConnectionTestWrapper::CreateDataChannel(
-    const std::string& label,
-    const webrtc::DataChannelInit& init) {
-  return peer_connection_->CreateDataChannel(label, &init);
-}
-
 void PeerConnectionTestWrapper::OnAddStream(MediaStreamInterface* stream) {
   LOG(LS_INFO) << "PeerConnectionTestWrapper " << name_
                << ": OnAddStream";
@@ -127,11 +120,6 @@ void PeerConnectionTestWrapper::OnIceCandidate(
   SignalOnIceCandidateCreated(&sdp);
   SignalOnIceCandidateReady(candidate->sdp_mid(), candidate->sdp_mline_index(),
                             sdp);
-}
-
-void PeerConnectionTestWrapper::OnDataChannel(
-    webrtc::DataChannelInterface* data_channel) {
-  SignalOnDataChannel(data_channel);
 }
 
 void PeerConnectionTestWrapper::OnSuccess(SessionDescriptionInterface* desc) {
