6568e97d108dffbf10af04a764287f3d1589691f
minyue@webrtc.org <minyue@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-06-25 12:17:41 +0000

    This is to compare NetEq with various codecs under a shared packet loss pattern.
    TEST=passed_all_trybots
    R=henrik.lundin@webrtc.org, tina.legrand@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/15529004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6536 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/audio_coding/neteq/neteq_tests.gypi
+++ webrtc/modules/audio_coding/neteq/neteq_tests.gypi
@@ -203,6 +203,22 @@
     },
 
     {
+      'target_name': 'neteq_isac_quality_test',
+      'type': 'executable',
+      'dependencies': [
+        'neteq',
+        'neteq_test_support',
+        'iSACFix',
+        '<(DEPTH)/testing/gtest.gyp:gtest',
+        '<(DEPTH)/third_party/gflags/gflags.gyp:gflags',
+        '<(webrtc_root)/test/test.gyp:test_support_main',
+      ],
+      'sources': [
+        'test/neteq_isac_quality_test.cc',
+      ],
+    },
+
+    {
      'target_name': 'neteq_test_tools',
       # Collection of useful functions used in other tests.
       'type': 'static_library',
--- webrtc/modules/audio_coding/neteq/tools/neteq_quality_test.h
+++ webrtc/modules/audio_coding/neteq/tools/neteq_quality_test.h
@@ -11,6 +11,7 @@
 #ifndef WEBRTC_MODULES_AUDIO_CODING_NETEQ_TOOLS_NETEQ_QUALITY_TEST_H_
 #define WEBRTC_MODULES_AUDIO_CODING_NETEQ_TOOLS_NETEQ_QUALITY_TEST_H_
 
+#include <gflags/gflags.h>
 #include <string>
 #include "testing/gtest/include/gtest/gtest.h"
 #include "webrtc/modules/audio_coding/neteq/interface/neteq.h"
@@ -19,8 +20,43 @@
 #include "webrtc/system_wrappers/interface/scoped_ptr.h"
 #include "webrtc/typedefs.h"
 
+using google::RegisterFlagValidator;
+
 namespace webrtc {
 namespace test {
+
+class LossModel {
+ public:
+  virtual ~LossModel() {};
+  virtual bool Lost() = 0;
+};
+
+class NoLoss : public LossModel {
+ public:
+  virtual bool Lost() OVERRIDE;
+};
+
+class UniformLoss : public LossModel {
+ public:
+  UniformLoss(int loss_rate);
+  virtual bool Lost() OVERRIDE;
+  void set_loss_rate(double loss_rate) { loss_rate_ = loss_rate; }
+ private:
+  double loss_rate_;
+};
+
+class GilbertElliotLoss : public LossModel {
+ public:
+  GilbertElliotLoss(double prob_trans_11, double prob_trans_01);
+  virtual bool Lost() OVERRIDE;
+ private:
+  // Prob. of losing current packet, when previous packet is lost.
+  double prob_trans_11_;
+  // Prob. of losing current packet, when previous packet is not lost.
+  double prob_trans_01_;
+  bool lost_last_;
+  scoped_ptr<UniformLoss> uniform_loss_model_;
+};
 
 class NetEqQualityTest : public ::testing::Test {
  protected:
@@ -29,7 +65,6 @@ class NetEqQualityTest : public ::testing::Test {
                    int out_sampling_khz,
                    enum NetEqDecoder decoder_type,
                    int channels,
-                   double drift_factor,
                    std::string in_filename,
                    std::string out_filename);
   virtual void SetUp() OVERRIDE;
@@ -43,9 +78,9 @@ class NetEqQualityTest : public ::testing::Test {
   virtual int EncodeBlock(int16_t* in_data, int block_size_samples,
                           uint8_t* payload, int max_bytes) = 0;
 
-  // PacketLoss(...) determines weather a packet sent at an indicated time gets
+  // PacketLost(...) determines weather a packet sent at an indicated time gets
   // lost or not.
-  virtual bool PacketLost(int packet_input_time_ms) { return false; }
+  bool PacketLost();
 
   // DecodeBlock() decodes a block of audio using the payload stored in
   // |payload_| with the length of |payload_size_bytes_| (bytes). The decoded
@@ -65,6 +100,7 @@ class NetEqQualityTest : public ::testing::Test {
   int decoded_time_ms_;
   int decodable_time_ms_;
   double drift_factor_;
+  int packet_loss_rate_;
   const int block_duration_ms_;
   const int in_sampling_khz_;
   const int out_sampling_khz_;
@@ -72,6 +108,7 @@ class NetEqQualityTest : public ::testing::Test {
   const int channels_;
   const std::string in_filename_;
   const std::string out_filename_;
+  const std::string log_filename_;
 
   // Number of samples per channel in a frame.
   const int in_size_samples_;
@@ -84,14 +121,18 @@ class NetEqQualityTest : public ::testing::Test {
 
   scoped_ptr<InputAudioFile> in_file_;
   FILE* out_file_;
+  FILE* log_file_;
 
   scoped_ptr<RtpGenerator> rtp_generator_;
   scoped_ptr<NetEq> neteq_;
+  scoped_ptr<LossModel> loss_model_;
 
   scoped_ptr<int16_t[]> in_data_;
   scoped_ptr<uint8_t[]> payload_;
   scoped_ptr<int16_t[]> out_data_;
   WebRtcRTPHeader rtp_header_;
+
+  long total_payload_size_bytes_;
 };
 
 }  // namespace test
