bfa758a54c066f4d0bb125e102fbb654ee177a88
buildbot@webrtc.org <buildbot@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-06-27 16:04:43 +0000

    (Auto)update libjingle 70004190-> 70103367
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6555 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/media/webrtc/fakewebrtcvoiceengine.h
+++ talk/media/webrtc/fakewebrtcvoiceengine.h
@@ -862,9 +862,12 @@ class FakeWebRtcVoiceEngine
   }
 #ifdef USE_WEBRTC_DEV_BRANCH
   WEBRTC_FUNC(SetREDStatus, (int channel, bool enable, int redPayloadtype)) {
-#else
+    return SetFECStatus(channel, enable, redPayloadtype);
+  }
+#endif
+  // TODO(minyue): remove the below function when transition to SetREDStatus
+  //               is finished.
   WEBRTC_FUNC(SetFECStatus, (int channel, bool enable, int redPayloadtype)) {
-#endif  // USE_WEBRTC_DEV_BRANCH
     WEBRTC_CHECK_CHANNEL(channel);
     channels_[channel]->red = enable;
     channels_[channel]->red_type = redPayloadtype;
@@ -872,9 +875,12 @@ class FakeWebRtcVoiceEngine
   }
 #ifdef USE_WEBRTC_DEV_BRANCH
   WEBRTC_FUNC(GetREDStatus, (int channel, bool& enable, int& redPayloadtype)) {
-#else
+    return GetFECStatus(channel, enable, redPayloadtype);
+  }
+#endif
+  // TODO(minyue): remove the below function when transition to GetREDStatus
+  //               is finished.
   WEBRTC_FUNC(GetFECStatus, (int channel, bool& enable, int& redPayloadtype)) {
-#endif  // USE_WEBRTC_DEV_BRANCH
     WEBRTC_CHECK_CHANNEL(channel);
     enable = channels_[channel]->red;
     redPayloadtype = channels_[channel]->red_type;
