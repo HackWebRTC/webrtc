1b84116417d5b5809482cd0b0d9dd4af54668508
henrike@webrtc.org <henrike@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-07-14 21:42:39 +0000

    Add a facility to the Thread class to catch blocking regressions.
    This facility should be used in methods that run on known threads
    (e.g. signaling, worker) and do not have blocking thread syncronization
    operations via the Thread class such as Invoke, Sleep, etc.
    
    This is a reland of an already reviewed cl (r6679) that got reverted by mistake.
    
    TBR=xians@google.com,tommi@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/21889004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6682 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/base/thread.cc
+++ talk/base/thread.cc
@@ -142,6 +142,16 @@ struct ThreadInit {
   Runnable* runnable;
 };
 
+Thread::ScopedDisallowBlockingCalls::ScopedDisallowBlockingCalls()
+  : thread_(Thread::Current()),
+    previous_state_(thread_->SetAllowBlockingCalls(false)) {
+}
+
+Thread::ScopedDisallowBlockingCalls::~ScopedDisallowBlockingCalls() {
+  ASSERT(thread_->IsCurrent());
+  thread_->SetAllowBlockingCalls(previous_state_);
+}
+
 Thread::Thread(SocketServer* ss)
     : MessageQueue(ss),
       priority_(PRIORITY_NORMAL),
@@ -150,7 +160,8 @@ Thread::Thread(SocketServer* ss)
       thread_(NULL),
       thread_id_(0),
 #endif
-      owned_(true) {
+      owned_(true),
+      blocking_calls_allowed_(true) {
   SetName("Thread", this);  // default name
 }
 
@@ -160,6 +171,8 @@ Thread::~Thread() {
 }
 
 bool Thread::SleepMs(int milliseconds) {
+  AssertBlockingIsAllowedOnCurrentThread();
+
 #ifdef WIN32
   ::Sleep(milliseconds);
   return true;
@@ -293,6 +306,8 @@ bool Thread::Start(Runnable* runnable) {
 }
 
 void Thread::Join() {
+  AssertBlockingIsAllowedOnCurrentThread();
+
   if (running()) {
     ASSERT(!IsCurrent());
 #if defined(WIN32)
@@ -306,6 +321,21 @@ void Thread::Join() {
 #endif
     running_.Reset();
   }
+}
+
+bool Thread::SetAllowBlockingCalls(bool allow) {
+  ASSERT(IsCurrent());
+  bool previous = blocking_calls_allowed_;
+  blocking_calls_allowed_ = allow;
+  return previous;
+}
+
+// static
+void Thread::AssertBlockingIsAllowedOnCurrentThread() {
+#ifdef _DEBUG
+  Thread* current = Thread::Current();
+  ASSERT(!current || current->blocking_calls_allowed_);
+#endif
 }
 
 #ifdef WIN32
@@ -374,6 +404,8 @@ void Thread::Stop() {
 }
 
 void Thread::Send(MessageHandler *phandler, uint32 id, MessageData *pdata) {
+  AssertBlockingIsAllowedOnCurrentThread();
+
   if (fStop_)
     return;
 
