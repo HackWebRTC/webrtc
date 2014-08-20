8dcf61f946c45affd25fea9a6def9a6639cf8de3
tnakamura@webrtc.org <tnakamura@webrtc.org@4adac7df-926f-26a2-2b94-8c16560cd09d>
2014-08-20 22:44:18 +0000

    Bump WebRTC version number. Starting now, we will be setting WebRTC major version numbers to align with Chrome.
    R=niklas.enbom@webrtc.org
    TBR=niklas.embom
    
    Review URL: https://webrtc-codereview.appspot.com/15219004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@6942 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/video_engine/vie_base_impl.cc
+++ webrtc/video_engine/vie_base_impl.cc
@@ -337,7 +337,7 @@ int ViEBaseImpl::GetVersion(char version[1024]) {
 
   // Add WebRTC Version.
   std::stringstream version_stream;
-  version_stream << "VideoEngine 3.55.0" << std::endl;
+  version_stream << "VideoEngine 38" << std::endl;
 
   // Add build info.
   version_stream << "Build: " << BUILDINFO << std::endl;
