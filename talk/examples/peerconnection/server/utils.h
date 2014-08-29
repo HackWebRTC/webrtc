44010f3e52b0907a73be0c1b41bf9c870446b8cb
thakis@chromium.org <thakis@chromium.org>
2014-08-29 03:00:15 +0000

    win: Replace custom assert() macro with regular assert.h
    The current code got added in libjingle r103; I don't see a good reason for it.
    Things still build with plain old assert.h.
    
    The custom assert was wrong: __debugbreak() is documented to return void,
    so doing `cond ? true : __debugbreak()` shouldn't build (and it doesn't in
    clang-cl). It's possible to make it build by writing
    `cond ? true : (__debugbreak(), true)`, but just using the regular header
    seems like a much better fix.
    
    BUG=chromium:82385
    Review URL: https://webrtc-codereview.appspot.com/19139004/
    
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@7007 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/examples/peerconnection/server/utils.h
+++ talk/examples/peerconnection/server/utils.h
@@ -29,18 +29,7 @@
 #define TALK_EXAMPLES_PEERCONNECTION_SERVER_UTILS_H_
 #pragma once
 
-#ifndef assert
-#ifndef WIN32
 #include <assert.h>
-#else
-#ifndef NDEBUG
-#define assert(expr)  ((void)((expr) ? true : __debugbreak()))
-#else
-#define assert(expr)  ((void)0)
-#endif  // NDEBUG
-#endif  // WIN32
-#endif  // assert
-
 #include <string>
 
 #ifndef ARRAYSIZE
