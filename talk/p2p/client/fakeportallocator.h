369a637ac8032b3e41fd2b4f2f6b2ef49a447f02
guoweis@webrtc.org <guoweis@webrtc.org>
2014-09-17 22:37:29 +0000

    Implemented Network::GetBestIP() selection logic as following.
    1) return the first global temporary and non-deprecrated ones.
    2) if #1 not available, return global one.
    3) if #2 not available, use ULA ipv6 as last resort.
    
    ULA stands for unique local address. They are only useful in a private
    WebRTC deployment. More detail: http://en.wikipedia.org/wiki/Unique_local_address
    
    BUG=3808
    
    At this point, rule #3 actually won't happen at current
    implementation. The reason being that ULA address starting with 0xfc 0r 0xfd will be grouped into its own Network. The result of that is WebRTC will have one extra Network to generate candidates but the lack of rule #3 shouldn't prevent turning on IPv6 since ULA should only be tried in a close deployment anyway.
    
    R=jiayl@webrtc.org
    
    Committed: https://code.google.com/p/webrtc/source/detail?r=7200
    
    Committed: https://code.google.com/p/webrtc/source/detail?r=7201
    
    Review URL: https://webrtc-codereview.appspot.com/31369004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@7216 4adac7df-926f-26a2-2b94-8c16560cd09d

--- talk/p2p/client/fakeportallocator.h
+++ talk/p2p/client/fakeportallocator.h
@@ -1,6 +1,29 @@
-// Copyright 2010 Google Inc. All Rights Reserved,
-//
-// Author: Justin Uberti (juberti@google.com)
+/*
+ * libjingle
+ * Copyright 2010, Google Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef TALK_P2P_CLIENT_FAKEPORTALLOCATOR_H_
 #define TALK_P2P_CLIENT_FAKEPORTALLOCATOR_H_
@@ -39,10 +62,14 @@ class FakePortAllocatorSession : public PortAllocatorSession {
 
   virtual void StartGettingPorts() {
     if (!port_) {
-      port_.reset(cricket::UDPPort::Create(worker_thread_, factory_,
-                      &network_, network_.ip(), 0, 0,
-                      username(),
-                      password()));
+      port_.reset(cricket::UDPPort::Create(worker_thread_,
+                                           factory_,
+                                           &network_,
+                                           network_.GetBestIP(),
+                                           0,
+                                           0,
+                                           username(),
+                                           password()));
       AddPort(port_.get());
     }
     ++port_config_count_;
--- webrtc/base/network.h
+++ webrtc/base/network.h
@@ -188,19 +188,28 @@ class Network {
   std::string key() const { return key_; }
 
   // Returns the Network's current idea of the 'best' IP it has.
-  // 'Best' currently means the first one added.
-  // Returns an unset IP if this network has no active addresses.
-  // Here is the rule on how we mark the IPv6 address as ignorable for webrtc.
+  // Or return an unset IP if this network has no active addresses.
+  // Here is the rule on how we mark the IPv6 address as ignorable for WebRTC.
   // 1) return all global temporary dynamic and non-deprecrated ones.
-  // 2) if #1 not available, return global dynamic ones.
-  // 3) if #2 not available, return global ones.
-  // 4) if #3 not available, use ULA ipv6 as last resort.
+  // 2) if #1 not available, return global ones.
+  // 3) if #2 not available, use ULA ipv6 as last resort. (ULA stands
+  // for unique local address, which is not route-able in open
+  // internet but might be useful for a close WebRTC deployment.
+
+  // TODO(guoweis): rule #3 actually won't happen at current
+  // implementation. The reason being that ULA address starting with
+  // 0xfc 0r 0xfd will be grouped into its own Network. The result of
+  // that is WebRTC will have one extra Network to generate candidates
+  // but the lack of rule #3 shouldn't prevent turning on IPv6 since
+  // ULA should only be tried in a close deployment anyway.
+
   // Note that when not specifying any flag, it's treated as case global
-  // dynamic IPv6 address
-  // TODO(guoweis): will change the name to a more meaningful name as
-  // this is not simply return the first address once the logic of ipv6
-  // address selection is complete.
-  IPAddress ip() const;
+  // IPv6 address
+  IPAddress GetBestIP() const;
+
+  // Keep the original function here for now.
+  // TODO(guoweis): Remove this when all callers are migrated to GetBestIP().
+  IPAddress ip() const { return GetBestIP(); }
 
   // Adds an active IP address to this network. Does not check for duplicates.
   void AddIP(const InterfaceAddress& ip) { ips_.push_back(ip); }
