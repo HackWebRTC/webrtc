94ff92ceec7a2fe3e1d0e244a06e71e7dcc28256
johannkoenig@google.com <johannkoenig@google.com>
2014-09-23 17:31:47 +0000

    Use VPX_IMG_FMT_*/VPX_PLANE_* defines
    The compatibility layer has been removed upstream:
    https://gerrit.chromium.org/gerrit/gitweb?p=webm%2Flibvpx.git;a=commit;h=9cdaa3d72eade9ad162ef8f78a93bd8f85c6de10
    
    BUG=webrtc:3839
    R=marpan@google.com, marpan@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/22729004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@7279 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/video_coding/codecs/vp8/vp8_impl.cc
+++ webrtc/modules/video_coding/codecs/vp8/vp8_impl.cc
@@ -175,7 +175,7 @@ int VP8EncoderImpl::InitEncode(const VideoCodec* inst,
   // Creating a wrapper to the image - setting image data to NULL. Actual
   // pointer will be set in encode. Setting align to 1, as it is meaningless
   // (actual memory is not allocated).
-  raw_ = vpx_img_wrap(NULL, IMG_FMT_I420, codec_.width, codec_.height,
+  raw_ = vpx_img_wrap(NULL, VPX_IMG_FMT_I420, codec_.width, codec_.height,
                       1, NULL);
   // populate encoder configuration with default values
   if (vpx_codec_enc_config_default(vpx_codec_vp8_cx(), config_, 0)) {
@@ -356,9 +356,9 @@ int VP8EncoderImpl::Encode(const I420VideoFrame& input_frame,
   }
   // Image in vpx_image_t format.
   // Input frame is const. VP8's raw frame is not defined as const.
-  raw_->planes[PLANE_Y] = const_cast<uint8_t*>(frame.buffer(kYPlane));
-  raw_->planes[PLANE_U] = const_cast<uint8_t*>(frame.buffer(kUPlane));
-  raw_->planes[PLANE_V] = const_cast<uint8_t*>(frame.buffer(kVPlane));
+  raw_->planes[VPX_PLANE_Y] = const_cast<uint8_t*>(frame.buffer(kYPlane));
+  raw_->planes[VPX_PLANE_U] = const_cast<uint8_t*>(frame.buffer(kUPlane));
+  raw_->planes[VPX_PLANE_V] = const_cast<uint8_t*>(frame.buffer(kVPlane));
   // TODO(mikhal): Stride should be set in initialization.
   raw_->stride[VPX_PLANE_Y] = frame.stride(kYPlane);
   raw_->stride[VPX_PLANE_U] = frame.stride(kUPlane);
