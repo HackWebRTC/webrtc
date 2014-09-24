a0ce9fa2a65a693ca9a6ee4920fb41d5cdd92e3b
aluebs@webrtc.org <aluebs@webrtc.org>
2014-09-24 14:18:03 +0000

    Call NS AnalyzeCaptureAudio before AEC
    This attenuates the noise pumping generated from the NS adapting to the AEC comfort noise.
    
    When there is echo present the AEC suppresses it and adds comfort noise. This is underestimated on purpose to avoid adding more than the original background noise. The NS has to be called after the AEC, because every non-linear processing before it can ruin its performance. Therefore the noise estimation can adapt to this comfort noise, making it less aggressive and generating noise pumping.
    
    By putting the noise estimation analysis stage from the NS before the AEC, this effect can be avoided. This has been tested manually on recordings where noise pumping was present: Two long recordings done in our team by bjornv and kwiberg plus the most noisy (5) recordings in the QA set.
    
    On the other hand, one risk of doing this is to not adapt to the comfort noise and therefore suppress too much. As verified in the tested files, this is not a problem in practice.
    
    BUG=webrtc:3763
    R=andrew@webrtc.org, bjornv@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/24679004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@7289 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/audio_processing/audio_processing_impl.cc
+++ webrtc/modules/audio_processing/audio_processing_impl.cc
@@ -482,12 +482,12 @@ int AudioProcessingImpl::ProcessStreamLocked() {
 
   RETURN_ON_ERR(high_pass_filter_->ProcessCaptureAudio(ca));
   RETURN_ON_ERR(gain_control_->AnalyzeCaptureAudio(ca));
+  RETURN_ON_ERR(noise_suppression_->AnalyzeCaptureAudio(ca));
   RETURN_ON_ERR(echo_cancellation_->ProcessCaptureAudio(ca));
 
   if (echo_control_mobile_->is_enabled() && noise_suppression_->is_enabled()) {
     ca->CopyLowPassToReference();
   }
-  RETURN_ON_ERR(noise_suppression_->AnalyzeCaptureAudio(ca));
   RETURN_ON_ERR(noise_suppression_->ProcessCaptureAudio(ca));
   RETURN_ON_ERR(echo_control_mobile_->ProcessCaptureAudio(ca));
   RETURN_ON_ERR(voice_detection_->ProcessCaptureAudio(ca));
