626c09f6a378f38a459fc7cff73856eca2bac451
kjellander@webrtc.org <kjellander@webrtc.org>
2014-12-11 11:59:46 +0000

    Move isolate path into webrtc/build/android/test_runner.py
    This will make it easier to execute tests and allows
    for more cleanup in the buildbot recipes.
    Now tests can be listed using:
    webrtc/build/android/test_runner.py gtest --help
    and executed like
    webrtc/build/android/test_runner.py gtest -s audio_decoder_unittests
    
    TESTED=
    Ran:
    webrtc/build/android/test_runner.py gtest --help
    and verified the tests were listed.
    I wiped /sdcard/resources on my device, executed:
    webrtc/build/android/test_runner.py gtest -s audio_decoder_unittests
    and verified it passed and that resources/audio_coding/testfile32kHz.pcm
    was copied to the device.
    
    BUG=
    R=phoglund@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/33619004
    
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@7873 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/build/android/test_runner.py
+++ webrtc/build/android/test_runner.py
@@ -27,7 +27,7 @@ sys.path.insert(0, CHROMIUM_BUILD_ANDROID_DIR)
 
 import test_runner
 from pylib.gtest import gtest_config
-
+from pylib.gtest import setup
 
 def main():
   # Override the stable test suites with the WebRTC tests.
@@ -46,6 +46,34 @@ def main():
     'voice_engine_unittests',
     'webrtc_perf_tests',
   ]
+  gtest_config.EXPERIMENTAL_TEST_SUITES = []
+
+  # Set our own paths to the .isolate files.
+  setup.ISOLATE_FILE_PATHS = {
+    'audio_decoder_unittests':
+        'webrtc/modules/audio_coding/neteq/audio_decoder_unittests.isolate',
+    'common_audio_unittests':
+        'webrtc/common_audio/common_audio_unittests.isolate',
+    'common_video_unittests':
+        'webrtc/common_video/common_video_unittests.isolate',
+    'modules_tests': 'webrtc/modules/modules_tests.isolate',
+    'modules_unittests': 'webrtc/modules/modules_unittests.isolate',
+    'system_wrappers_unittests':
+        'webrtc/system_wrappers/source/system_wrappers_unittests.isolate',
+    'test_support_unittests': 'webrtc/test/test_support_unittests.isolate',
+    'tools_unittests': 'webrtc/tools/tools_unittests.isolate',
+    'video_capture_tests':
+        'webrtc/modules/video_capture/video_capture_tests.isolate',
+    'video_engine_tests': 'webrtc/video_engine_tests.isolate',
+    'video_engine_core_unittests':
+        'webrtc/video_engine/video_engine_core_unittests.isolate',
+    'voice_engine_unittests':
+        'webrtc/voice_engine/voice_engine_unittests.isolate',
+    'webrtc_perf_tests': 'webrtc/webrtc_perf_tests.isolate',
+  }
+  # Override environment variable to make it possible for the scripts to find
+  # the root directory (our symlinking of the Chromium build toolchain would
+  # otherwise make them fail to do so).
   os.environ['CHECKOUT_SOURCE_ROOT'] = SRC_DIR
   return test_runner.main()
 
