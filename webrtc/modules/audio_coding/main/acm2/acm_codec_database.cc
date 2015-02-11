fddeaf5daabd5a095135b8440ec3c4aaf46b3525
henrik.lundin@webrtc.org <henrik.lundin@webrtc.org>
2015-02-11 13:28:20 +0000

    Switch to using AudioEncoderG722 instead of ACMG722
    This change switches from the old codec wrapper ACMG722 to the new
    AudioEncodeG722 wrapped in an ACMGenericCodecWrapper.
    
    BUG=4228
    COAUTHOR=kwiberg@webrtc.org
    R=tina.legrand@webrtc.org
    
    Review URL: https://webrtc-codereview.appspot.com/39879004
    
    Cr-Commit-Position: refs/heads/master@{#8330}
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@8330 4adac7df-926f-26a2-2b94-8c16560cd09d

--- webrtc/modules/audio_coding/main/acm2/acm_codec_database.cc
+++ webrtc/modules/audio_coding/main/acm2/acm_codec_database.cc
@@ -601,11 +601,9 @@ ACMGenericCodec* ACMCodecDB::CreateCodecInstance(const CodecInst& codec_inst,
 #endif
   } else if (!STR_CASE_CMP(codec_inst.plname, "G722")) {
 #ifdef WEBRTC_CODEC_G722
-    if (codec_inst.channels == 1) {
-      return new ACMG722(kG722, enable_red);
-    } else {
-      return new ACMG722(kG722_2ch, enable_red);
-    }
+    return new ACMGenericCodecWrapper(codec_inst, cng_pt_nb, cng_pt_wb,
+                                      cng_pt_swb, cng_pt_fb, enable_red,
+                                      red_payload_type);
 #endif
   } else if (!STR_CASE_CMP(codec_inst.plname, "G7221")) {
     switch (codec_inst.plfreq) {
