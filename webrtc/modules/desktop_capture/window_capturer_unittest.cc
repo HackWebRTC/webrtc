907bfb25bd1c497a8b1562d47c964d8c8c1f47dc
Peter Kasting <pkasting@google.com>
2015-06-01 14:50:02 -0700

    Fix an apparent typo in a unittest that caused it to not actually check the new window list it fetched.
    This would only have an effect when window capture failed, and apparently that was never happening.
    
    BUG=none
    R=sergeyu@chromium.org
    
    Review URL: https://webrtc-codereview.appspot.com/52009004
    
    Cr-Commit-Position: refs/heads/master@{#9348}

--- webrtc/modules/desktop_capture/window_capturer_unittest.cc
+++ webrtc/modules/desktop_capture/window_capturer_unittest.cc
@@ -75,8 +75,8 @@ TEST_F(WindowCapturerTest, Capture) {
     if (!frame_.get()) {
       WindowCapturer::WindowList new_list;
       EXPECT_TRUE(capturer_->GetWindowList(&new_list));
-      for (WindowCapturer::WindowList::iterator new_list_it = windows.begin();
-           new_list_it != windows.end(); ++new_list_it) {
+      for (WindowCapturer::WindowList::iterator new_list_it = new_list.begin();
+           new_list_it != new_list.end(); ++new_list_it) {
         EXPECT_FALSE(it->id == new_list_it->id);
       }
       continue;
