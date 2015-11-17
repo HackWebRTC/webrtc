c073615d56b8f626bf694868a276fd135898da11
davidben <davidben@webrtc.org>
2015-11-17 12:58:11 -0800

    Update references to TLS1_CK_ECDHE_RSA_CHACHA20_POLY1305, etc.
    In preparation for implementing the standardized variant of CHACHA20_POLY1305
    (it changed slightly in the standardization process),
    TLS1_CK_ECDHE_RSA_CHACHA20_POLY1305 and TLS1_CK_ECDHE_ECDSA_CHACHA20_POLY1305
    were renamed to have an _OLD suffix with compatibility unsuffixed #defines
    temporarily available.
    
    Update references to include the _OLD suffixed ones. Once we've cycled through
    the few consumers of the unsuffixed names (just WebRTC and QUIC), the unsuffixed
    names can refer to the to-be-implemented standardized variant and eventually
    the draft version will be removed.
    
    (This has no effect on upstream OpenSSL compatibility as OpenSSL never defined
    these symbols to begin with. Though probably they will once standardization is
    done.)
    
    BUG=none
    
    Review URL: https://codereview.webrtc.org/1412803010
    
    Cr-Commit-Position: refs/heads/master@{#10681}

--- webrtc/base/opensslstreamadapter.cc
+++ webrtc/base/opensslstreamadapter.cc
@@ -158,10 +158,12 @@ static int kDefaultSslCipher12 =
 static int kDefaultSslEcCipher12 =
     static_cast<uint16_t>(TLS1_CK_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256);
 // Fallback cipher for DTLS 1.2 if hardware-accelerated AES-GCM is unavailable.
+// TODO(davidben): Switch to the standardized CHACHA20_POLY1305 variant when
+// available.
 static int kDefaultSslCipher12NoAesGcm =
-    static_cast<uint16_t>(TLS1_CK_ECDHE_RSA_CHACHA20_POLY1305);
+    static_cast<uint16_t>(TLS1_CK_ECDHE_RSA_CHACHA20_POLY1305_OLD);
 static int kDefaultSslEcCipher12NoAesGcm =
-    static_cast<uint16_t>(TLS1_CK_ECDHE_ECDSA_CHACHA20_POLY1305);
+    static_cast<uint16_t>(TLS1_CK_ECDHE_ECDSA_CHACHA20_POLY1305_OLD);
 #else  // !OPENSSL_IS_BORINGSSL
 // OpenSSL sorts differently than BoringSSL, so the default cipher doesn't
 // change between TLS 1.0 and TLS 1.2 with the current setup.
