a34c39e549f384585730d0247d8b912419772b74
Guo-wei Shieh <guoweis@webrtc.org>
2015-11-25 13:12:26 -0800

    GetDefaultLocalAddress should return false when the address is invalid
    BUG=
    R=pthatcher@webrtc.org
    
    Committed: https://crrev.com/67c6df6153b7b6dceb2b569daf683a498b2fc13c
    Cr-Commit-Position: refs/heads/master@{#10779}
    
    Review URL: https://codereview.webrtc.org/1471203002 .
    
    Cr-Commit-Position: refs/heads/master@{#10794}

--- webrtc/base/network_unittest.cc
+++ webrtc/base/network_unittest.cc
@@ -100,6 +100,7 @@ class NetworkTest : public testing::Test, public sigslot::has_slots<>  {
 class TestBasicNetworkManager : public BasicNetworkManager {
  public:
   using BasicNetworkManager::QueryDefaultLocalAddress;
+  using BasicNetworkManager::set_default_local_addresses;
 };
 
 // Test that the Network ctor works properly.
@@ -848,9 +849,17 @@ TEST_F(NetworkTest, TestNetworkMonitoring) {
   NetworkMonitorFactory::ReleaseFactory(factory);
 }
 
-TEST_F(NetworkTest, DefaultPrivateAddress) {
+TEST_F(NetworkTest, DefaultLocalAddress) {
   TestBasicNetworkManager manager;
   manager.StartUpdating();
+  IPAddress ip;
+
+  // GetDefaultLocalAddress should return false when not set.
+  EXPECT_FALSE(manager.GetDefaultLocalAddress(AF_INET, &ip));
+  EXPECT_FALSE(manager.GetDefaultLocalAddress(AF_INET6, &ip));
+
+  // Make sure we can query default local address when an address for such
+  // address family exists.
   std::vector<Network*> networks;
   manager.GetNetworks(&networks);
   for (auto& network : networks) {
@@ -860,6 +869,14 @@ TEST_F(NetworkTest, DefaultPrivateAddress) {
       EXPECT_TRUE(manager.QueryDefaultLocalAddress(AF_INET6) != IPAddress());
     }
   }
+
+  // GetDefaultLocalAddress should return the valid default address after set.
+  manager.set_default_local_addresses(GetLoopbackIP(AF_INET),
+                                      GetLoopbackIP(AF_INET6));
+  EXPECT_TRUE(manager.GetDefaultLocalAddress(AF_INET, &ip));
+  EXPECT_EQ(ip, GetLoopbackIP(AF_INET));
+  EXPECT_TRUE(manager.GetDefaultLocalAddress(AF_INET6, &ip));
+  EXPECT_EQ(ip, GetLoopbackIP(AF_INET6));
   manager.StopUpdating();
 }
 
