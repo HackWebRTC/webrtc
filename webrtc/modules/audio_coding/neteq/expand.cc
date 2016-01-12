e84e96e8be15fb80a1cf37ba6ef70c01cc94199d
Henrik Lundin <henrik.lundin@webrtc.org>
2016-01-12 16:36:13 +0100

    NetEq: Fix a typo in a comment
    TBR=minyue@webrtc.org
    NOTRY=true
    
    Review URL: https://codereview.webrtc.org/1578223003 .
    
    Cr-Commit-Position: refs/heads/master@{#11226}

--- webrtc/modules/audio_coding/neteq/expand.cc
+++ webrtc/modules/audio_coding/neteq/expand.cc
@@ -519,7 +519,7 @@ void Expand::AnalyzeSignal(int16_t* random_vector) {
     energy2 = WebRtcSpl_DotProductWithScale(vector2, vector2, expansion_length,
                                             correlation_scale);
     // Confirm that amplitude ratio sqrt(energy1 / energy2) is within 0.5 - 2.0,
-    // i.e., energy1 / energy1 is within 0.25 - 4.
+    // i.e., energy1 / energy2 is within 0.25 - 4.
     int16_t amplitude_ratio;
     if ((energy1 / 4 < energy2) && (energy1 > energy2 / 4)) {
       // Energy constraint fulfilled. Use both vectors and scale them
