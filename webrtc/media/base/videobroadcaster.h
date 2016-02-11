8e16e61d64c6d4df7ba7ed0257b4bd25f32d2bcf
Per <perkj@chromium.org>
2016-02-11 15:56:51 +0100

    Added empty files for VideoBroadcaster so adding them don't break Chrome.
    Cl where implementation is added: https://codereview.webrtc.org/1689923002/
    
    TBR=kjellander@webrtc.org
    
    BUG=webrtc:5426
    
    Review URL: https://codereview.webrtc.org/1688033004 .
    
    Cr-Commit-Position: refs/heads/master@{#11579}

---
+++ webrtc/media/base/videobroadcaster.h
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef WEBRTC_MEDIA_BASE_VIDEOBROADCASTER_H_
+#define WEBRTC_MEDIA_BASE_VIDEOBROADCASTER_H_
+
+// TODO(perkj): Implement.
+
+#endif  // WEBRTC_MEDIA_BASE_VIDEOBROADCASTER_H_
