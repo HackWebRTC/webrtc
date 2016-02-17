a08bb0d16384fe3c3066d0fce73952dfa5eaed79
peah <peah@webrtc.org>
2016-02-17 10:27:52 -0800

    Disabled the test EndToEndTest RestartingSendStreamPreservesRtpState due to the test being flaky.
    BUG=webrtc:5543
    
    Review URL: https://codereview.webrtc.org/1703963002
    
    Cr-Commit-Position: refs/heads/master@{#11659}

--- webrtc/video/end_to_end_tests.cc
+++ webrtc/video/end_to_end_tests.cc
@@ -3148,7 +3148,7 @@ void EndToEndTest::TestRtpStatePreservation(bool use_rtx) {
   DestroyStreams();
 }
 
-TEST_F(EndToEndTest, RestartingSendStreamPreservesRtpState) {
+TEST_F(EndToEndTest, DISABLED_RestartingSendStreamPreservesRtpState) {
   TestRtpStatePreservation(false);
 }
 
