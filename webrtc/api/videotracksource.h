11e1805a31c8c6bdbfedc7ca328dee2f0cd4398a
perkj <perkj@webrtc.org>
2016-03-07 22:03:23 +0100

    Add new empty files for VideoCapturerTrackSource and VideoTrackSource to make Chrome compile when adding implementation.
    BUG=webrtc:5621
    TBR=kjellander@webrtc.org
    
    Review URL: https://codereview.webrtc.org/1768243002 .
    
    Cr-Commit-Position: refs/heads/master@{#11889}

---
+++ webrtc/api/videotracksource.h
@@ -0,0 +1,18 @@
+/*
+ /*
+ *  Copyright 2016 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef WEBRTC_API_VIDEOTRACKSOURCE_H_
+#define WEBRTC_API_VIDEOTRACKSOURCE_H_
+
+// TODO(perkj): This file is added to prepare for splitting VideoSource
+// into two parts, VideoCapturerTrackSource that will inherit VideoTrackSource.
+
+#endif  //  WEBRTC_API_VIDEOTRACKSOURCE_H_
