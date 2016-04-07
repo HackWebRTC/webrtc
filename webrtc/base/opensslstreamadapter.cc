aad6780e5c25b1622904ef83659461706f6a25db
torbjorng <torbjorng@webrtc.org>
2016-04-07 08:55:28 -0700

    Protect some cipher suites with ifdefs for legacy openssl compatibility.
    BUG=webrtc:5714
    
    Review URL: https://codereview.webrtc.org/1851303002
    
    Cr-Commit-Position: refs/heads/master@{#12284}

--- webrtc/base/opensslstreamadapter.cc
+++ webrtc/base/opensslstreamadapter.cc
@@ -18,6 +18,9 @@
 #include <openssl/rand.h>
 #include <openssl/tls1.h>
 #include <openssl/x509v3.h>
+#ifndef OPENSSL_IS_BORINGSSL
+#include <openssl/dtls1.h>
+#endif
 
 #include <vector>
 
@@ -1138,7 +1141,9 @@ static const cipher_list OK_RSA_ciphers[] = {
 #ifdef TLS1_CK_ECDHE_RSA_WITH_AES_256_GCM_SHA256
   CDEF(ECDHE_RSA_WITH_AES_256_GCM_SHA256),
 #endif
+#ifdef TLS1_CK_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
   CDEF(ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256),
+#endif
 };
 
 static const cipher_list OK_ECDSA_ciphers[] = {
@@ -1148,7 +1153,9 @@ static const cipher_list OK_ECDSA_ciphers[] = {
 #ifdef TLS1_CK_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256
   CDEF(ECDHE_ECDSA_WITH_AES_256_GCM_SHA256),
 #endif
+#ifdef TLS1_CK_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
   CDEF(ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256),
+#endif
 };
 #undef CDEF
 
