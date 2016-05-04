fac23f00efca1e8f9fc58294dfc9d6ffcc83f344
Alex Glaznev <glaznev@google.com>
2016-05-04 12:58:22 -0700

    Tune QP threshold for HW codecs.
    Lower down bad thresholds a bit to avoid staying
    at 720p resolution at 300 - 500 kbps.
    
    R=wzh@webrtc.org
    
    Review URL: https://codereview.webrtc.org/1954433002 .
    
    Cr-Commit-Position: refs/heads/master@{#12633}

--- webrtc/api/java/jni/androidmediaencoder_jni.cc
+++ webrtc/api/java/jni/androidmediaencoder_jni.cc
@@ -392,15 +392,15 @@ int32_t MediaCodecVideoEncoder::InitEncode(
       // always = 127. Note that in SW, QP is that of the user-level range [0,
       // 63].
       const int kLowQpThreshold = 29;
-      const int kBadQpThreshold = 100;
+      const int kBadQpThreshold = 90;
       quality_scaler_.Init(kLowQpThreshold, kBadQpThreshold,
                            codec_settings->startBitrate, codec_settings->width,
                            codec_settings->height,
                            codec_settings->maxFramerate);
     } else if (codecType_ == kVideoCodecH264) {
       // H264 QP is in the range [0, 51].
-      const int kLowQpThreshold = 24;
-      const int kBadQpThreshold = 39;
+      const int kLowQpThreshold = 22;
+      const int kBadQpThreshold = 35;
       quality_scaler_.Init(kLowQpThreshold, kBadQpThreshold,
                            codec_settings->startBitrate, codec_settings->width,
                            codec_settings->height,
