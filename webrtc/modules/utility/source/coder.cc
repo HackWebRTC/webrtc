73987c9932b2b6a49bda467a448d428119e933f5
kwiberg <kwiberg@webrtc.org>
2016-05-04 05:12:19 -0700

    Run "git cl format --full" on a pair of files with ancient formatting
    Review-Url: https://codereview.webrtc.org/1946873003
    Cr-Commit-Position: refs/heads/master@{#12625}

--- webrtc/modules/utility/source/coder.cc
+++ webrtc/modules/utility/source/coder.cc
@@ -13,21 +13,19 @@
 #include "webrtc/modules/utility/source/coder.h"
 
 namespace webrtc {
+
 AudioCoder::AudioCoder(uint32_t instanceID)
     : _acm(AudioCodingModule::Create(instanceID)),
       _receiveCodec(),
       _encodeTimestamp(0),
       _encodedData(NULL),
       _encodedLengthInBytes(0),
-      _decodeTimestamp(0)
-{
-    _acm->InitializeReceiver();
-    _acm->RegisterTransportCallback(this);
+      _decodeTimestamp(0) {
+  _acm->InitializeReceiver();
+  _acm->RegisterTransportCallback(this);
 }
 
-AudioCoder::~AudioCoder()
-{
-}
+AudioCoder::~AudioCoder() {}
 
 int32_t AudioCoder::SetEncodeCodec(const CodecInst& codecInst) {
   const bool success = codec_manager_.RegisterEncoder(codecInst) &&
@@ -46,63 +44,54 @@ int32_t AudioCoder::SetDecodeCodec(const CodecInst& codecInst) {
 
 int32_t AudioCoder::Decode(AudioFrame& decodedAudio,
                            uint32_t sampFreqHz,
-                           const int8_t*  incomingPayload,
-                           size_t  payloadLength)
-{
-    if (payloadLength > 0)
-    {
-        const uint8_t payloadType = _receiveCodec.pltype;
-        _decodeTimestamp += _receiveCodec.pacsize;
-        if(_acm->IncomingPayload((const uint8_t*) incomingPayload,
-                                 payloadLength,
-                                 payloadType,
-                                 _decodeTimestamp) == -1)
-        {
-            return -1;
-        }
+                           const int8_t* incomingPayload,
+                           size_t payloadLength) {
+  if (payloadLength > 0) {
+    const uint8_t payloadType = _receiveCodec.pltype;
+    _decodeTimestamp += _receiveCodec.pacsize;
+    if (_acm->IncomingPayload((const uint8_t*)incomingPayload, payloadLength,
+                              payloadType, _decodeTimestamp) == -1) {
+      return -1;
     }
-    return _acm->PlayoutData10Ms((uint16_t)sampFreqHz, &decodedAudio);
+  }
+  return _acm->PlayoutData10Ms((uint16_t)sampFreqHz, &decodedAudio);
 }
 
 int32_t AudioCoder::PlayoutData(AudioFrame& decodedAudio,
-                                uint16_t& sampFreqHz)
-{
-    return _acm->PlayoutData10Ms(sampFreqHz, &decodedAudio);
+                                uint16_t& sampFreqHz) {
+  return _acm->PlayoutData10Ms(sampFreqHz, &decodedAudio);
 }
 
 int32_t AudioCoder::Encode(const AudioFrame& audio,
                            int8_t* encodedData,
-                           size_t& encodedLengthInBytes)
-{
-    // Fake a timestamp in case audio doesn't contain a correct timestamp.
-    // Make a local copy of the audio frame since audio is const
-    AudioFrame audioFrame;
-    audioFrame.CopyFrom(audio);
-    audioFrame.timestamp_ = _encodeTimestamp;
-    _encodeTimestamp += static_cast<uint32_t>(audioFrame.samples_per_channel_);
+                           size_t& encodedLengthInBytes) {
+  // Fake a timestamp in case audio doesn't contain a correct timestamp.
+  // Make a local copy of the audio frame since audio is const
+  AudioFrame audioFrame;
+  audioFrame.CopyFrom(audio);
+  audioFrame.timestamp_ = _encodeTimestamp;
+  _encodeTimestamp += static_cast<uint32_t>(audioFrame.samples_per_channel_);
 
-    // For any codec with a frame size that is longer than 10 ms the encoded
-    // length in bytes should be zero until a a full frame has been encoded.
-    _encodedLengthInBytes = 0;
-    if(_acm->Add10MsData((AudioFrame&)audioFrame) == -1)
-    {
-        return -1;
-    }
-    _encodedData = encodedData;
-    encodedLengthInBytes = _encodedLengthInBytes;
-    return 0;
+  // For any codec with a frame size that is longer than 10 ms the encoded
+  // length in bytes should be zero until a a full frame has been encoded.
+  _encodedLengthInBytes = 0;
+  if (_acm->Add10MsData((AudioFrame&)audioFrame) == -1) {
+    return -1;
+  }
+  _encodedData = encodedData;
+  encodedLengthInBytes = _encodedLengthInBytes;
+  return 0;
 }
 
-int32_t AudioCoder::SendData(
-    FrameType /* frameType */,
-    uint8_t   /* payloadType */,
-    uint32_t  /* timeStamp */,
-    const uint8_t*  payloadData,
-    size_t  payloadSize,
-    const RTPFragmentationHeader* /* fragmentation*/)
-{
-    memcpy(_encodedData,payloadData,sizeof(uint8_t) * payloadSize);
-    _encodedLengthInBytes = payloadSize;
-    return 0;
+int32_t AudioCoder::SendData(FrameType /* frameType */,
+                             uint8_t /* payloadType */,
+                             uint32_t /* timeStamp */,
+                             const uint8_t* payloadData,
+                             size_t payloadSize,
+                             const RTPFragmentationHeader* /* fragmentation*/) {
+  memcpy(_encodedData, payloadData, sizeof(uint8_t) * payloadSize);
+  _encodedLengthInBytes = payloadSize;
+  return 0;
 }
+
 }  // namespace webrtc
--- webrtc/modules/utility/source/coder.h
+++ webrtc/modules/utility/source/coder.h
@@ -22,45 +22,47 @@
 namespace webrtc {
 class AudioFrame;
 
-class AudioCoder : public AudioPacketizationCallback
-{
-public:
-    AudioCoder(uint32_t instanceID);
-    ~AudioCoder();
+class AudioCoder : public AudioPacketizationCallback {
+ public:
+  AudioCoder(uint32_t instanceID);
+  ~AudioCoder();
 
-    int32_t SetEncodeCodec(const CodecInst& codecInst);
+  int32_t SetEncodeCodec(const CodecInst& codecInst);
 
-    int32_t SetDecodeCodec(const CodecInst& codecInst);
+  int32_t SetDecodeCodec(const CodecInst& codecInst);
 
-    int32_t Decode(AudioFrame& decodedAudio, uint32_t sampFreqHz,
-                   const int8_t* incomingPayload, size_t payloadLength);
+  int32_t Decode(AudioFrame& decodedAudio,
+                 uint32_t sampFreqHz,
+                 const int8_t* incomingPayload,
+                 size_t payloadLength);
 
-    int32_t PlayoutData(AudioFrame& decodedAudio, uint16_t& sampFreqHz);
+  int32_t PlayoutData(AudioFrame& decodedAudio, uint16_t& sampFreqHz);
 
-    int32_t Encode(const AudioFrame& audio, int8_t* encodedData,
-                   size_t& encodedLengthInBytes);
+  int32_t Encode(const AudioFrame& audio,
+                 int8_t* encodedData,
+                 size_t& encodedLengthInBytes);
 
-protected:
- int32_t SendData(FrameType frameType,
-                  uint8_t payloadType,
-                  uint32_t timeStamp,
-                  const uint8_t* payloadData,
-                  size_t payloadSize,
-                  const RTPFragmentationHeader* fragmentation) override;
+ protected:
+  int32_t SendData(FrameType frameType,
+                   uint8_t payloadType,
+                   uint32_t timeStamp,
+                   const uint8_t* payloadData,
+                   size_t payloadSize,
+                   const RTPFragmentationHeader* fragmentation) override;
 
-private:
- std::unique_ptr<AudioCodingModule> _acm;
- acm2::CodecManager codec_manager_;
- acm2::RentACodec rent_a_codec_;
+ private:
+  std::unique_ptr<AudioCodingModule> _acm;
+  acm2::CodecManager codec_manager_;
+  acm2::RentACodec rent_a_codec_;
 
-    CodecInst _receiveCodec;
+  CodecInst _receiveCodec;
 
-    uint32_t _encodeTimestamp;
-    int8_t*  _encodedData;
-    size_t _encodedLengthInBytes;
+  uint32_t _encodeTimestamp;
+  int8_t* _encodedData;
+  size_t _encodedLengthInBytes;
 
-    uint32_t _decodeTimestamp;
+  uint32_t _decodeTimestamp;
 };
 }  // namespace webrtc
 
-#endif // WEBRTC_MODULES_UTILITY_SOURCE_CODER_H_
+#endif  // WEBRTC_MODULES_UTILITY_SOURCE_CODER_H_
