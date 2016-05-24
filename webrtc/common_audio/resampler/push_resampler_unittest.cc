fb98b9edb445244cf5ad7fa7a69a5a95da854fd7
tommi <tommi@webrtc.org>
2016-05-24 06:44:27 -0700

    Revert of Adding a some checks and switching out a few assert for RTC_[D]CHECK. (patchset #6 id:100001 of https://codereview.webrtc.org/2007563002/ )
    Reason for revert:
    Reverting temporarily.  Need to fix tests downstream that pass invalid arguments.
    
    Original issue's description:
    > Adding a some checks and switching out a few assert for RTC_[D]CHECK.
    > These changes are around use of AudioFrame.data_ to help us catch issues earlier since assert() is left out in release builds, including builds with DCHECK enabled.  I've also added a few full-on CHECKs to avoid reading past buffer boundaries or continuing on in a failed state.
    >
    > BUG=chromium:613482
    > NOTRY=true
    > (using notry due to offline android_arm64_rel bot)
    >
    > Committed: https://crrev.com/d36df89d40bde3c62ee5cbff841933e50b3c007b
    > Cr-Commit-Position: refs/heads/master@{#12870}
    
    TBR=henrik.lundin@webrtc.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:613482
    
    Review-Url: https://codereview.webrtc.org/2006243002
    Cr-Commit-Position: refs/heads/master@{#12874}

--- webrtc/common_audio/resampler/push_resampler_unittest.cc
+++ webrtc/common_audio/resampler/push_resampler_unittest.cc
@@ -9,7 +9,6 @@
  */
 
 #include "testing/gtest/include/gtest/gtest.h"
-#include "webrtc/base/checks.h"  // force defintion of RTC_DCHECK_IS_ON
 #include "webrtc/common_audio/resampler/include/push_resampler.h"
 
 // Quality testing of PushResampler is handled through output_mixer_unittest.cc.
@@ -18,32 +17,12 @@ namespace webrtc {
 
 TEST(PushResamplerTest, VerifiesInputParameters) {
   PushResampler<int16_t> resampler;
+  EXPECT_EQ(-1, resampler.InitializeIfNeeded(-1, 16000, 1));
+  EXPECT_EQ(-1, resampler.InitializeIfNeeded(16000, -1, 1));
+  EXPECT_EQ(-1, resampler.InitializeIfNeeded(16000, 16000, 0));
+  EXPECT_EQ(-1, resampler.InitializeIfNeeded(16000, 16000, 3));
   EXPECT_EQ(0, resampler.InitializeIfNeeded(16000, 16000, 1));
   EXPECT_EQ(0, resampler.InitializeIfNeeded(16000, 16000, 2));
 }
-
-#if RTC_DCHECK_IS_ON && GTEST_HAS_DEATH_TEST && !defined(WEBRTC_ANDROID)
-TEST(PushResamplerTest, VerifiesBadInputParameters1) {
-  PushResampler<int16_t> resampler;
-  EXPECT_DEATH(resampler.InitializeIfNeeded(-1, 16000, 1),
-               "src_sample_rate_hz");
-}
-
-TEST(PushResamplerTest, VerifiesBadInputParameters2) {
-  PushResampler<int16_t> resampler;
-  EXPECT_DEATH(resampler.InitializeIfNeeded(16000, -1, 1),
-               "dst_sample_rate_hz");
-}
-
-TEST(PushResamplerTest, VerifiesBadInputParameters3) {
-  PushResampler<int16_t> resampler;
-  EXPECT_DEATH(resampler.InitializeIfNeeded(16000, 16000, 0), "num_channels");
-}
-
-TEST(PushResamplerTest, VerifiesBadInputParameters4) {
-  PushResampler<int16_t> resampler;
-  EXPECT_DEATH(resampler.InitializeIfNeeded(16000, 16000, 3), "num_channels");
-}
-#endif
 
 }  // namespace webrtc
--- webrtc/voice_engine/utility.cc
+++ webrtc/voice_engine/utility.cc
@@ -10,7 +10,6 @@
 
 #include "webrtc/voice_engine/utility.h"
 
-#include "webrtc/base/checks.h"
 #include "webrtc/base/logging.h"
 #include "webrtc/common_audio/resampler/include/push_resampler.h"
 #include "webrtc/common_audio/signal_processing/include/signal_processing_library.h"
@@ -53,18 +52,21 @@ void RemixAndResample(const int16_t* src_data,
 
   if (resampler->InitializeIfNeeded(sample_rate_hz, dst_frame->sample_rate_hz_,
                                     audio_ptr_num_channels) == -1) {
-    FATAL() << "InitializeIfNeeded failed: sample_rate_hz = " << sample_rate_hz
-            << ", dst_frame->sample_rate_hz_ = " << dst_frame->sample_rate_hz_
-            << ", audio_ptr_num_channels = " << audio_ptr_num_channels;
+    LOG(LS_ERROR) << "InitializeIfNeeded failed: sample_rate_hz = "
+                  << sample_rate_hz << ", dst_frame->sample_rate_hz_ = "
+                  << dst_frame->sample_rate_hz_
+                  << ", audio_ptr_num_channels = " << audio_ptr_num_channels;
+    assert(false);
   }
 
   const size_t src_length = samples_per_channel * audio_ptr_num_channels;
   int out_length = resampler->Resample(audio_ptr, src_length, dst_frame->data_,
                                        AudioFrame::kMaxDataSizeSamples);
   if (out_length == -1) {
-    FATAL() << "Resample failed: audio_ptr = " << audio_ptr
-            << ", src_length = " << src_length
-            << ", dst_frame->data_ = " << dst_frame->data_;
+    LOG(LS_ERROR) << "Resample failed: audio_ptr = " << audio_ptr
+                  << ", src_length = " << src_length
+                  << ", dst_frame->data_ = " << dst_frame->data_;
+    assert(false);
   }
   dst_frame->samples_per_channel_ = out_length / audio_ptr_num_channels;
 
@@ -82,10 +84,8 @@ void MixWithSat(int16_t target[],
                 const int16_t source[],
                 size_t source_channel,
                 size_t source_len) {
-  RTC_DCHECK_GE(target_channel, 1u);
-  RTC_DCHECK_LE(target_channel, 2u);
-  RTC_DCHECK_GE(source_channel, 1u);
-  RTC_DCHECK_LE(source_channel, 2u);
+  assert(target_channel == 1 || target_channel == 2);
+  assert(source_channel == 1 || source_channel == 2);
 
   if (target_channel == 2 && source_channel == 1) {
     // Convert source from mono to stereo.
