18c65a448f9bcdd7bac7dfec7f78791d2e77df10
Danil Chapovalov <danilchap@webrtc.org>
2016-06-29 15:29:34 +0200

    Expanded error message for unexpected packet in the flaky test EndToEndTest.DecodesRetransmittedFrame*
    BUG=webrtc:5540
    R=pbos@webrtc.org, pbos
    
    Review URL: https://codereview.webrtc.org/2100793002 .
    
    Cr-Commit-Position: refs/heads/master@{#13323}

--- webrtc/video/end_to_end_tests.cc
+++ webrtc/video/end_to_end_tests.cc
@@ -790,13 +790,19 @@ void EndToEndTest::DecodesRetransmittedFrame(bool enable_rtx, bool enable_red) {
       }
 
       EXPECT_EQ(kVideoSendSsrcs[0], header.ssrc)
-          << "Payload type " << static_cast<int>(header.payloadType)
-          << " not expected.";
+          << "Unexpected packet length " << length
+          << ", header_length " << header.headerLength
+          << ", padding_length " << header.paddingLength
+          << ", timestamp " << header.timestamp
+          << ", expected timestamp " << retransmitted_timestamp_
+          << ", payload type " << static_cast<int>(header.payloadType);
       EXPECT_EQ(payload_type_, header.payloadType);
 
       // Found the final packet of the frame to inflict loss to, drop this and
       // expect a retransmission.
       if (header.markerBit && ++marker_bits_observed_ == kDroppedFrameNumber) {
+        // This should be the only dropped packet.
+        EXPECT_EQ(0u, retransmitted_timestamp_);
         retransmitted_timestamp_ = header.timestamp;
         return DROP_PACKET;
       }
