025aa94ccb85e4c6fe20a3fecdac5d27ec9ba3da
sprang <sprang@webrtc.org>
2016-07-12 03:08:45 -0700

    Revert of Adds data logging in native AudioDeviceBuffer class (patchset #10 id:180001 of https://codereview.webrtc.org/2132613002/ )
    Reason for revert:
    Seems to break things upstream.
    
    Original issue's description:
    > Adds data logging in native AudioDeviceBuffer class.
    >
    > Goal is to provide periodic logging of most essential audio parameters
    > for playout and recording sides. It will allow us to track if the native audio layer is working as intended.
    >
    > BUG=NONE
    >
    > Committed: https://crrev.com/348e411dd27e6dbe9b84b27ce46e9b7c657c1eae
    > Cr-Commit-Position: refs/heads/master@{#13440}
    
    TBR=stefan@webrtc.org,henrika@webrtc.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=NONE
    
    Review-Url: https://codereview.webrtc.org/2139233002
    Cr-Commit-Position: refs/heads/master@{#13441}

--- webrtc/modules/audio_device/audio_device_buffer.h
+++ webrtc/modules/audio_device/audio_device_buffer.h
@@ -8,12 +8,9 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef WEBRTC_MODULES_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H_
-#define WEBRTC_MODULES_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H_
+#ifndef WEBRTC_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H
+#define WEBRTC_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H
 
-#include "webrtc/base/criticalsection.h"
-#include "webrtc/base/task_queue.h"
-#include "webrtc/base/thread_checker.h"
 #include "webrtc/modules/audio_device/include/audio_device.h"
 #include "webrtc/system_wrappers/include/file_wrapper.h"
 #include "webrtc/typedefs.h"
@@ -66,35 +63,10 @@ class AudioDeviceBuffer {
   int32_t SetTypingStatus(bool typingStatus);
 
  private:
-  // Posts the first delayed task in the task queue and starts the periodic
-  // timer.
-  void StartTimer();
-
-  // Called periodically on the internal thread created by the TaskQueue.
-  void LogStats();
-
-  // Updates counters in each play/record callback but does it on the task
-  // queue to ensure that they can be read by LogStats() without any locks since
-  // each task is serialized by the task queue.
-  void UpdateRecStats(size_t num_samples);
-  void UpdatePlayStats(size_t num_samples);
-
-  // Ensures that methods are called on the same thread as the thread that
-  // creates this object.
-  rtc::ThreadChecker thread_checker_;
-
-  rtc::CriticalSection _critSect;
-  rtc::CriticalSection _critSectCb;
+  CriticalSectionWrapper& _critSect;
+  CriticalSectionWrapper& _critSectCb;
 
   AudioTransport* _ptrCbAudioTransport;
-
-  // Task queue used to invoke LogStats() periodically. Tasks are executed on a
-  // worker thread but it does not necessarily have to be the same thread for
-  // each task.
-  rtc::TaskQueue task_queue_;
-
-  // Ensures that the timer is only started once.
-  bool timer_has_started_;
 
   uint32_t _recSampleRate;
   uint32_t _playSampleRate;
@@ -135,40 +107,8 @@ class AudioDeviceBuffer {
   int _recDelayMS;
   int _clockDrift;
   int high_delay_counter_;
-
-  // Counts number of times LogStats() has been called.
-  size_t num_stat_reports_;
-
-  // Total number of recording callbacks where the source provides 10ms audio
-  // data each time.
-  uint64_t rec_callbacks_;
-
-  // Total number of recording callbacks stored at the last timer task.
-  uint64_t last_rec_callbacks_;
-
-  // Total number of playback callbacks where the sink asks for 10ms audio
-  // data each time.
-  uint64_t play_callbacks_;
-
-  // Total number of playout callbacks stored at the last timer task.
-  uint64_t last_play_callbacks_;
-
-  // Total number of recorded audio samples.
-  uint64_t rec_samples_;
-
-  // Total number of recorded samples stored at the previous timer task.
-  uint64_t last_rec_samples_;
-
-  // Total number of played audio samples.
-  uint64_t play_samples_;
-
-  // Total number of played samples stored at the previous timer task.
-  uint64_t last_play_samples_;
-
-  // Time stamp of last stat report.
-  uint64_t last_log_stat_time_;
 };
 
 }  // namespace webrtc
 
-#endif  // WEBRTC_MODULES_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H_
+#endif  // WEBRTC_AUDIO_DEVICE_AUDIO_DEVICE_BUFFER_H
