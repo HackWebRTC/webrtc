70cfc14af67ce2f85143fefa9ff28703445d0f43
Danil Chapovalov <danilchap@webrtc.org>
2016-07-29 19:14:56 +0200

    Adds move assignment operator to scoped_refptr Implementation borrowed from chromium.
    BUG=webrtc:5556
    R=pthatcher@webrtc.org
    
    Review URL: https://codereview.webrtc.org/2193443002 .
    
    Cr-Commit-Position: refs/heads/master@{#13577}

--- webrtc/base/scoped_ref_ptr.h
+++ webrtc/base/scoped_ref_ptr.h
@@ -63,7 +63,7 @@
 #ifndef WEBRTC_BASE_SCOPED_REF_PTR_H_
 #define WEBRTC_BASE_SCOPED_REF_PTR_H_
 
-#include <stddef.h>
+#include <memory>
 
 namespace rtc {
 
@@ -132,6 +132,17 @@ class scoped_refptr {
   template <typename U>
   scoped_refptr<T>& operator=(const scoped_refptr<U>& r) {
     return *this = r.get();
+  }
+
+  scoped_refptr<T>& operator=(scoped_refptr<T>&& r) {
+    scoped_refptr<T>(std::move(r)).swap(*this);
+    return *this;
+  }
+
+  template <typename U>
+  scoped_refptr<T>& operator=(scoped_refptr<U>&& r) {
+    scoped_refptr<T>(std::move(r)).swap(*this);
+    return *this;
   }
 
   void swap(T** pp) {
