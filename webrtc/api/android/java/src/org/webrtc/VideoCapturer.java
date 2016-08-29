1b365a8db070f9cdcbf35ec871f758dcd909e51d
sakal <sakal@webrtc.org>
2016-08-29 02:20:21 -0700

    Remove the old AndroidVideoCapturer stack code.
    This code is no longer needed. Apps should be using the new API introduced here: https://codereview.webrtc.org/2127893002/
    
    Review-Url: https://codereview.webrtc.org/2235893003
    Cr-Commit-Position: refs/heads/master@{#13950}

--- webrtc/api/android/java/src/org/webrtc/VideoCapturer.java
+++ webrtc/api/android/java/src/org/webrtc/VideoCapturer.java
@@ -41,53 +41,6 @@ public interface VideoCapturer {
 
   // An implementation of CapturerObserver that forwards all calls from
   // Java to the C layer.
-  static class NativeObserver implements CapturerObserver {
-    private final long nativeCapturer;
-
-    public NativeObserver(long nativeCapturer) {
-      this.nativeCapturer = nativeCapturer;
-    }
-
-    @Override
-    public void onCapturerStarted(boolean success) {
-      nativeCapturerStarted(nativeCapturer, success);
-    }
-
-    @Override
-    public void onCapturerStopped() {}
-
-    @Override
-    public void onByteBufferFrameCaptured(byte[] data, int width, int height,
-        int rotation, long timeStamp) {
-      nativeOnByteBufferFrameCaptured(nativeCapturer, data, data.length, width, height, rotation,
-          timeStamp);
-    }
-
-    @Override
-    public void onTextureFrameCaptured(
-        int width, int height, int oesTextureId, float[] transformMatrix, int rotation,
-        long timestamp) {
-      nativeOnTextureFrameCaptured(nativeCapturer, width, height, oesTextureId, transformMatrix,
-          rotation, timestamp);
-    }
-
-    @Override
-    public void onOutputFormatRequest(int width, int height, int framerate) {
-      nativeOnOutputFormatRequest(nativeCapturer, width, height, framerate);
-    }
-
-    private native void nativeCapturerStarted(long nativeCapturer,
-        boolean success);
-    private native void nativeOnByteBufferFrameCaptured(long nativeCapturer,
-        byte[] data, int length, int width, int height, int rotation, long timeStamp);
-    private native void nativeOnTextureFrameCaptured(long nativeCapturer, int width, int height,
-        int oesTextureId, float[] transformMatrix, int rotation, long timestamp);
-    private native void nativeOnOutputFormatRequest(long nativeCapturer,
-        int width, int height, int framerate);
-  }
-
-  // An implementation of CapturerObserver that forwards all calls from
-  // Java to the C layer.
   static class AndroidVideoTrackSourceObserver implements CapturerObserver {
     // Pointer to VideoTrackSourceProxy proxying AndroidVideoTrackSource.
     private final long nativeSource;
