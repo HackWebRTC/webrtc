180e4525ca7c9a23602cdf37a8756df7d23e7143
danilchap <danilchap@webrtc.org>
2016-09-12 01:40:40 -0700

    Revert of Make rtcp parsing implementation private in RtcpReceiver (patchset #1 id:1 of https://codereview.webrtc.org/2320603002/ )
    Reason for revert:
    Breaks fuzzer compilation.
    
    Original issue's description:
    > Make rtcp parsing implementation private in RtcpReceiver:
    > Function just for Parse and for Callbacks moved to private section.
    > All handles moved from protected to private section.
    >
    > BUG=webrtc:5260
    > R=sprang@webrtc.org
    >
    > Committed: https://crrev.com/faf708e238c7b43a732fbebf79ac9298b4b95a95
    > Cr-Commit-Position: refs/heads/master@{#14181}
    
    TBR=sprang@webrtc.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=webrtc:5260
    
    Review-Url: https://codereview.webrtc.org/2332673003
    Cr-Commit-Position: refs/heads/master@{#14182}

--- webrtc/modules/rtp_rtcp/source/rtcp_receiver.h
+++ webrtc/modules/rtp_rtcp/source/rtcp_receiver.h
@@ -60,6 +60,13 @@ class RTCPReceiver {
   void SetRemoteSSRC(uint32_t ssrc);
   uint32_t RemoteSSRC() const;
 
+  int32_t IncomingRTCPPacket(
+      RTCPHelp::RTCPPacketInformation& rtcpPacketInformation,
+      RTCPUtility::RTCPParserV2* rtcpParser);
+
+  void TriggerCallbacksFromRTCPPacket(
+      RTCPHelp::RTCPPacketInformation& rtcpPacketInformation);
+
   // get received cname
   int32_t CNAME(uint32_t remoteSSRC, char cName[RTCP_CNAME_SIZE]) const;
 
@@ -108,21 +115,7 @@ class RTCPReceiver {
   void RegisterRtcpStatisticsCallback(RtcpStatisticsCallback* callback);
   RtcpStatisticsCallback* GetRtcpStatisticsCallback();
 
- private:
-  using ReceivedInfoMap = std::map<uint32_t, RTCPHelp::RTCPReceiveInformation*>;
-  // RTCP report block information mapped by remote SSRC.
-  using ReportBlockInfoMap =
-      std::map<uint32_t, RTCPHelp::RTCPReportBlockInformation*>;
-  // RTCP report block information map mapped by source SSRC.
-  using ReportBlockMap = std::map<uint32_t, ReportBlockInfoMap>;
-
-  int32_t IncomingRTCPPacket(
-      RTCPHelp::RTCPPacketInformation& rtcpPacketInformation,
-      RTCPUtility::RTCPParserV2* rtcpParser);
-
-  void TriggerCallbacksFromRTCPPacket(
-      RTCPHelp::RTCPPacketInformation& rtcpPacketInformation);
-
+ protected:
   RTCPUtility::RTCPCnameInformation* CreateCnameInformation(
       uint32_t remoteSSRC);
   RTCPUtility::RTCPCnameInformation* GetCnameInformation(
@@ -259,6 +252,14 @@ class RTCPReceiver {
       RTCPUtility::RTCPParserV2* rtcp_parser,
       RTCPHelp::RTCPPacketInformation* rtcp_packet_information)
       EXCLUSIVE_LOCKS_REQUIRED(_criticalSectionRTCPReceiver);
+
+ private:
+  typedef std::map<uint32_t, RTCPHelp::RTCPReceiveInformation*> ReceivedInfoMap;
+  // RTCP report block information mapped by remote SSRC.
+  typedef std::map<uint32_t, RTCPHelp::RTCPReportBlockInformation*>
+      ReportBlockInfoMap;
+  // RTCP report block information map mapped by source SSRC.
+  typedef std::map<uint32_t, ReportBlockInfoMap> ReportBlockMap;
 
   RTCPHelp::RTCPReportBlockInformation* CreateOrGetReportBlockInformation(
       uint32_t remote_ssrc,
