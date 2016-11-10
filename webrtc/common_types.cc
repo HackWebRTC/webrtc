792738640234d81c916ac4458ac72286cb2548a4
hta <hta@webrtc.org>
2016-11-10 03:26:38 -0800

    Declare VideoCodec.codec_specific_info private
    This completes the privatization of the codec specific
    information in VideoCodec.
    
    BUG=webrtc:6603
    
    Review-Url: https://codereview.webrtc.org/2452963002
    Cr-Commit-Position: refs/heads/master@{#15013}

--- webrtc/common_types.cc
+++ webrtc/common_types.cc
@@ -59,36 +59,36 @@ VideoCodec::VideoCodec()
       simulcastStream(),
       spatialLayers(),
       mode(kRealtimeVideo),
-      codecSpecific() {}
+      codec_specific_() {}
 
 VideoCodecVP8* VideoCodec::VP8() {
   RTC_DCHECK_EQ(codecType, kVideoCodecVP8);
-  return &codecSpecific.VP8;
+  return &codec_specific_.VP8;
 }
 
 const VideoCodecVP8& VideoCodec::VP8() const {
   RTC_DCHECK_EQ(codecType, kVideoCodecVP8);
-  return codecSpecific.VP8;
+  return codec_specific_.VP8;
 }
 
 VideoCodecVP9* VideoCodec::VP9() {
   RTC_DCHECK_EQ(codecType, kVideoCodecVP9);
-  return &codecSpecific.VP9;
+  return &codec_specific_.VP9;
 }
 
 const VideoCodecVP9& VideoCodec::VP9() const {
   RTC_DCHECK_EQ(codecType, kVideoCodecVP9);
-  return codecSpecific.VP9;
+  return codec_specific_.VP9;
 }
 
 VideoCodecH264* VideoCodec::H264() {
   RTC_DCHECK_EQ(codecType, kVideoCodecH264);
-  return &codecSpecific.H264;
+  return &codec_specific_.H264;
 }
 
 const VideoCodecH264& VideoCodec::H264() const {
   RTC_DCHECK_EQ(codecType, kVideoCodecH264);
-  return codecSpecific.H264;
+  return codec_specific_.H264;
 }
 
 }  // namespace webrtc
