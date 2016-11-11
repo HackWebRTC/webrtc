c63fb3a0d3b9b2081a6a5e6e238d8ee595dca2a2
hta <hta@webrtc.org>
2016-11-11 05:44:49 -0800

    Reland of Declare VideoCodec.codec_specific_info private (patchset #1 id:1 of https://codereview.webrtc.org/2491933002/ )
    Reason for revert:
    Relanding, now that downstream issues have been fixed.
    
    Original issue's description:
    > Revert of Declare VideoCodec.codec_specific_info private (patchset #5 id:80001 of https://codereview.webrtc.org/2452963002/ )
    >
    > Reason for revert:
    > Broke a google3 build
    >
    > Original issue's description:
    > > Declare VideoCodec.codec_specific_info private
    > >
    > > This completes the privatization of the codec specific
    > > information in VideoCodec.
    > >
    > > BUG=webrtc:6603
    > >
    > > Committed: https://crrev.com/792738640234d81c916ac4458ac72286cb2548a4
    > > Cr-Commit-Position: refs/heads/master@{#15013}
    >
    > TBR=tommi@chromium.org,magjed@chromium.org,tommi@webrtc.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=webrtc:6603
    >
    > Committed: https://crrev.com/7fe6db91d99cf6d43874651bcca56092cf869e2f
    > Cr-Commit-Position: refs/heads/master@{#15027}
    
    TBR=tommi@chromium.org,magjed@chromium.org,tommi@webrtc.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=webrtc:6603
    
    Review-Url: https://codereview.webrtc.org/2494683006
    Cr-Commit-Position: refs/heads/master@{#15041}

--- webrtc/common_types.h
+++ webrtc/common_types.h
@@ -641,11 +641,10 @@ class VideoCodec {
   VideoCodecH264* H264();
   const VideoCodecH264& H264() const;
 
-  // This variable will be declared private and renamed to codec_specific_
-  // once Chromium is not accessing it.
+ private:
   // TODO(hta): Consider replacing the union with a pointer type.
   // This will allow removing the VideoCodec* types from this file.
-  VideoCodecUnion codecSpecific;
+  VideoCodecUnion codec_specific_;
 };
 
 // Bandwidth over-use detector options.  These are used to drive
