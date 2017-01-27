b621c3f5e4dbaf979872657740a1f463212a7c55
sakal <sakal@webrtc.org>
2017-01-27 05:14:59 -0800

    Move Android tests under sdk/android.
    BUG=None
    
    Review-Url: https://codereview.webrtc.org/2657813003
    Cr-Commit-Position: refs/heads/master@{#16323}

--- webrtc/sdk/android/tests/src/org/webrtc/CameraEnumerationTest.java
+++ webrtc/sdk/android/tests/src/org/webrtc/CameraEnumerationTest.java
@@ -13,14 +13,12 @@ package org.webrtc;
 import static org.junit.Assert.assertEquals;
 import static org.webrtc.CameraEnumerationAndroid.getClosestSupportedFramerateRange;
 
-import org.webrtc.CameraEnumerationAndroid.CaptureFormat.FramerateRange;
-
+import java.util.Arrays;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
-
-import java.util.Arrays;
+import org.webrtc.CameraEnumerationAndroid.CaptureFormat.FramerateRange;
 
 /**
  * Tests for CameraEnumerationAndroid.
@@ -31,20 +29,20 @@ public class CameraEnumerationTest {
   @Test
   public void testGetClosestSupportedFramerateRange() {
     assertEquals(new FramerateRange(10000, 30000),
-        getClosestSupportedFramerateRange(Arrays.asList(new FramerateRange(10000, 30000),
-                                              new FramerateRange(30000, 30000)),
-                     30 /* requestedFps */));
+        getClosestSupportedFramerateRange(
+            Arrays.asList(new FramerateRange(10000, 30000), new FramerateRange(30000, 30000)),
+            30 /* requestedFps */));
 
     assertEquals(new FramerateRange(10000, 20000),
         getClosestSupportedFramerateRange(
-                     Arrays.asList(new FramerateRange(0, 30000), new FramerateRange(10000, 20000),
-                         new FramerateRange(14000, 16000), new FramerateRange(15000, 15000)),
-                     15 /* requestedFps */));
+            Arrays.asList(new FramerateRange(0, 30000), new FramerateRange(10000, 20000),
+                new FramerateRange(14000, 16000), new FramerateRange(15000, 15000)),
+            15 /* requestedFps */));
 
     assertEquals(new FramerateRange(10000, 20000),
         getClosestSupportedFramerateRange(
-                     Arrays.asList(new FramerateRange(15000, 15000),
-                         new FramerateRange(10000, 20000), new FramerateRange(10000, 30000)),
-                     10 /* requestedFps */));
+            Arrays.asList(new FramerateRange(15000, 15000), new FramerateRange(10000, 20000),
+                new FramerateRange(10000, 30000)),
+            10 /* requestedFps */));
   }
 }
