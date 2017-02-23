4b1bf6c2f02e4be5a81965b125e7472a7dc8dff2
deadbeef <deadbeef@webrtc.org>
2017-02-22 23:45:38 -0800

    Adding placeholder ortc_unittests target.
    This will allow the trybots to be updated to start running this new test
    executable, so that they can be used when landing this CL which will
    replace the dummy test with real tests:
    https://codereview.webrtc.org/2675173003/
    
    BUG=webrtc:7013
    TBR=tommi@webrtc.org
    
    Review-Url: https://codereview.webrtc.org/2707013005
    Cr-Commit-Position: refs/heads/master@{#16784}

---
+++ webrtc/ortc/dummy_test.cc
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2017 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "webrtc/test/gtest.h"
+
+// This is just a placeholder test so that trybots can be updated to run the
+// new "ortc_unittests" target.
+TEST(DummyOrtcTest, Test) {
+}
