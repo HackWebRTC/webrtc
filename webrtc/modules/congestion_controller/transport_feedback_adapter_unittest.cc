f7c26d40abb3149f473a6c384fd94ba8c2f62b48
elad.alon <elad.alon@webrtc.org>
2017-03-01 09:05:20 -0800

    Small TransportFeedbackAdapterTest efficiency boost and TODO resolved
    BUG=None
    
    Review-Url: https://codereview.webrtc.org/2726853002
    Cr-Commit-Position: refs/heads/master@{#16950}

--- webrtc/modules/congestion_controller/transport_feedback_adapter_unittest.cc
+++ webrtc/modules/congestion_controller/transport_feedback_adapter_unittest.cc
@@ -99,10 +99,7 @@ class TransportFeedbackAdapterTest : public ::testing::Test {
     }
   }
 
-  // Utility method, to reset arrival_time_ms before adding send time.
-  void OnSentPacket(PacketInfo info) {
-    info.arrival_time_ms = 0;
-    // TODO(philipel): Change -1, -1 to some values.
+  void OnSentPacket(const PacketInfo& info) {
     adapter_->AddPacket(info.sequence_number, info.payload_size,
                         info.pacing_info);
     adapter_->OnSentPacket(info.sequence_number, info.send_time_ms);
