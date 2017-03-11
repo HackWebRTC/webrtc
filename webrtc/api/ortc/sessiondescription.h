55adc0e1a5f39f7b4bb706c1cbc688e75c9754ae
zhihuang <zhihuang@webrtc.org>
2017-03-10 18:33:45 -0800

    Add skeleton webrtc::SessionDescription and webrtc::MediaDescription classes.
    BUG=webrtc:7311
    
    Review-Url: https://codereview.webrtc.org/2743003004
    Cr-Commit-Position: refs/heads/master@{#17181}

---
+++ webrtc/api/ortc/sessiondescription.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef WEBRTC_API_ORTC_SESSIONDESCRIPTION_H_
+#define WEBRTC_API_ORTC_SESSIONDESCRIPTION_H_
+
+#include <string>
+#include <utility>
+
+namespace webrtc {
+
+// A structured representation of an SDP session description.
+class SessionDescription {
+ public:
+  SessionDescription(std::string session_id, std::string session_version)
+      : session_id_(std::move(session_id)),
+        session_version_(std::move(session_version)) {}
+
+  // https://tools.ietf.org/html/rfc4566#section-5.2
+  // o=<username> <sess-id> <sess-version> <nettype> <addrtype>
+  //   <unicast-address>
+  // session_id_ is the "sess-id" field.
+  // session_version_ is the "sess-version" field.
+  const std::string& session_id() const { return session_id_; }
+  void set_session_id(std::string session_id) {
+    session_id_ = std::move(session_id);
+  }
+
+  const std::string& session_version() const { return session_version_; }
+  void set_session_version(std::string session_version) {
+    session_version_ = std::move(session_version);
+  }
+
+ private:
+  std::string session_id_;
+  std::string session_version_;
+};
+
+}  // namespace webrtc
+
+#endif  // WEBRTC_API_ORTC_SESSIONDESCRIPTION_H_
