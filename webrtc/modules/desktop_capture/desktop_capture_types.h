65cb53a5c670d3aa1091f1fabf1d8b443e331296
zijiehe <zijiehe@chromium.org>
2017-03-23 00:02:01 -0700

    Use constexpr function FourCC instead of macro
    constexpr function should be preferred than a macro. So this change replaces
    FOURCC() macro with a constexpr uint32_t FourCC() function.
    
    BUG=679523, 650926
    
    Review-Url: https://codereview.webrtc.org/2771573002
    Cr-Commit-Position: refs/heads/master@{#17351}

--- webrtc/modules/desktop_capture/desktop_capture_types.h
+++ webrtc/modules/desktop_capture/desktop_capture_types.h
@@ -36,32 +36,20 @@ const ScreenId kFullDesktopScreenId = -1;
 
 const ScreenId kInvalidScreenId = -2;
 
-// Depends on webrtc/media:rtc_media_base will trigger a build break in
-// Chromium (Refer to https://codereview.chromium.org/2759493002/). The root
-// cause is still unclear. Before the root cause has been found, copy-paste the
-// definition of the FOURCC() macro here is a simple choice.
-
-/**** Copy from webrtc/media/base/videocommon.h ****/
-// Convert four characters to a FourCC code.
-// Needs to be a macro otherwise the OS X compiler complains when the kFormat*
-// constants are used in a switch.
-#define FOURCC(a, b, c, d)                                        \
-  ((static_cast<uint32_t>(a)) | (static_cast<uint32_t>(b) << 8) | \
-   (static_cast<uint32_t>(c) << 16) | (static_cast<uint32_t>(d) << 24))
-// Some pages discussing FourCC codes:
-//   http://www.fourcc.org/yuv.php
-//   http://v4l2spec.bytesex.org/spec/book1.htm
-//   http://developer.apple.com/quicktime/icefloe/dispatch020.html
-//   http://msdn.microsoft.com/library/windows/desktop/dd206750.aspx#nv12
-//   http://people.xiph.org/~xiphmont/containers/nut/nut4cc.txt
-/**** End copy from webrtc/media/base/videocommon.h ****/
-
 // An integer to attach to each DesktopFrame to differentiate the generator of
 // the frame.
 namespace DesktopCapturerId {
+  constexpr uint32_t CreateFourCC(char a, char b, char c, char d) {
+    return ((static_cast<uint32_t>(a)) |
+            (static_cast<uint32_t>(b) << 8) |
+            (static_cast<uint32_t>(c) << 16) |
+            (static_cast<uint32_t>(d) << 24));
+  }
+
   constexpr uint32_t kUnknown = 0;
-  constexpr uint32_t kScreenCapturerWinGdi = FOURCC('G', 'D', 'I', ' ');
-  constexpr uint32_t kScreenCapturerWinDirectx = FOURCC('D', 'X', 'G', 'I');
+  constexpr uint32_t kScreenCapturerWinGdi = CreateFourCC('G', 'D', 'I', ' ');
+  constexpr uint32_t kScreenCapturerWinDirectx =
+      CreateFourCC('D', 'X', 'G', 'I');
 }  // namespace DesktopCapturerId
 
 }  // namespace webrtc
