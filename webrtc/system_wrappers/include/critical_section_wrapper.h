16d5bae3963950082447dad230f827a134f0cc68
kthelgason <kthelgason@webrtc.org>
2017-03-31 08:23:57 -0700

    Add empty header to fix internal project.
    BUG=None
    TBR=tommi@webrtc.org
    
    Review-Url: https://codereview.webrtc.org/2790493006
    Cr-Commit-Position: refs/heads/master@{#17492}

---
+++ webrtc/system_wrappers/include/critical_section_wrapper.h
@@ -0,0 +1,12 @@
+/*
+ *  Copyright 2017 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef WEBRTC_SYSTEM_WRAPPERS_INCLUDE_CRITICAL_SECTION_WRAPPER_H_
+#define WEBRTC_SYSTEM_WRAPPERS_INCLUDE_CRITICAL_SECTION_WRAPPER_H_
+#endif  // WEBRTC_SYSTEM_WRAPPERS_INCLUDE_CRITICAL_SECTION_WRAPPER_H_
