52a0ce68b8afd059124e6bf9e9dc5f96a37b23bc
nisse <nisse@webrtc.org>
2017-04-03 01:39:58 -0700

    Delete unused method OnProbeBitrate.
    Appears unused since cl https://codereview.webrtc.org/2378103005.
    
    BUG=None
    
    Review-Url: https://codereview.webrtc.org/2792593002
    Cr-Commit-Position: refs/heads/master@{#17500}

--- webrtc/modules/remote_bitrate_estimator/include/remote_bitrate_estimator.h
+++ webrtc/modules/remote_bitrate_estimator/include/remote_bitrate_estimator.h
@@ -34,7 +34,6 @@ class RemoteBitrateObserver {
   // incoming streams.
   virtual void OnReceiveBitrateChanged(const std::vector<uint32_t>& ssrcs,
                                        uint32_t bitrate) = 0;
-  virtual void OnProbeBitrate(uint32_t bitrate) {}
 
   virtual ~RemoteBitrateObserver() {}
 };
