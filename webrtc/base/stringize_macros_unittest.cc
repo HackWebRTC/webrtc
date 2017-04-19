ed754e71ae8866db641677073274e86fe704eeac
kjellander <kjellander@webrtc.org>
2017-04-19 08:37:36 -0700

    Enable GN check for webrtc/base
    It's not possible to enable it for the rtc_base_approved
    target but since a larger refactoring is ongoing for webrtc/base
    this CL doesn't attempt to fix that.
    
    Changes made:
    * Move webrtc/system_wrappers/include/stringize_macros.h into
      webrtc/base:rtc_base_approved_unittests (and corresponding
      unit test to rtc_base_approved_unittests).
    * Move md5digest.* from rtc_base_approved to rtc_base_test_utils target.
    * Move webrtc/system_wrappers/include/stringize_macros.h (+test) into
      webrtc/base.
    * Remove unused use include of webrtc/base/fileutils.h in
      webrtc/base/pathutils.cc
    
    BUG=webrtc:6828, webrtc:3806, webrtc:7480
    NOTRY=True
    
    Review-Url: https://codereview.webrtc.org/2717083002
    Cr-Commit-Position: refs/heads/master@{#17766}

--- webrtc/base/stringize_macros_unittest.cc
+++ webrtc/base/stringize_macros_unittest.cc
@@ -8,7 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "webrtc/system_wrappers/include/stringize_macros.h"
+#include "webrtc/base/stringize_macros.h"
 
 #include "webrtc/test/gtest.h"
 
