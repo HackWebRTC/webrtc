bc83e2ee696ca3672b8cebd658672e95eec4e465
sakal <sakal@webrtc.org>
2017-05-18 06:28:45 -0700

    Removes usage of native base::android::GetApplicationContext()
    BUG=webrtc:7665
    
    Review-Url: https://codereview.webrtc.org/2888093004
    Cr-Commit-Position: refs/heads/master@{#18195}

--- webrtc/sdk/android/src/jni/peerconnection_jni.cc
+++ webrtc/sdk/android/src/jni/peerconnection_jni.cc
@@ -1136,7 +1136,7 @@ JOW(jlong, PeerConnectionFactory_nativeCreateObserver)(
   return (jlong)new PCOJava(jni, j_observer);
 }
 
-JOW(void, PeerConnectionFactory_initializeAndroidGlobals)
+JOW(void, PeerConnectionFactory_nativeInitializeAndroidGlobals)
 (JNIEnv* jni,
  jclass,
  jobject context,
@@ -1146,7 +1146,7 @@ JOW(void, PeerConnectionFactory_initializeAndroidGlobals)
   if (!factory_static_initialized) {
     RTC_DCHECK(j_application_context == nullptr);
     j_application_context = NewGlobalRef(jni, context);
-    webrtc::JVM::Initialize(GetJVM(), context);
+    webrtc::JVM::Initialize(GetJVM());
     factory_static_initialized = true;
   }
 }
