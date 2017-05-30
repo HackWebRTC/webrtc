d7620582342bb896b766bb306135b12aefbeb12c
mbonadei <mbonadei@webrtc.org>
2017-05-30 01:50:35 -0700

    Reland of Adding backward compatibility header (patchset #1 id:1 of https://codereview.webrtc.org/2908143003/ )
    Reason for revert:
    Take three of relanding this after all internal issues have been resolved.
    
    Original issue's description:
    > Revert of Adding backward compatibility header (patchset #1 id:1 of https://codereview.webrtc.org/2909923002/ )
    >
    > Reason for revert:
    > Breaks downstream project.
    >
    > Original issue's description:
    > > Adding backward compatibility header
    > >
    > > This header will be removed ad soon as downstream projects will be
    > > updated.
    > >
    > > BUG=webrtc:4867
    > > NOTRY=True
    > > TBR=kjellander@webrtc.org
    > >
    > > Review-Url: https://codereview.webrtc.org/2909923002
    > > Cr-Commit-Position: refs/heads/master@{#18306}
    > > Committed: https://chromium.googlesource.com/external/webrtc/+/4fa8a97cc3268de73ad021ff56aaa9bf1a566ed4
    >
    > TBR=kjellander@webrtc.org,magjed@webrtc.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=webrtc:4867
    >
    > Review-Url: https://codereview.webrtc.org/2908143003
    > Cr-Commit-Position: refs/heads/master@{#18308}
    > Committed: https://chromium.googlesource.com/external/webrtc/+/6684fdce93ff25a837117cd7a811a6d86cbe4b20
    
    TBR=kjellander@webrtc.org,magjed@webrtc.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=webrtc:4867
    
    Review-Url: https://codereview.webrtc.org/2912773003
    Cr-Commit-Position: refs/heads/master@{#18320}

---
+++ webrtc/sdk/objc/Framework/Classes/corevideo_frame_buffer.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright 2016 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef WEBRTC_SDK_OBJC_FRAMEWORK_CLASSES_COREVIDEO_FRAME_BUFFER_H_
+#define WEBRTC_SDK_OBJC_FRAMEWORK_CLASSES_COREVIDEO_FRAME_BUFFER_H_
+
+// This is a backward compatibility header. It will be removed as soon as
+// dowstream projects will be updated.
+
+#include "webrtc/sdk/objc/Framework/Classes/Video/corevideo_frame_buffer.h"
+
+#endif  // WEBRTC_SDK_OBJC_FRAMEWORK_CLASSES_COREVIDEO_FRAME_BUFFER_H_
