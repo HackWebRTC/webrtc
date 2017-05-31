eb80679c7903c6ea78f2f00f6c4bbe7e16a6563b
brandtr <brandtr@webrtc.org>
2017-05-31 07:46:56 -0700

    Disable flaky test EndToEndTest.TestFlexfecRtpStatePreservation on linux for now.
    BUG=webrtc:7737
    
    Review-Url: https://codereview.webrtc.org/2917673002
    Cr-Commit-Position: refs/heads/master@{#18355}

--- webrtc/video/end_to_end_tests.cc
+++ webrtc/video/end_to_end_tests.cc
@@ -4145,7 +4145,16 @@ TEST_F(EndToEndTest, MAYBE_PictureIdStateRetainedAfterReinitingVp8) {
   TestPictureIdStatePreservation(encoder.get());
 }
 
-TEST_F(EndToEndTest, TestFlexfecRtpStatePreservation) {
+// This test is flaky on linux_memcheck. Disable on all linux bots until
+// flakyness has been fixed.
+// https://bugs.chromium.org/p/webrtc/issues/detail?id=7737
+#if defined(WEBRTC_LINUX)
+#define MAYBE_TestFlexfecRtpStatePreservation \
+  DISABLED_TestFlexfecRtpStatePreservation
+#else
+#define MAYBE_TestFlexfecRtpStatePreservation TestFlexfecRtpStatePreservation
+#endif
+TEST_F(EndToEndTest, MAYBE_TestFlexfecRtpStatePreservation) {
   class RtpSequenceObserver : public test::RtpRtcpObserver {
    public:
     RtpSequenceObserver()
