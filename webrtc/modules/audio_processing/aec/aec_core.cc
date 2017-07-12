10b0d0367346419cce329a4a4ba198e22a4d7015
saza <saza@webrtc.org>
2017-07-12 00:29:36 -0700

    Removes internal buffer memory check in AEC module.
    The check triggered in 30 / 1000 cases of running PeerConnectionIntegrationTest.CallTransferredForCaller locally, far more often than expected.
    
    It will soon be replaced by more graceful handling.
    
    BUG=webrtc:7845
    
    Review-Url: https://codereview.webrtc.org/2975043002
    Cr-Commit-Position: refs/heads/master@{#18983}

--- webrtc/modules/audio_processing/aec/aec_core.cc
+++ webrtc/modules/audio_processing/aec/aec_core.cc
@@ -206,18 +206,15 @@ void BlockBuffer::ExtractExtendedBlock(float extended_block[PART_LEN2]) {
 
   // Extract the previous block.
   WebRtc_MoveReadPtr(buffer_, -1);
-  size_t read_elements = WebRtc_ReadBuffer(
-      buffer_, reinterpret_cast<void**>(&block_ptr), &extended_block[0], 1);
-  RTC_CHECK_EQ(read_elements, 1);
+  WebRtc_ReadBuffer(buffer_, reinterpret_cast<void**>(&block_ptr),
+                    &extended_block[0], 1);
   if (block_ptr != &extended_block[0]) {
     memcpy(&extended_block[0], block_ptr, PART_LEN * sizeof(float));
   }
 
   // Extract the current block.
-  read_elements =
-      WebRtc_ReadBuffer(buffer_, reinterpret_cast<void**>(&block_ptr),
-                        &extended_block[PART_LEN], 1);
-  RTC_CHECK_EQ(read_elements, 1);
+  WebRtc_ReadBuffer(buffer_, reinterpret_cast<void**>(&block_ptr),
+                    &extended_block[PART_LEN], 1);
   if (block_ptr != &extended_block[PART_LEN]) {
     memcpy(&extended_block[PART_LEN], block_ptr, PART_LEN * sizeof(float));
   }
