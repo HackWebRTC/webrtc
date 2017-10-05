e2d6a06fa8c53f757b285318ce5ebaa3619f0597
Patrik Höglund <phoglund@webrtc.org>
2017-10-05 14:53:33 +0200

    Reland "Clean up libjingle API dependencies."
    This is a reland of 9185aca9ce1f66e983d9a5e797cab77a64cc46b0
    
    > Original change's description:
    > > > Clean up libjingle API dependencies.
    > > > 
    > > > This CL moves candidate.h into the public API, since it has
    > > > been implicitly included before.
    > > > 
    > > > This is a straightforward way of solving the circular
    > > > dependencies involving that file. For instance,
    > > > libjingle_peerconnection_api includes candidate.h from
    > > > jsepicecandidate.h, but _api can't depend on rtc_p2p, which
    > > > depends on _api. In fact, _api can't depend on much at all
    > > > since it's a very high level abstraction; instead, things
    > > > should depend on it.
    > > > 
    > > > Furthermore, we have the case where deprecated headers
    > > > include headers in internal modules. I just have to turn
    > > > off include checking for those, but that's not a big deal.
    > > > 
    > > > This CL punts the problem of callfactoryinterface.h being
    > > > implicitly included, and pulling in most of the call
    > > > module with it. This should be addressed in a follow-up
    > > > CL.
    > Bug: webrtc:7504
    > Change-Id: Icae0ba1a0488550e2871cc65e66d3661707aa5b6
    > Reviewed-on: https://webrtc-review.googlesource.com/6460
    > Reviewed-by: Fredrik Solenberg <solenberg@webrtc.org>
    > Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
    > Commit-Queue: Patrik Höglund <phoglund@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#20156}
    
    TBR=deadbeef@webrtc.org
    
    Bug: webrtc:7504
    Change-Id: Ic6598ac2af9355b60bbd289c86dc75e0ae9fed2e
    Reviewed-on: https://webrtc-review.googlesource.com/6801
    Commit-Queue: Patrik Höglund <phoglund@webrtc.org>
    Reviewed-by: Fredrik Solenberg <solenberg@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#20167}

--- p2p/base/icetransportinternal.h
+++ p2p/base/icetransportinternal.h
@@ -13,7 +13,7 @@
 
 #include <string>
 
-#include "p2p/base/candidate.h"
+#include "api/candidate.h"
 #include "p2p/base/candidatepairinterface.h"
 #include "p2p/base/jseptransport.h"
 #include "p2p/base/packettransportinternal.h"
