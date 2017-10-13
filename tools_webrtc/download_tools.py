44ba5e577ac9019835aa5a9b192ad261fa21f374
Henrik Kjellander <kjellander@webrtc.org>
2017-10-13 06:06:25 +0000

    Revert "Use subprocess2 for downloading tools."
    This reverts commit 4d39dc392294807d84eefb1c7414f807d9b4855b.
    
    Reason for revert: To benefit from the other fix in crbug.com/773671. Will reland once DEPS rolling is unblocked.
    
    Original change's description:
    > Use subprocess2 for downloading tools.
    > 
    > Change to use exactly the same approach as when gclient hooks
    > are executing [1]. It should be safe since it's using the DEPS-pinned
    > depot_tools in third_party. Hopefully this solves the race condition
    > problems we've been seeing in crbug.com/773671
    > 
    > [1]: https://cs.chromium.org/chromium/tools/depot_tools/gclient.py?rcl=b3ce73d028b1d44137d533220fd41be31bc31801&l=214
    > 
    > Bug: chromium:773671
    > Change-Id: Ia003dbca394e42556afa1a416fcb4844b960ad6c
    > No-try: True
    > Reviewed-on: https://webrtc-review.googlesource.com/8820
    > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > Commit-Queue: Henrik Kjellander <kjellander@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#20256}
    
    TBR=kjellander@webrtc.org,mbonadei@webrtc.org
    
    Change-Id: I825d291d6f723cac79a6cc6fc54cd5f9d4877152
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:773671
    Reviewed-on: https://webrtc-review.googlesource.com/9080
    Reviewed-by: Henrik Kjellander <kjellander@webrtc.org>
    Commit-Queue: Henrik Kjellander <kjellander@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#20263}

--- tools_webrtc/download_tools.py
+++ tools_webrtc/download_tools.py
@@ -15,17 +15,13 @@ so please download and compile these tools manually if this script fails.
 """
 
 import os
+import subprocess
 import sys
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-SRC_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir))
-DEPOT_TOOLS_DIR = os.path.join(SRC_DIR, 'third_party', 'depot_tools')
-sys.path.insert(0, DEPOT_TOOLS_DIR)
-
-
-import gclient_utils
-import subprocess2
+# Needed to properly resolve PATH and executable extensions on Windows.
+USE_SHELL = sys.platform == 'win32'
 
 
 def main(directories):
@@ -34,8 +30,7 @@ def main(directories):
 
   for path in directories:
     cmd = [
-      sys.executable,
-      os.path.join(DEPOT_TOOLS_DIR, 'download_from_google_storage.py'),
+      'download_from_google_storage',
       '--directory',
       '--num_threads=10',
       '--bucket', 'chrome-webrtc-resources',
@@ -44,14 +39,7 @@ def main(directories):
       path,
     ]
     print 'Downloading precompiled tools...'
-
-    # Perform download similar to how gclient hooks execute.
-    try:
-      gclient_utils.CheckCallAndFilterAndHeader(cmd, cwd=SRC_DIR, always=True)
-    except (gclient_utils.Error, subprocess2.CalledProcessError) as e:
-      print 'Error: %s' % str(e)
-      return 2
-    return 0
+    subprocess.check_call(cmd, shell=USE_SHELL)
 
 
 if __name__ == '__main__':
