5d7fd19c209f703b6abf455bf69793e6741a1a4d
Edward Lemur <ehmaldonado@webrtc.org>
2017-10-27 13:42:43 +0200

    Don't build windows core audio if using dummy file devices.
    If WEBRTC_DUMMY_FILE_DEVICES is set, WEBRTC_CORE_AUDIO_BUILD should not.
    Otherwise audio_device_core_win.h will be included [1] when it shouldn't
    (according to [2]).
    
    [1] https://webrtc.googlesource.com/src/+/master/modules/audio_device/audio_device_impl.cc#22
    [2] https://webrtc.googlesource.com/src/+/master/modules/audio_device/BUILD.gn#177
    
    Bug: webrtc:6265
    Change-Id: Ia6ccb9dda39f411c0d8a548a0501408e87d11a40
    Reviewed-on: https://webrtc-review.googlesource.com/16430
    Reviewed-by: Oleh Prypin <oprypin@webrtc.org>
    Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
    Commit-Queue: Edward Lemur <ehmaldonado@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#20464}

--- modules/audio_device/audio_device_config.h
+++ modules/audio_device/audio_device_config.h
@@ -19,9 +19,11 @@ enum { GET_MIC_VOLUME_INTERVAL_MS = 1000 };
 //
 #if defined(_WIN32)
 #if (_MSC_VER >= 1400)
+#if !defined(WEBRTC_DUMMY_FILE_DEVICES)
 // Windows Core Audio is the default audio layer in Windows.
 // Only supported for VS 2005 and higher.
 #define WEBRTC_WINDOWS_CORE_AUDIO_BUILD
+#endif
 #endif
 #endif
 
