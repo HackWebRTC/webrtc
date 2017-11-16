057ce1de459775affd165fd9bcb531dc44d09a3d
Oskar Sundbom <ossu@webrtc.org>
2017-11-16 10:56:15 +0100

    Optional: Use nullopt and implicit construction in /sdk/android/src/jni/androidvideotracksource.h
    Changes places where we explicitly construct an Optional to instead use
    nullopt or the requisite value type only.
    
    This CL was uploaded by git cl split.
    
    R=sakal@webrtc.org
    
    Bug: None
    Change-Id: Ib01eb9ef7b3b76c0f55890d7ea1b8dfcd30f0617
    Reviewed-on: https://webrtc-review.googlesource.com/23607
    Reviewed-by: Sami Kalliomäki <sakal@webrtc.org>
    Commit-Queue: Oskar Sundbom <ossu@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#20845}

--- sdk/android/src/jni/androidvideotracksource.h
+++ sdk/android/src/jni/androidvideotracksource.h
@@ -38,9 +38,7 @@ class AndroidVideoTrackSource : public rtc::AdaptedVideoTrackSource {
   // Indicates that the encoder should denoise video before encoding it.
   // If it is not set, the default configuration is used which is different
   // depending on video codec.
-  rtc::Optional<bool> needs_denoising() const override {
-    return rtc::Optional<bool>(false);
-  }
+  rtc::Optional<bool> needs_denoising() const override { return false; }
 
   // Called by the native capture observer
   void SetState(SourceState state);
