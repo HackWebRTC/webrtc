36207d600af8344b5fae71d57c1ac5398bfd383c
Tommi <tommi@webrtc.org>
2017-11-28 20:13:03 +0100

    Remove "using rtc::scoped_ptr" from audiotrack.cc.
    This is causing compilation issues in a chromium cl because of type conflicts.
    
    BUG=none
    TBR=henrikg@webrtc.org
    
    Tbr-ing to fix build issue upstream and because there's no code change.
    
    Change-Id: Ia34ae3844fe3f57f047cb44422fa591f752b7bda
    Reviewed-on: https://webrtc-review.googlesource.com/26680
    Reviewed-by: Tommi <tommi@webrtc.org>
    Commit-Queue: Tommi <tommi@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#20921}

--- pc/audiotrack.cc
+++ pc/audiotrack.cc
@@ -13,19 +13,17 @@
 #include "rtc_base/checks.h"
 #include "rtc_base/refcountedobject.h"
 
-using rtc::scoped_refptr;
-
 namespace webrtc {
 
 // static
-scoped_refptr<AudioTrack> AudioTrack::Create(
+rtc::scoped_refptr<AudioTrack> AudioTrack::Create(
     const std::string& id,
-    const scoped_refptr<AudioSourceInterface>& source) {
+    const rtc::scoped_refptr<AudioSourceInterface>& source) {
   return new rtc::RefCountedObject<AudioTrack>(id, source);
 }
 
 AudioTrack::AudioTrack(const std::string& label,
-                       const scoped_refptr<AudioSourceInterface>& source)
+                       const rtc::scoped_refptr<AudioSourceInterface>& source)
     : MediaStreamTrack<AudioTrackInterface>(label), audio_source_(source) {
   if (audio_source_) {
     audio_source_->RegisterObserver(this);
