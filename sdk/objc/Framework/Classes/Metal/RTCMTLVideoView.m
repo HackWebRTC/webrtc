4feb2044db4b3563323c24e2372885cbf7708c73
JT Teh <jtteh@webrtc.org>
2018-02-13 16:20:35 -0800

    Fix rendering on an iPhone X's tall screen.
    Bug: webrtc:8884
    Change-Id: I850e4ea1919837e15a78c90968a4879a1ccbd22c
    Reviewed-on: https://webrtc-review.googlesource.com/52761
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22011}

--- sdk/objc/Framework/Classes/Metal/RTCMTLVideoView.m
+++ sdk/objc/Framework/Classes/Metal/RTCMTLVideoView.m
@@ -90,7 +90,7 @@
   if (_metalView) {
     _metalView.delegate = self;
     [self addSubview:_metalView];
-    _metalView.contentMode = UIViewContentModeScaleAspectFit;
+    _metalView.contentMode = UIViewContentModeScaleAspectFill;
   }
 }
 
