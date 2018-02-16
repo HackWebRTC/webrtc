c453c16dbedbf703c040add2cbcecf6aa9f88e78
Gustaf Ullberg <gustaf@webrtc.org>
2018-02-16 12:09:55 +0100

    Add stubs for AEC3 factory.
    This CL creates empty placeholders for EchoCanceller3Factory. This
    allows for moving the factory of AEC3 as soon as downstream has been
    updated to include echo_canceller3_factory.h.
    
    Bug: webrtc:8844
    Change-Id: I77c53d8257291f189c637e1c9ed76c4e74be1858
    Reviewed-on: https://webrtc-review.googlesource.com/53862
    Commit-Queue: Gustaf Ullberg <gustaf@webrtc.org>
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22050}

---
+++ api/audio/echo_canceller3_factory.cc
@@ -0,0 +1,12 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#include "api/audio/echo_canceller3_factory.h"
+
+// TODO(bugs.webrtc.org/8844): EchoCanceller3Factory will be moved here.
---
+++ api/audio/echo_canceller3_factory.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_AUDIO_ECHO_CANCELLER3_FACTORY_H_
+#define API_AUDIO_ECHO_CANCELLER3_FACTORY_H_
+
+#include "api/audio/echo_canceller3_config.h"
+#include "api/audio/echo_control.h"
+
+// TODO(bugs.webrtc.org/8844): EchoCanceller3Factory will be moved here.
+
+#endif  // API_AUDIO_ECHO_CANCELLER3_FACTORY_H_
