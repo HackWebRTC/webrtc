c4f9824cee43d32cb6296d7d58c8bfb8951f0f0e
Lu Liu <lliuu@webrtc.org>
2018-02-21 19:37:14 +0000

    Revert "Reduce locking in VideoReceiver and check the threading model."
    This reverts commit c75f1e45093a8d5cc62937c7708b87aa5c5bf0b0.
    
    Reason for revert: Breaking internal project
    
    Original change's description:
    > Reduce locking in VideoReceiver and check the threading model.
    > 
    > Note: This is a subset of code that was previously reviewed here:
    >   - https://codereview.webrtc.org/2764573002/
    > 
    > * Added two notification methods, DecoderThreadStarting() and DecoderThreadStopped()
    >   * Allows us to establish a period when the decoder thread is not running and it is
    >     safe to modify variables such as callbacks, that are only read when the decoder
    >     thread is running.
    >   * Allows us to DCHECK thread guarantees/correctness.
    >   * Allows synchronizing callbacks from the module process thread and have them only
    >     active while the decoder thread is running.
    >   * The above, allows us to establish two modes for the thread,
    >     single-threaded-mutable and multi-threaded-const.
    >   * Using that knowledge, we can remove |receive_crit_| as well as locking for a
    >     number of member variables.
    > * Removed |VCMFrameBuffer _frameFromFile| (unused).
    > * Clean up several of my TODOs
    > 
    > Bug: webrtc:7361, chromium:695438
    > Change-Id: Id0048ee9624f76103c088d02825eb5c0d6c8913c
    > Reviewed-on: https://webrtc-review.googlesource.com/55000
    > Commit-Queue: Tommi <tommi@webrtc.org>
    > Reviewed-by: Philip Eliasson <philipel@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#22133}
    
    TBR=tommi@webrtc.org,philipel@webrtc.org
    
    Change-Id: I4d78e8b2c05b36e1a3f64cb38d652579b3a23f22
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:7361, chromium:695438
    Reviewed-on: https://webrtc-review.googlesource.com/56280
    Reviewed-by: Lu Liu <lliuu@webrtc.org>
    Commit-Queue: Lu Liu <lliuu@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22141}

--- video/video_receive_stream.cc
+++ video/video_receive_stream.cc
@@ -226,7 +226,6 @@ void VideoReceiveStream::Start() {
   process_thread_->RegisterModule(&video_receiver_, RTC_FROM_HERE);
 
   // Start the decode thread
-  video_receiver_.DecoderThreadStarting();
   decode_thread_.Start();
   rtp_video_stream_receiver_.StartReceive();
 }
@@ -250,7 +249,6 @@ void VideoReceiveStream::Stop() {
     video_receiver_.TriggerDecoderShutdown();
 
     decode_thread_.Stop();
-    video_receiver_.DecoderThreadStopped();
     // Deregister external decoders so they are no longer running during
     // destruction. This effectively stops the VCM since the decoder thread is
     // stopped, the VCM is deregistered and no asynchronous decoder threads are
@@ -425,6 +423,7 @@ bool VideoReceiveStream::Decode() {
       frame_buffer_->NextFrame(wait_ms, &frame);
 
   if (res == video_coding::FrameBuffer::ReturnReason::kStopped) {
+    video_receiver_.DecodingStopped();
     return false;
   }
 
