0191441f3914025fead7f6f54e36f08040798b37
Mirko Bonadei <mbonadei@webrtc.org>
2018-03-23 16:30:58 +0100

    Lowercase Windows headers.
    Bug: None
    Change-Id: I5a53cc74fd0f0da0b9f71929b9481f8ff651e835
    Reviewed-on: https://webrtc-review.googlesource.com/64449
    Reviewed-by: Tommi <tommi@webrtc.org>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22601}

--- rtc_base/network.cc
+++ rtc_base/network.cc
@@ -24,7 +24,7 @@
 
 #if defined(WEBRTC_WIN)
 #include "rtc_base/win32.h"
-#include <Iphlpapi.h>
+#include <iphlpapi.h>
 #elif !defined(__native_client__)
 #include "rtc_base/ifaddrs_converter.h"
 #endif
