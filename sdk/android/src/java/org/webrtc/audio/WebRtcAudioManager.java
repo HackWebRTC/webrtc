27e41c52f597aefaf1946808e0cdff1848be0593
Magnus Jedvert <magjed@webrtc.org>
2018-03-29 09:06:32 +0200

    Android: Split out VolumeLogger class
    The VolumeLogger class contains enough logic to deserve its own file.
    Also, I want to potentially remove WebRtcAudioManager.java but keep
    volume logging. One problem I see with the VolumeLogger is that it
    spawns a new thread, and we should try to keep the number of threads
    in WebRTC to a minimum. Right now we use excessively many threads.
    
    Bug: webrtc:7452
    Change-Id: I4dd8ffb4265903926f0b372715fc6b876fe5d393
    Reviewed-on: https://webrtc-review.googlesource.com/65401
    Commit-Queue: Magnus Jedvert <magjed@webrtc.org>
    Reviewed-by: Paulina Hensman <phensman@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22676}

--- sdk/android/src/java/org/webrtc/audio/WebRtcAudioManager.java
+++ sdk/android/src/java/org/webrtc/audio/WebRtcAudioManager.java
@@ -18,8 +18,6 @@ import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.os.Build;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.annotation.Nullable;
 import org.webrtc.Logging;
 import org.webrtc.CalledByNative;
@@ -77,61 +75,6 @@ class WebRtcAudioManager {
   private static final int BITS_PER_SAMPLE = 16;
 
   private static final int DEFAULT_FRAME_PER_BUFFER = 256;
-
-  // Private utility class that periodically checks and logs the volume level
-  // of the audio stream that is currently controlled by the volume control.
-  // A timer triggers logs once every 30 seconds and the timer's associated
-  // thread is named "WebRtcVolumeLevelLoggerThread".
-  private static class VolumeLogger {
-    private static final String THREAD_NAME = "WebRtcVolumeLevelLoggerThread";
-    private static final int TIMER_PERIOD_IN_SECONDS = 30;
-
-    private final AudioManager audioManager;
-    private @Nullable Timer timer;
-
-    public VolumeLogger(AudioManager audioManager) {
-      this.audioManager = audioManager;
-    }
-
-    public void start() {
-      timer = new Timer(THREAD_NAME);
-      timer.schedule(new LogVolumeTask(audioManager.getStreamMaxVolume(AudioManager.STREAM_RING),
-                         audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL)),
-          0, TIMER_PERIOD_IN_SECONDS * 1000);
-    }
-
-    private class LogVolumeTask extends TimerTask {
-      private final int maxRingVolume;
-      private final int maxVoiceCallVolume;
-
-      LogVolumeTask(int maxRingVolume, int maxVoiceCallVolume) {
-        this.maxRingVolume = maxRingVolume;
-        this.maxVoiceCallVolume = maxVoiceCallVolume;
-      }
-
-      @Override
-      public void run() {
-        final int mode = audioManager.getMode();
-        if (mode == AudioManager.MODE_RINGTONE) {
-          Logging.d(TAG,
-              "STREAM_RING stream volume: " + audioManager.getStreamVolume(AudioManager.STREAM_RING)
-                  + " (max=" + maxRingVolume + ")");
-        } else if (mode == AudioManager.MODE_IN_COMMUNICATION) {
-          Logging.d(TAG,
-              "VOICE_CALL stream volume: "
-                  + audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL)
-                  + " (max=" + maxVoiceCallVolume + ")");
-        }
-      }
-    }
-
-    private void stop() {
-      if (timer != null) {
-        timer.cancel();
-        timer = null;
-      }
-    }
-  }
 
   private final AudioManager audioManager;
   private final int sampleRate;
