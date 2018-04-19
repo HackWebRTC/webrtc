f9deb7ab5f20cca254dc8f9b5ae7a1f6b2aaf48c
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2018-04-19 17:32:53 +0300

    Fixed comparator in AppRTCMobile for iOS
    Bug: webrtc:9170
    Change-Id: Ib2e27e26c9b5b1459066f59f100ae6cae87be820
    Reviewed-on: https://webrtc-review.googlesource.com/71060
    Reviewed-by: Magnus Jedvert <magjed@webrtc.org>
    Commit-Queue: Magnus Jedvert <magjed@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22982}

--- examples/objc/AppRTCMobile/ARDSettingsModel.m
+++ examples/objc/AppRTCMobile/ARDSettingsModel.m
@@ -39,7 +39,11 @@ NS_ASSUME_NONNULL_BEGIN
   NSArray<NSArray<NSNumber *> *> *sortedResolutions =
       [[resolutions allObjects] sortedArrayUsingComparator:^NSComparisonResult(
                                     NSArray<NSNumber *> *obj1, NSArray<NSNumber *> *obj2) {
-        return obj1.firstObject > obj2.firstObject;
+        NSComparisonResult cmp = [obj1.firstObject compare:obj2.firstObject];
+        if (cmp != NSOrderedSame) {
+          return cmp;
+        }
+        return [obj1.lastObject compare:obj2.lastObject];
       }];
 
   NSMutableArray<NSString *> *resolutionStrings = [[NSMutableArray<NSString *> alloc] init];
