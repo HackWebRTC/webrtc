bd7392829a81d0720887dae9ac0bf83da8d9e80d
Taylor Brandstetter <deadbeef@webrtc.org>
2018-04-20 15:58:11 +0000

    Revert "Reland "Remove our stream << overloads from non-test build targets.""
    This reverts commit d7ee72041f882c023c73e27a7436c626c4e43604.
    
    Reason for revert: Broke downstream build which was using SdpAudioFormat operator<<
    
    Original change's description:
    > Reland "Remove our stream << overloads from non-test build targets."
    > 
    > This is a reland of c841d18d257ba8e4ed7d77d105e3c46006bb1e7e
    > 
    > Original change's description:
    > > Remove our stream << overloads from non-test build targets.
    > >
    > > Most are removed entirely, but RtcErrorType, RtpTransceiverDirection, IPAddress and
    > > SocketAddress are kept behind gtest's #ifdef UNIT_TEST.
    > >
    > > Bug: webrtc:8982
    > > Change-Id: I36db19891e7d25aeacb08b9a08aa2b4004765e70
    > > Reviewed-on: https://webrtc-review.googlesource.com/64143
    > > Commit-Queue: Jonas Olsson <jonasolsson@webrtc.org>
    > > Reviewed-by: Benjamin Wright <benwright@webrtc.org>
    > > Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
    > > Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    > > Reviewed-by: Åsa Persson <asapersson@webrtc.org>
    > > Cr-Commit-Position: refs/heads/master@{#22916}
    > 
    > TBR=deadbeef@webrtc.org,kwiberg@webrtc.org,asapersson@webrtc.org,jonasolsson@webrtc.org,benwright@webrtc.org
    > 
    > Bug: webrtc:8982
    > Change-Id: Ibe08c6270e5e693eb661a6ce9e8f074b34ef8123
    > Reviewed-on: https://webrtc-review.googlesource.com/71161
    > Commit-Queue: Jonas Olsson <jonasolsson@webrtc.org>
    > Reviewed-by: Jonas Olsson <jonasolsson@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#22949}
    
    TBR=deadbeef@webrtc.org,kwiberg@webrtc.org,asapersson@webrtc.org,jonasolsson@webrtc.org,benwright@webrtc.org
    
    Change-Id: I3c2b18ec2877d68a522ecbae7a2955c4eecf36df
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:8982
    Reviewed-on: https://webrtc-review.googlesource.com/71446
    Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
    Commit-Queue: Taylor Brandstetter <deadbeef@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#22963}

--- api/rtcerror.h
+++ api/rtcerror.h
@@ -11,9 +11,7 @@
 #ifndef API_RTCERROR_H_
 #define API_RTCERROR_H_
 
-#ifdef UNIT_TEST
 #include <ostream>
-#endif  // UNIT_TEST
 #include <string>
 #include <utility>  // For std::move.
 
@@ -145,15 +143,9 @@ class RTCError {
 // error type.
 //
 // Only intended to be used for logging/disagnostics.
-std::string ToString(RTCErrorType error);
+std::ostream& operator<<(std::ostream& stream, RTCErrorType error);
 
-#ifdef UNIT_TEST
-inline std::ostream& operator<<(  // no-presubmit-check TODO(webrtc:8982)
-    std::ostream& stream,         // no-presubmit-check TODO(webrtc:8982)
-    RTCErrorType error) {
-  return stream << ToString(error);
-}
-#endif  // UNIT_TEST
+std::string ToString(RTCErrorType error);
 
 // Helper macro that can be used by implementations to create an error with a
 // message and log it. |message| should be a string literal or movable
