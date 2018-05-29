183f4d90bd4f9c3fe5462f78138e657e43954bf5
Taylor Brandstetter <deadbeef@webrtc.org>
2018-05-29 12:09:02 -0700

    Disabling VideoCaptureTest on Linux.
    Has been really flaky lately, due to NumberOfDevices returning 0.
    
    TBR=perkj@webrtc.org
    NOTRY=True
    
    Bug: webrtc:9292
    Change-Id: I5a74236559f13bb6316abced5c12e5d276c398d6
    Reviewed-on: https://webrtc-review.googlesource.com/79680
    Commit-Queue: Taylor Brandstetter <deadbeef@webrtc.org>
    Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#23436}

--- modules/video_capture/test/video_capture_unittest.cc
+++ modules/video_capture/test/video_capture_unittest.cc
@@ -190,9 +190,10 @@ class VideoCaptureTest : public testing::Test {
   unsigned int number_of_devices_;
 };
 
-#ifdef WEBRTC_MAC
 // Currently fails on Mac 64-bit, see
 // https://bugs.chromium.org/p/webrtc/issues/detail?id=5406
+// And flaky on Linux, see bugs.webrtc.org/9292.
+#if defined(WEBRTC_MAC) || defined(WEBRTC_LINUX)
 #define MAYBE_CreateDelete DISABLED_CreateDelete
 #else
 #define MAYBE_CreateDelete CreateDelete
@@ -232,9 +233,10 @@ TEST_F(VideoCaptureTest, MAYBE_CreateDelete) {
   }
 }
 
-#ifdef WEBRTC_MAC
 // Currently fails on Mac 64-bit, see
 // https://bugs.chromium.org/p/webrtc/issues/detail?id=5406
+// And flaky on Linux, see bugs.webrtc.org/9292.
+#if defined(WEBRTC_MAC) || defined(WEBRTC_LINUX)
 #define MAYBE_Capabilities DISABLED_Capabilities
 #else
 #define MAYBE_Capabilities Capabilities
