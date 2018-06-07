6bac5c182ab8845d2f6daba7f1eefb34f5f67367
Ilya Nikolaevskiy <ilnik@webrtc.org>
2018-06-07 11:17:10 +0200

    Remove TimingFrameFlags from its old place after it was moved
    Bug: webrtc:9351
    Change-Id: I97df702adddb600804fe643e17d84331073f4ba1
    Reviewed-on: https://webrtc-review.googlesource.com/81681
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#23564}

--- api/video/video_timing.h
+++ api/video/video_timing.h
@@ -21,16 +21,6 @@
 
 namespace webrtc {
 
-// Deprecated. use VideoSendTiming::TimingFrameFlags instead.
-// TODO(ilnik): remove after some time.
-enum TimingFrameFlags : uint8_t {
-  kNotTriggered = 0,  // Timing info valid, but not to be transmitted.
-                      // Used on send-side only.
-  kTriggeredByTimer = 1 << 0,  // Frame marked for tracing by periodic timer.
-  kTriggeredBySize = 1 << 1,   // Frame marked for tracing due to size.
-  kInvalid = std::numeric_limits<uint8_t>::max()  // Invalid, ignore!
-};
-
 // Video timing timestamps in ms counted from capture_time_ms of a frame.
 // This structure represents data sent in video-timing RTP header extension.
 struct VideoSendTiming {
