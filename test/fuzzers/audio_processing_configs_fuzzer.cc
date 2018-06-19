f344dbbb8c6a821bece625a06e08b00ba36e23c5
Alex Loiko <aleloi@webrtc.org>
2018-06-19 12:33:20 +0200

    Cover AecDump calls in APM fuzzer.
    This is done by attaching a mocked AecDump to APM in the APM fuzzer. It
    gives higher fuzzer coverage.
    
    BEFORE: #4905	DONE   cov: 7739 ft: 46097 corp: 4093/387Mb lim: 4 exec/s: 3 rss: 504Mb
    AFTER: #4905	DONE   cov: 8130 ft: 47662 corp: 4099/386Mb lim: 4 exec/s: 3 rss: 524Mb
    
    
    Bug: webrtc:7820
    Change-Id: If8bae9bfd7aca08f1873e2440ae65a2e74ba3a6b
    Reviewed-on: https://webrtc-review.googlesource.com/84127
    Reviewed-by: Sam Zackrisson <saza@webrtc.org>
    Commit-Queue: Alex Loiko <aleloi@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#23656}

--- test/fuzzers/audio_processing_configs_fuzzer.cc
+++ test/fuzzers/audio_processing_configs_fuzzer.cc
@@ -12,9 +12,11 @@
 #include <string>
 
 #include "api/audio/echo_canceller3_factory.h"
+#include "modules/audio_processing/aec_dump/mock_aec_dump.h"
 #include "modules/audio_processing/include/audio_processing.h"
 #include "rtc_base/arraysize.h"
 #include "rtc_base/numerics/safe_minmax.h"
+#include "rtc_base/ptr_util.h"
 #include "system_wrappers/include/field_trial_default.h"
 #include "test/fuzzers/audio_processing_fuzzer_helper.h"
 #include "test/fuzzers/fuzz_data_helper.h"
@@ -115,6 +117,9 @@ std::unique_ptr<AudioProcessing> CreateApm(test::FuzzDataHelper* fuzz_data,
       AudioProcessingBuilder()
           .SetEchoControlFactory(std::move(echo_control_factory))
           .Create(config));
+
+  apm->AttachAecDump(
+      rtc::MakeUnique<testing::NiceMock<webrtc::test::MockAecDump>>());
 
   webrtc::AudioProcessing::Config apm_config;
   apm_config.residual_echo_detector.enabled = red;
