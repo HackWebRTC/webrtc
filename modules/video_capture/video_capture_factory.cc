aaa483bff61695a36451f700b9dfe2a4d1cc4dd7
Kári Helgason <kthelgason@webrtc.org>
2018-06-20 11:52:03 +0000

    Revert "Remove deprecated mac capture code."
    This reverts commit 3f1d15b35223dc129afd180d020318a56ea1d006.
    
    Reason for revert: Removing this breaks a debugging tool that people relied on. I will update that tool to use the new capturer before relanding this.
    
    Original change's description:
    > Remove deprecated mac capture code.
    > 
    > Bug: webrtc:6898, webrtc:6333, webrtc:7861
    > Change-Id: Ie33eaa47585012f98b59ccffc0c849c1d9da54da
    > Reviewed-on: https://webrtc-review.googlesource.com/79920
    > Reviewed-by: Anders Carlsson <andersc@webrtc.org>
    > Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
    > Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#23454}
    
    TBR=henrika@webrtc.org,andersc@webrtc.org,kthelgason@webrtc.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: webrtc:6898, webrtc:6333, webrtc:7861
    Change-Id: Ifc367eecfe92a2b2e4a826a820dc9c3c970ea01e
    Reviewed-on: https://webrtc-review.googlesource.com/84380
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#23681}

--- modules/video_capture/video_capture_factory.cc
+++ modules/video_capture/video_capture_factory.cc
@@ -16,7 +16,7 @@ namespace webrtc {
 
 rtc::scoped_refptr<VideoCaptureModule> VideoCaptureFactory::Create(
     const char* deviceUniqueIdUTF8) {
-#if defined(WEBRTC_ANDROID) || defined(WEBRTC_MAC)
+#if defined(WEBRTC_ANDROID)
   return nullptr;
 #else
   return videocapturemodule::VideoCaptureImpl::Create(deviceUniqueIdUTF8);
@@ -29,7 +29,7 @@ rtc::scoped_refptr<VideoCaptureModule> VideoCaptureFactory::Create(
 }
 
 VideoCaptureModule::DeviceInfo* VideoCaptureFactory::CreateDeviceInfo() {
-#if defined(WEBRTC_ANDROID) || defined(WEBRTC_MAC)
+#if defined(WEBRTC_ANDROID)
   return nullptr;
 #else
   return videocapturemodule::VideoCaptureImpl::CreateDeviceInfo();
