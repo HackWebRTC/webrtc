8b23dba0e50f6afbdba4ae0de46f56e6f0fde77c
philipel <philipel@webrtc.org>
2018-06-21 11:16:30 +0200

    Add RTPVideoHeader const accessor.
    Preparation CL to remove RTPTypeHeader.
    
    Follow up to this CL (https://webrtc-review.googlesource.com/c/src/+/84423).
    
    Bug: none
    Change-Id: I40516c1791c1ead45e082f554f2f5fcda529e7d6
    Reviewed-on: https://webrtc-review.googlesource.com/84588
    Reviewed-by: Stefan Holmer <stefan@webrtc.org>
    Commit-Queue: Philip Eliasson <philipel@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#23698}

--- modules/include/module_common_types.h
+++ modules/include/module_common_types.h
@@ -72,6 +72,7 @@ struct WebRtcRTPHeader {
   FrameType frameType;
   // TODO(philipel): Remove with RTPTypeHeader.
   RTPVideoHeader& video_header() { return type.Video; }
+  const RTPVideoHeader& video_header() const { return type.Video; }
   RTPTypeHeader type;
   // NTP time of the capture time in local timebase in milliseconds.
   int64_t ntp_time_ms;
