304da796abc656877349dfbd65ccd94b77d6702b
Niels Moller <nisse@webrtc.org>
2018-07-31 09:19:41 +0000

    Revert "Delete root header file typedefs.h. Part 2."
    This reverts commit 4e307f2d880152bbf2a102c8279ebaad7b0aabd7.
    
    Reason for revert: Breaks chromium, file was included in third_party/libjingle_xmpp/xmpp/xmpplogintask_unittest.cc
    
    Original change's description:
    > Delete root header file typedefs.h. Part 2.
    > 
    > This is a followup to
    > https://webrtc-review.googlesource.com/c/src/+/90249, which deleted
    > all uses of the typedefs.h throughout the source tree, but didn't
    > actually delete the file itself.
    > 
    > This cl deletes the file, and also deletes mention of it in the top-level
    > DEPS file.
    > 
    > TBR: kwiberg@webrtc.org
    > Bug: webrtc:6854
    > Change-Id: I90bb72961ff7e9f3ac8b7123cfc1177143d77a20
    > Reviewed-on: https://webrtc-review.googlesource.com/90410
    > Reviewed-by: Niels Moller <nisse@webrtc.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > Reviewed-by: Fredrik Solenberg <solenberg@webrtc.org>
    > Commit-Queue: Niels Moller <nisse@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#24149}
    
    TBR=solenberg@webrtc.org,mbonadei@webrtc.org,kwiberg@webrtc.org,nisse@webrtc.org
    
    Change-Id: I9c3e2099ecd5bf848827f89374fe1a0488a98823
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:6854
    Reviewed-on: https://webrtc-review.googlesource.com/91420
    Reviewed-by: Niels Moller <nisse@webrtc.org>
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#24150}

---
+++ typedefs.h
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+// TODO(solenberg): Make the files including typedefs.h directly include the
+//                  files below they need.
+#ifndef TYPEDEFS_H_
+#define TYPEDEFS_H_
+
+#include <stdint.h>
+
+#include "rtc_base/system/arch.h"
+#include "rtc_base/system/unused.h"
+
+#endif  // TYPEDEFS_H_
