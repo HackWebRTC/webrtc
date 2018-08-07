f96b1ca609364655482b767c0cb77d3aafde4028
Sebastian Jansson <srte@webrtc.org>
2018-08-07 18:58:05 +0200

    Move SimulatedNetwork class to separate file.
    Bug: webrtc:9467
    Change-Id: Iaf91f27ea7ad9e9e59991bbeb0ef3868578e6a8f
    Reviewed-on: https://webrtc-review.googlesource.com/92884
    Reviewed-by: Niels Moller <nisse@webrtc.org>
    Commit-Queue: Sebastian Jansson <srte@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#24221}

--- call/fake_network_pipe.h
+++ call/fake_network_pipe.h
@@ -22,11 +22,11 @@
 #include "api/call/transport.h"
 #include "api/test/simulated_network.h"
 #include "call/call.h"
+#include "call/simulated_network.h"
 #include "common_types.h"  // NOLINT(build/include)
 #include "modules/include/module.h"
 #include "rtc_base/constructormagic.h"
 #include "rtc_base/criticalsection.h"
-#include "rtc_base/random.h"
 #include "rtc_base/thread_annotations.h"
 
 namespace webrtc {
@@ -96,58 +96,6 @@ class NetworkPacket {
   // network pipe.
   MediaType media_type_;
   absl::optional<int64_t> packet_time_us_;
-};
-
-// Class simulating a network link. This is a simple and naive solution just
-// faking capacity and adding an extra transport delay in addition to the
-// capacity introduced delay.
-class SimulatedNetwork : public NetworkSimulationInterface {
- public:
-  using Config = NetworkSimulationInterface::SimulatedNetworkConfig;
-  explicit SimulatedNetwork(Config config, uint64_t random_seed = 1);
-  ~SimulatedNetwork() override;
-
-  // Sets a new configuration. This won't affect packets already in the pipe.
-  void SetConfig(const Config& config);
-  void PauseTransmissionUntil(int64_t until_us);
-
-  // NetworkSimulationInterface
-  bool EnqueuePacket(PacketInFlightInfo packet) override;
-  std::vector<PacketDeliveryInfo> DequeueDeliverablePackets(
-      int64_t receive_time_us) override;
-
-  absl::optional<int64_t> NextDeliveryTimeUs() const override;
-
- private:
-  struct PacketInfo {
-    PacketInFlightInfo packet;
-    int64_t arrival_time_us;
-  };
-  rtc::CriticalSection config_lock_;
-
-  // |process_lock| guards the data structures involved in delay and loss
-  // processes, such as the packet queues.
-  rtc::CriticalSection process_lock_;
-  std::queue<PacketInfo> capacity_link_ RTC_GUARDED_BY(process_lock_);
-  Random random_;
-
-  std::deque<PacketInfo> delay_link_;
-
-  // Link configuration.
-  Config config_ RTC_GUARDED_BY(config_lock_);
-  absl::optional<int64_t> pause_transmission_until_us_
-      RTC_GUARDED_BY(config_lock_);
-
-  // Are we currently dropping a burst of packets?
-  bool bursting_;
-
-  // The probability to drop the packet if we are currently dropping a
-  // burst of packet
-  double prob_loss_bursting_ RTC_GUARDED_BY(config_lock_);
-
-  // The probability to drop a burst of packets.
-  double prob_start_bursting_ RTC_GUARDED_BY(config_lock_);
-  int64_t capacity_delay_error_bytes_ = 0;
 };
 
 // Class faking a network link, internally is uses an implementation of a
