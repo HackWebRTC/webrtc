657f2e6c3efbdb1a9fdea305f2eec97b3a84062a
Jesús de Vicente Peña <devicentepena@webrtc.org>
2018-08-27 15:48:20 +0200

    AEC3: audioproc_f: adding the read of the parameter fixed_capture_delay_samples
    Bug: webrtc:8671
    Change-Id: Ibbf1a725c1ec3a26879ab4feb2a655ed1460b359
    Reviewed-on: https://webrtc-review.googlesource.com/96220
    Reviewed-by: Per Åhgren <peah@webrtc.org>
    Commit-Queue: Jesus de Vicente Pena <devicentepena@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#24452}

--- modules/audio_processing/test/audio_processing_simulator.cc
+++ modules/audio_processing/test/audio_processing_simulator.cc
@@ -220,6 +220,8 @@ class Aec3ParametersParser {
                 &cfg.delay.hysteresis_limit_2_blocks);
       ReadParam(section, "skew_hysteresis_blocks",
                 &cfg.delay.skew_hysteresis_blocks);
+      ReadParam(section, "fixed_capture_delay_samples",
+                &cfg.delay.fixed_capture_delay_samples);
     }
 
     if (rtc::GetValueFromJsonObject(root, "filter", &section)) {
