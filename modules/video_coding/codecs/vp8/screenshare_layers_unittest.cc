260182d9f3042f1bd4534271c95d77cc1a43556f
Rasmus Brandt <brandtr@webrtc.org>
2018-09-03 12:59:51 +0200

    Remove unused MockFrameDropper and make FrameDropper non-virtual.
    Bug: webrtc:9711
    Change-Id: I962039c3ebea1a9445ab3a43071279c4ce8a55cf
    Reviewed-on: https://webrtc-review.googlesource.com/97326
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Commit-Queue: Rasmus Brandt <brandtr@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#24535}

--- modules/video_coding/codecs/vp8/screenshare_layers_unittest.cc
+++ modules/video_coding/codecs/vp8/screenshare_layers_unittest.cc
@@ -14,10 +14,10 @@
 #include "modules/video_coding/codecs/vp8/libvpx_vp8_encoder.h"
 #include "modules/video_coding/codecs/vp8/screenshare_layers.h"
 #include "modules/video_coding/include/video_codec_interface.h"
-#include "modules/video_coding/utility/mock/mock_frame_dropper.h"
 #include "system_wrappers/include/clock.h"
 #include "system_wrappers/include/metrics.h"
 #include "system_wrappers/include/metrics_default.h"
+#include "test/gmock.h"
 #include "test/gtest.h"
 
 using ::testing::_;
