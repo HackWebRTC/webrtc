8db246a6bbda2b73573c07e196d428647967b259
Sami Kalliomäki <sakal@webrtc.org>
2018-10-04 10:58:24 +0200

    Document methods that are only supported on a specific Android version.
    R=phensman@webrtc.org
    
    Bug: webrtc:9819
    Change-Id: Ifd3da9e1b70d0cfc479777c3a8031f632296be38
    Reviewed-on: https://webrtc-review.googlesource.com/c/103680
    Reviewed-by: Paulina Hensman <phensman@webrtc.org>
    Commit-Queue: Sami Kalliomäki <sakal@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#24987}

--- sdk/android/api/org/webrtc/ScreenCapturerAndroid.java
+++ sdk/android/api/org/webrtc/ScreenCapturerAndroid.java
@@ -31,6 +31,8 @@ import javax.annotation.Nullable;
  * place on the HandlerThread of the given {@code SurfaceTextureHelper}. When done with each frame,
  * the native code returns the buffer to the  {@code SurfaceTextureHelper} to be used for new
  * frames. At any time, at most one frame is being processed.
+ *
+ * @note This class is only supported on Android Lollipop and above.
  */
 @TargetApi(21)
 public class ScreenCapturerAndroid implements VideoCapturer, VideoSink {
