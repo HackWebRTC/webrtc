9f80b97309dffa42780dd1f0e8eab79ff4db490d
Jonathan Metzman <metzman@chromium.org>
2018-10-05 10:38:13 -0700

    Fix fuzzer build failures on Windows
    Fix the following issues with fuzz targets when built on Windows:
    1. Fix audio_processing_fuzzer by making types match in
    invocations of CheckedDivExact by explicitly casting to size_t.
    2. Fix packet_buffer_fuzzer by including "frame_object.h" for
    declaration of RtpFrameObject.
    3. Fix rtcp_receiver_fuzzer by including "tmmb_item.h" for declaration
    of TmmbItem.
    
    Bug: chromium:891867
    Change-Id: Iddc338360ca37d5fc31488ec908eb4cdb5cc7b94
    Reviewed-on: https://webrtc-review.googlesource.com/c/103844
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Alex Loiko <aleloi@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25028}

--- test/fuzzers/packet_buffer_fuzzer.cc
+++ test/fuzzers/packet_buffer_fuzzer.cc
@@ -8,6 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#include "modules/video_coding/frame_object.h"
 #include "modules/video_coding/packet_buffer.h"
 #include "system_wrappers/include/clock.h"
 #include "test/fuzzers/fuzz_data_helper.h"
