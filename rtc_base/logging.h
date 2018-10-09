99a70a2d785c0353c3e0eddb463cf75941b6ffd7
Mirko Bonadei <mbonadei@webrtc.org>
2018-10-09 16:45:14 +0200

    Remove rtc_base_approved_objc and introduce rtc_base:logging_mac.
    This CL removes the need of having rtc_base:rtc_base_approved_generic
    and rtc_base:rtc_base_approved_objc since it removes the _objc build
    target by moving the declaration of rtc::DescriptionFromOSStatus into
    rtc_base/logging_mac.h in order to have a new target
    rtc_base:logging_mac on which rtc_base:logging can depend on.
    
    The target rtc_base:rtc_base_approved_generic will be removed in a
    follow up CL.
    
    Bug: webrtc:9838
    Change-Id: Id93ac7bced213128e7d654694ff15337c26dab62
    Reviewed-on: https://webrtc-review.googlesource.com/c/104802
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25085}

--- rtc_base/logging.h
+++ rtc_base/logging.h
@@ -51,15 +51,15 @@
 #include <string>
 #include <utility>
 
-#if defined(WEBRTC_MAC) && !defined(WEBRTC_IOS)
-#include <CoreServices/CoreServices.h>
-#endif
-
 #include "absl/strings/string_view.h"
 #include "rtc_base/constructormagic.h"
 #include "rtc_base/deprecation.h"
 #include "rtc_base/strings/string_builder.h"
 #include "rtc_base/system/inline.h"
+
+#if defined(WEBRTC_MAC) && !defined(WEBRTC_IOS)
+#include "rtc_base/logging_mac.h"
+#endif  // WEBRTC_MAC
 
 #if !defined(NDEBUG) || defined(DLOG_ALWAYS_ON)
 #define RTC_DLOG_IS_ON 1
@@ -68,11 +68,6 @@
 #endif
 
 namespace rtc {
-
-#if defined(WEBRTC_MAC) && !defined(WEBRTC_IOS)
-// Returns a UTF8 description from an OS X Status error.
-std::string DescriptionFromOSStatus(OSStatus err);
-#endif
 
 //////////////////////////////////////////////////////////////////////
 
