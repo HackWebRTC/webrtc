dd20c9c1e3f681f6c33d1879c76f588bd4b095bd
Tomas Popela <tomas.popela@gmail.com>
2018-11-01 06:12:33 +0100

    Add support for screen sharing with PipeWire on Wayland
    Currently, when users want to use the screen sharing and are using the
    Wayland display server (the default on Fedora distribution), then it
    doesn't work, because the WebRTC only includes the X11 implementation.
    This change adds the support by using the PipeWire multimedia server.
    
    The PipeWire implementation in WebRTC stays in
    screen-capturer-pipewire.c and is guarded by the rtc_use_pipewire build
    flag that is automatically enabled on Linux.
    
    More information are included in the relevant commit messages.
    
    Tested on the current Chromium master and Firefox.
    
    The sysroot changes are requested in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1258174
    
    Co-authored-by: Jan Grulich <grulja@gmail.com>
    Co-authored-by: Eike Rathke <erathke@redhat.com>
    Change-Id: I212074a4bc437b99a77bf383266026c5bfae7c4a
    
    BUG=chromium:682122
    
    Change-Id: I212074a4bc437b99a77bf383266026c5bfae7c4a
    Reviewed-on: https://webrtc-review.googlesource.com/c/103504
    Commit-Queue: Patrik Höglund <phoglund@webrtc.org>
    Reviewed-by: Patrik Höglund <phoglund@webrtc.org>
    Reviewed-by: Brave Yao <braveyao@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25461}

--- modules/desktop_capture/desktop_capture_options.h
+++ modules/desktop_capture/desktop_capture_options.h
@@ -15,7 +15,7 @@
 #include "rtc_base/system/rtc_export.h"
 
 #if defined(USE_X11)
-#include "modules/desktop_capture/x11/shared_x_display.h"
+#include "modules/desktop_capture/linux/shared_x_display.h"
 #endif
 
 #if defined(WEBRTC_MAC) && !defined(WEBRTC_IOS)
@@ -115,6 +115,11 @@ class RTC_EXPORT DesktopCaptureOptions {
   }
 #endif
 
+#if defined(WEBRTC_USE_PIPEWIRE)
+  bool allow_pipewire() const { return allow_pipewire_; }
+  void set_allow_pipewire(bool allow) { allow_pipewire_ = allow; }
+#endif
+
  private:
 #if defined(USE_X11)
   rtc::scoped_refptr<SharedXDisplay> x_display_;
@@ -138,6 +143,9 @@ class RTC_EXPORT DesktopCaptureOptions {
 #endif
   bool disable_effects_ = true;
   bool detect_updated_region_ = false;
+#if defined(WEBRTC_USE_PIPEWIRE)
+  bool allow_pipewire_ = false;
+#endif
 };
 
 }  // namespace webrtc
