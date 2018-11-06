34fc346a0c77e21bffb6a6631e265a707e6e4ae2
Artem Titarenko <artit@webrtc.org>
2018-11-06 12:29:29 +0100

    Add support for computing iOS code coverage
    Also disable failing PosixSignalDeliveryTest* tests for iOS
    
    Bug: chromium:844647
    Change-Id: I64bb233bef2f06f6778f2d475b6d3ad685fb9143
    Reviewed-on: https://webrtc-review.googlesource.com/c/105641
    Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
    Reviewed-by: Patrik Höglund <phoglund@webrtc.org>
    Reviewed-by: Peter Hanspers <peterhanspers@webrtc.org>
    Reviewed-by: Artem Titarenko <artit@webrtc.org>
    Commit-Queue: Artem Titarenko <artit@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25524}

--- examples/objc/AppRTCMobile/tests/main.mm
+++ examples/objc/AppRTCMobile/tests/main.mm
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *  Copyright 2018 The WebRTC Project Authors. All rights reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,10 +8,13 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#include "test/ios/coverage_util_ios.h"
+
 int main(int argc, char* argv[]) {
+  rtc::test::ConfigureCoverageReportPath();
+
   @autoreleasepool {
     return UIApplicationMain(argc, argv, nil, nil);
   }
