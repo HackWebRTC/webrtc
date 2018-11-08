90e6745f77c5389e21c034f8bbfc1c88b1605b5d
Niels Möller <nisse@webrtc.org>
2018-11-08 17:07:27 +0100

    Delete deprecated class WrappedI420Buffer
    Bug: None
    Change-Id: Ife3ac3f65d7631732e8007ba1563e7eaf8606ff7
    Reviewed-on: https://webrtc-review.googlesource.com/c/110249
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Reviewed-by: Magnus Jedvert <magjed@webrtc.org>
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25615}

--- common_video/include/video_frame_buffer.h
+++ common_video/include/video_frame_buffer.h
@@ -20,43 +20,6 @@
 
 namespace webrtc {
 
-// Deprecated. Please use WrapI420Buffer(...) instead.
-class WrappedI420Buffer : public I420BufferInterface {
- public:
-  WrappedI420Buffer(int width,
-                    int height,
-                    const uint8_t* y_plane,
-                    int y_stride,
-                    const uint8_t* u_plane,
-                    int u_stride,
-                    const uint8_t* v_plane,
-                    int v_stride,
-                    const rtc::Callback0<void>& no_longer_used);
-  int width() const override;
-  int height() const override;
-
-  const uint8_t* DataY() const override;
-  const uint8_t* DataU() const override;
-  const uint8_t* DataV() const override;
-  int StrideY() const override;
-  int StrideU() const override;
-  int StrideV() const override;
-
- private:
-  friend class rtc::RefCountedObject<WrappedI420Buffer>;
-  ~WrappedI420Buffer() override;
-
-  const int width_;
-  const int height_;
-  const uint8_t* const y_plane_;
-  const uint8_t* const u_plane_;
-  const uint8_t* const v_plane_;
-  const int y_stride_;
-  const int u_stride_;
-  const int v_stride_;
-  rtc::Callback0<void> no_longer_used_cb_;
-};
-
 rtc::scoped_refptr<I420BufferInterface> WrapI420Buffer(
     int width,
     int height,
