3cf8f3e005c4ce0950567dcc259e95dc656b7189
Mirko Bonadei <mbonadei@webrtc.org>
2018-11-19 09:17:51 +0100

    Adding empty api:create_peerconnection_factory.
    Functions to create instances of webrtc::PeerConnectionFactoryInterface
    will be moved to this build target soon (in CL [1]).
    
    This change allows downstream customers to forward fix their builds
    by including api/create_peerconnection_factory.h and depending on
    api:create_peerconnection_factory.
    
    [1] - https://webrtc-review.googlesource.com/c/src/+/111186
    
    Bug: webrtc:9862
    Change-Id: Iff4aa12ae72b44386cf538bf7addba073a77f5cf
    Reviewed-on: https://webrtc-review.googlesource.com/c/111248
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25687}

---
+++ api/create_peerconnection_factory.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_CREATE_PEERCONNECTION_FACTORY_H_
+#define API_CREATE_PEERCONNECTION_FACTORY_H_
+
+// TODO(bugs.webrtc.org/9862): Move webrtc::CreatePeerConnectionFactory and
+// webrtc::CreatePeerConnectionFactoryWithAudioMixer here.
+
+#endif  // API_CREATE_PEERCONNECTION_FACTORY_H_
