aa3d8ad71b9d1022a94091469e2f457deb7fafc5
Elad Alon <eladalon@webrtc.org>
2018-12-03 12:49:41 +0100

    Reland "Fix output period in RtcEventLogImpl"
    This is a reland of a4dcb749fbbc83a874d4e2c65de5a98465d3e200
    
    Original change's description:
    > Fix output period in RtcEventLogImpl
    > 
    > RtcEventLogImpl::StartLogging() was ignoring one of its parameters.
    > This CL fixes the issue.
    > 
    > Bug: webrtc:10082
    > Change-Id: Ie1790c1a7299748dabe99909d967384ad9895635
    > Reviewed-on: https://webrtc-review.googlesource.com/c/112586
    > Reviewed-by: Björn Terelius <terelius@webrtc.org>
    > Commit-Queue: Elad Alon <eladalon@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#25858}
    
    Bug: webrtc:10082
    Change-Id: I783fba84aa35e489f6235538c624b19f2f98a962
    Reviewed-on: https://webrtc-review.googlesource.com/c/112860
    Reviewed-by: Björn Terelius <terelius@webrtc.org>
    Commit-Queue: Elad Alon <eladalon@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#25876}

--- logging/rtc_event_log/rtc_event_log_impl.cc
+++ logging/rtc_event_log/rtc_event_log_impl.cc
@@ -185,10 +185,11 @@ bool RtcEventLogImpl::StartLogging(std::unique_ptr<RtcEventLogOutput> output,
                    << "(" << timestamp_us << ", " << utc_time_us << ").";
 
   // Binding to |this| is safe because |this| outlives the |task_queue_|.
-  auto start = [this, timestamp_us,
+  auto start = [this, output_period_ms, timestamp_us,
                 utc_time_us](std::unique_ptr<RtcEventLogOutput> output) {
     RTC_DCHECK_RUN_ON(task_queue_.get());
     RTC_DCHECK(output->IsActive());
+    output_period_ms_ = output_period_ms;
     event_output_ = std::move(output);
     num_config_events_written_ = 0;
     WriteToOutput(event_encoder_->EncodeLogStart(timestamp_us, utc_time_us));
