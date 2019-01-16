20aae1de5c745fd62025e3464f736416c849e270
Sebastian Jansson <srte@webrtc.org>
2019-01-16 07:44:11 +0000

    Reland "Adds richer packet and ice processing to ParsedRtcEventLog."
    This reverts commit 5586d7fb5717fd772463c17a4675491980c5d017.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "Adds richer packet and ice processing to ParsedRtcEventLog."
    > 
    > This reverts commit 4306a25dfcaba7defe09f5d4b669736d374fe985.
    > 
    > Reason for revert: Breaks downstream project
    > 
    > Original change's description:
    > > Adds richer packet and ice processing to ParsedRtcEventLog.
    > > 
    > > Bug: webrtc:10170
    > > Change-Id: I0f10a8c0b5656917a806cf0f3ad88b7a6baee000
    > > Reviewed-on: https://webrtc-review.googlesource.com/c/116069
    > > Reviewed-by: Björn Terelius <terelius@webrtc.org>
    > > Commit-Queue: Sebastian Jansson <srte@webrtc.org>
    > > Cr-Commit-Position: refs/heads/master@{#26268}
    > 
    > TBR=terelius@webrtc.org,srte@webrtc.org
    > 
    > Change-Id: Ic50fdfb6b10c26e77728b594f553bc4aac4eb0ab
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: webrtc:10170
    > Reviewed-on: https://webrtc-review.googlesource.com/c/117780
    > Reviewed-by: Amit Hilbuch <amithi@webrtc.org>
    > Commit-Queue: Amit Hilbuch <amithi@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#26270}
    
    TBR=terelius@webrtc.org,srte@webrtc.org,amithi@webrtc.org
    
    Change-Id: I5e87fb472b91dd4b6fa177418f03a9031035ec60
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:10170
    Reviewed-on: https://webrtc-review.googlesource.com/c/117721
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Commit-Queue: Sebastian Jansson <srte@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#26274}

--- logging/rtc_event_log/logged_events.h
+++ logging/rtc_event_log/logged_events.h
@@ -13,7 +13,11 @@
 #include <string>
 #include <vector>
 
+#include "absl/types/optional.h"
 #include "api/rtp_headers.h"
+#include "api/units/data_rate.h"
+#include "api/units/time_delta.h"
+#include "api/units/timestamp.h"
 #include "logging/rtc_event_log/events/rtc_event_dtls_transport_state.h"
 #include "logging/rtc_event_log/events/rtc_event_ice_candidate_pair.h"
 #include "logging/rtc_event_log/events/rtc_event_ice_candidate_pair_config.h"
@@ -437,5 +441,71 @@ struct LoggedVideoSendConfig {
   int64_t timestamp_us;
   rtclog::StreamConfig config;
 };
+
+struct LoggedRouteChangeEvent {
+  uint32_t route_id;
+  int64_t timestamp_us;
+  uint16_t send_overhead;
+  uint16_t return_overhead;
+};
+
+enum class LoggedMediaType : uint8_t { kUnknown, kAudio, kVideo };
+
+struct LoggedPacketInfo {
+  LoggedPacketInfo(const LoggedRtpPacket& rtp,
+                   LoggedMediaType media_type,
+                   bool rtx,
+                   Timestamp capture_time);
+  LoggedPacketInfo(const LoggedPacketInfo&);
+  ~LoggedPacketInfo();
+  uint32_t ssrc;
+  uint16_t stream_seq_no;
+  uint16_t size;
+  uint16_t overhead = 0;
+  uint8_t payload_type;
+  LoggedMediaType media_type = LoggedMediaType::kUnknown;
+  bool rtx = false;
+  bool marker_bit = false;
+  bool has_transport_seq_no = false;
+  bool last_in_feedback = false;
+  uint16_t transport_seq_no = 0;
+  // The RTP header timestamp unwrapped and converted from tick count to seconds
+  // based timestamp.
+  Timestamp capture_time;
+  // The time the packet was logged. This is the receive time for incoming
+  // packets and send time for outgoing.
+  Timestamp log_packet_time;
+  // The receive time that was reported in feedback. For incoming packets this
+  // corresponds to log_packet_time, but might be measured using another clock.
+  // PlusInfinity indicates that the packet was lost.
+  Timestamp reported_recv_time = Timestamp::MinusInfinity();
+  // The time feedback message was logged. This is the feedback send time for
+  // incoming packets and feedback receive time for outgoing.
+  // PlusInfinity indicates that feedback was expected but not received.
+  Timestamp log_feedback_time = Timestamp::MinusInfinity();
+  // The delay betweeen receiving an RTP packet and sending feedback for
+  // incoming packets. For outgoing packets we don't know the feedback send
+  // time, and this is instead calculated as the difference in reported receive
+  // time between this packet and the last packet in the same feedback message.
+  TimeDelta feedback_hold_duration = TimeDelta::MinusInfinity();
+};
+
+enum class LoggedIceEventType {
+  kAdded,
+  kUpdated,
+  kDestroyed,
+  kSelected,
+  kCheckSent,
+  kCheckReceived,
+  kCheckResponseSent,
+  kCheckResponseReceived,
+};
+
+struct LoggedIceEvent {
+  uint32_t candidate_pair_id;
+  int64_t timestamp_us;
+  LoggedIceEventType event_type;
+};
+
 }  // namespace webrtc
 #endif  // LOGGING_RTC_EVENT_LOG_LOGGED_EVENTS_H_
