0774bd9583a3a8a8efa80820ba01ee75c02e5677
Artem Titov <titovartem@google.com>
2019-01-30 15:26:05 +0100

    Introduce network layer.
    This CL contains network emulation layer and is a first part of landing
    CL https://webrtc-review.googlesource.com/c/src/+/116663
    
    Bug: webrtc:10138
    Change-Id: If664b21e9df847aef8144d622d08fc7e9f6608da
    Reviewed-on: https://webrtc-review.googlesource.com/c/120406
    Commit-Queue: Artem Titov <titovartem@webrtc.org>
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#26470}

--- test/scenario/network/network_emulation.cc
+++ test/scenario/network/network_emulation.cc
@@ -10,9 +10,11 @@
 
 #include "test/scenario/network/network_emulation.h"
 
+#include <limits>
 #include <memory>
 
 #include "absl/memory/memory.h"
+#include "rtc_base/bind.h"
 #include "rtc_base/logging.h"
 
 namespace webrtc {
@@ -28,10 +30,9 @@ EmulatedIpPacket::EmulatedIpPacket(const rtc::SocketAddress& from,
       dest_endpoint_id(dest_endpoint_id),
       data(data),
       arrival_time(arrival_time) {}
-
 EmulatedIpPacket::~EmulatedIpPacket() = default;
-
 EmulatedIpPacket::EmulatedIpPacket(EmulatedIpPacket&&) = default;
+EmulatedIpPacket& EmulatedIpPacket::operator=(EmulatedIpPacket&&) = default;
 
 void EmulatedNetworkNode::CreateRoute(
     uint64_t receiver_id,
@@ -57,8 +58,9 @@ EmulatedNetworkNode::~EmulatedNetworkNode() = default;
 
 void EmulatedNetworkNode::OnPacketReceived(EmulatedIpPacket packet) {
   rtc::CritScope crit(&lock_);
-  if (routing_.find(packet.dest_endpoint_id) == routing_.end())
+  if (routing_.find(packet.dest_endpoint_id) == routing_.end()) {
     return;
+  }
   uint64_t packet_id = next_packet_id_++;
   bool sent = network_behavior_->EnqueuePacket(
       PacketInFlightInfo(packet.size(), packet.arrival_time.us(), packet_id));
@@ -119,12 +121,118 @@ void EmulatedNetworkNode::SetReceiver(
                 .insert(std::pair<uint64_t, EmulatedNetworkReceiverInterface*>(
                     dest_endpoint_id, receiver))
                 .second)
-      << "Such routing already exists";
+      << "Routing for endpoint " << dest_endpoint_id << " already exists";
 }
 
 void EmulatedNetworkNode::RemoveReceiver(uint64_t dest_endpoint_id) {
   rtc::CritScope crit(&lock_);
   routing_.erase(dest_endpoint_id);
+}
+
+EndpointNode::EndpointNode(uint64_t id, rtc::IPAddress ip, Clock* clock)
+    : id_(id),
+      peer_local_addr_(ip),
+      send_node_(nullptr),
+      clock_(clock),
+      next_port_(kFirstEphemeralPort),
+      connected_endpoint_id_(absl::nullopt) {}
+EndpointNode::~EndpointNode() = default;
+
+uint64_t EndpointNode::GetId() const {
+  return id_;
+}
+
+void EndpointNode::SetSendNode(EmulatedNetworkNode* send_node) {
+  send_node_ = send_node;
+}
+
+void EndpointNode::SendPacket(const rtc::SocketAddress& from,
+                              const rtc::SocketAddress& to,
+                              rtc::CopyOnWriteBuffer packet) {
+  RTC_CHECK(from.ipaddr() == peer_local_addr_);
+  RTC_CHECK(connected_endpoint_id_);
+  RTC_CHECK(send_node_);
+  send_node_->OnPacketReceived(EmulatedIpPacket(
+      from, to, connected_endpoint_id_.value(), std::move(packet),
+      Timestamp::us(clock_->TimeInMicroseconds())));
+}
+
+absl::optional<uint16_t> EndpointNode::BindReceiver(
+    uint16_t desired_port,
+    EmulatedNetworkReceiverInterface* receiver) {
+  rtc::CritScope crit(&receiver_lock_);
+  uint16_t port = desired_port;
+  if (port == 0) {
+    // Because client can specify its own port, next_port_ can be already in
+    // use, so we need to find next available port.
+    int ports_pool_size =
+        std::numeric_limits<uint16_t>::max() - kFirstEphemeralPort + 1;
+    for (int i = 0; i < ports_pool_size; ++i) {
+      uint16_t next_port = NextPort();
+      if (port_to_receiver_.find(next_port) == port_to_receiver_.end()) {
+        port = next_port;
+        break;
+      }
+    }
+  }
+  RTC_CHECK(port != 0) << "Can't find free port for receiver in endpoint "
+                       << id_;
+  bool result = port_to_receiver_.insert({port, receiver}).second;
+  if (!result) {
+    RTC_LOG(INFO) << "Can't bind receiver to used port " << desired_port
+                  << " in endpoint " << id_;
+    return absl::nullopt;
+  }
+  RTC_LOG(INFO) << "New receiver is binded to endpoint " << id_ << " on port "
+                << port;
+  return port;
+}
+
+uint16_t EndpointNode::NextPort() {
+  uint16_t out = next_port_;
+  if (next_port_ == std::numeric_limits<uint16_t>::max()) {
+    next_port_ = kFirstEphemeralPort;
+  } else {
+    next_port_++;
+  }
+  return out;
+}
+
+void EndpointNode::UnbindReceiver(uint16_t port) {
+  rtc::CritScope crit(&receiver_lock_);
+  port_to_receiver_.erase(port);
+}
+
+rtc::IPAddress EndpointNode::GetPeerLocalAddress() const {
+  return peer_local_addr_;
+}
+
+void EndpointNode::OnPacketReceived(EmulatedIpPacket packet) {
+  RTC_CHECK(packet.dest_endpoint_id == id_)
+      << "Routing error: wrong destination endpoint. Destination id: "
+      << packet.dest_endpoint_id << "; Receiver id: " << id_;
+  rtc::CritScope crit(&receiver_lock_);
+  auto it = port_to_receiver_.find(packet.to.port());
+  if (it == port_to_receiver_.end()) {
+    // It can happen, that remote peer closed connection, but there still some
+    // packets, that are going to it. It can happen during peer connection close
+    // process: one peer closed connection, second still sending data.
+    RTC_LOG(INFO) << "No receiver registered in " << id_ << " on port "
+                  << packet.to.port();
+    return;
+  }
+  // Endpoint assumes frequent calls to bind and unbind methods, so it holds
+  // lock during packet processing to ensure that receiver won't be deleted
+  // before call to OnPacketReceived.
+  it->second->OnPacketReceived(std::move(packet));
+}
+
+EmulatedNetworkNode* EndpointNode::GetSendNode() const {
+  return send_node_;
+}
+
+void EndpointNode::SetConnectedEndpointId(uint64_t endpoint_id) {
+  connected_endpoint_id_ = endpoint_id;
 }
 
 }  // namespace test
--- test/scenario/network/network_emulation.h
+++ test/scenario/network/network_emulation.h
@@ -23,8 +23,10 @@
 #include "api/units/timestamp.h"
 #include "rtc_base/async_socket.h"
 #include "rtc_base/copy_on_write_buffer.h"
+#include "rtc_base/critical_section.h"
 #include "rtc_base/socket_address.h"
 #include "rtc_base/thread.h"
+#include "system_wrappers/include/clock.h"
 
 namespace webrtc {
 namespace test {
@@ -36,7 +38,6 @@ struct EmulatedIpPacket {
                    uint64_t dest_endpoint_id,
                    rtc::CopyOnWriteBuffer data,
                    Timestamp arrival_time);
-
   ~EmulatedIpPacket();
   // This object is not copyable or assignable.
   EmulatedIpPacket(const EmulatedIpPacket&) = delete;
@@ -105,6 +106,72 @@ class EmulatedNetworkNode : public EmulatedNetworkReceiverInterface {
   std::deque<StoredPacket> packets_ RTC_GUARDED_BY(lock_);
 
   uint64_t next_packet_id_ RTC_GUARDED_BY(lock_) = 1;
+};
+
+// Represents single network interface on the device.
+// It will be used as sender from socket side to send data to the network and
+// will act as packet receiver from emulated network side to receive packets
+// from other EmulatedNetworkNodes.
+class EndpointNode : public EmulatedNetworkReceiverInterface {
+ public:
+  EndpointNode(uint64_t id, rtc::IPAddress, Clock* clock);
+  ~EndpointNode() override;
+
+  uint64_t GetId() const;
+
+  // Set network node, that will be used to send packets to the network.
+  void SetSendNode(EmulatedNetworkNode* send_node);
+  // Send packet into network.
+  // |from| will be used to set source address for the packet in destination
+  // socket.
+  // |to| will be used for routing verification and picking right socket by port
+  // on destination endpoint.
+  void SendPacket(const rtc::SocketAddress& from,
+                  const rtc::SocketAddress& to,
+                  rtc::CopyOnWriteBuffer packet);
+
+  // Binds receiver to this endpoint to send and receive data.
+  // |desired_port| is a port that should be used. If it is equal to 0,
+  // endpoint will pick the first available port starting from
+  // |kFirstEphemeralPort|.
+  //
+  // Returns the port, that should be used (it will be equals to desired, if
+  // |desired_port| != 0 and is free or will be the one, selected by endpoint)
+  // or absl::nullopt if desired_port in used. Also fails if there are no more
+  // free ports to bind to.
+  absl::optional<uint16_t> BindReceiver(
+      uint16_t desired_port,
+      EmulatedNetworkReceiverInterface* receiver);
+  void UnbindReceiver(uint16_t port);
+
+  rtc::IPAddress GetPeerLocalAddress() const;
+
+  // Will be called to deliver packet into endpoint from network node.
+  void OnPacketReceived(EmulatedIpPacket packet) override;
+
+ protected:
+  friend class NetworkEmulationManager;
+
+  EmulatedNetworkNode* GetSendNode() const;
+  void SetConnectedEndpointId(uint64_t endpoint_id);
+
+ private:
+  static constexpr uint16_t kFirstEphemeralPort = 49152;
+  uint16_t NextPort() RTC_EXCLUSIVE_LOCKS_REQUIRED(receiver_lock_);
+
+  rtc::CriticalSection receiver_lock_;
+
+  uint64_t id_;
+  // Peer's local IP address for this endpoint network interface.
+  const rtc::IPAddress peer_local_addr_;
+  EmulatedNetworkNode* send_node_;
+  Clock* const clock_;
+
+  uint16_t next_port_ RTC_GUARDED_BY(receiver_lock_);
+  std::map<uint16_t, EmulatedNetworkReceiverInterface*> port_to_receiver_
+      RTC_GUARDED_BY(receiver_lock_);
+
+  absl::optional<uint64_t> connected_endpoint_id_;
 };
 
 }  // namespace test
