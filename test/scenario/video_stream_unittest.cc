d37307c56132d4a6c65ad2907ec9c9fa703cf9a9
Sebastian Jansson <srte@webrtc.org>
2019-02-22 17:07:49 +0100

    Reland "Adds resource path support for video files in scenario tests."
    This is a reland of 8306a733f0dc45f19462268e29c90ada9f46b28e
    
    Original change's description:
    > Adds resource path support for video files in scenario tests.
    > 
    > Bug: webrtc:9510
    > Change-Id: Id41a32325cc5b16b119e62fba483cec88f52975b
    > Reviewed-on: https://webrtc-review.googlesource.com/c/123189
    > Commit-Queue: Sebastian Jansson <srte@webrtc.org>
    > Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#26804}
    
    Bug: webrtc:9510
    Change-Id: I97a5568063569ca66d87f28204200a582d01e2e1
    Reviewed-on: https://webrtc-review.googlesource.com/c/123960
    Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Commit-Queue: Sebastian Jansson <srte@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#26836}

--- test/scenario/video_stream_unittest.cc
+++ test/scenario/video_stream_unittest.cc
@@ -15,11 +15,61 @@
 namespace webrtc {
 namespace test {
 namespace {
+using Capture = VideoStreamConfig::Source::Capture;
+using ContentType = VideoStreamConfig::Encoder::ContentType;
 using Codec = VideoStreamConfig::Encoder::Codec;
 using CodecImpl = VideoStreamConfig::Encoder::Implementation;
 }  // namespace
 
-// TODO(srte): Enable after landing fix causing flakiness.
+// TODO(srte): Enable this after resolving flakiness issues.
+TEST(VideoStreamTest, DISABLED_ReceivesFramesFromFileBasedStreams) {
+  TimeDelta kRunTime = TimeDelta::ms(500);
+  std::vector<int> kFrameRates = {15, 30};
+  std::deque<std::atomic<int>> frame_counts(2);
+  frame_counts[0] = 0;
+  frame_counts[1] = 0;
+  {
+    Scenario s;
+    auto route = s.CreateRoutes(s.CreateClient("caller", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())},
+                                s.CreateClient("callee", CallClientConfig()),
+                                {s.CreateSimulationNode(NetworkNodeConfig())});
+
+    s.CreateVideoStream(route->forward(), [&](VideoStreamConfig* c) {
+      c->analyzer.frame_quality_handler = [&](const VideoFrameQualityInfo&) {
+        frame_counts[0]++;
+      };
+      c->source.capture = Capture::kVideoFile;
+      c->source.video_file.name = "foreman_cif";
+      c->source.video_file.width = 352;
+      c->source.video_file.height = 288;
+      c->source.framerate = kFrameRates[0];
+      c->encoder.implementation = CodecImpl::kSoftware;
+      c->encoder.codec = Codec::kVideoCodecVP8;
+    });
+    s.CreateVideoStream(route->forward(), [&](VideoStreamConfig* c) {
+      c->analyzer.frame_quality_handler = [&](const VideoFrameQualityInfo&) {
+        frame_counts[1]++;
+      };
+      c->source.capture = Capture::kImageSlides;
+      c->source.slides.images.crop.width = 320;
+      c->source.slides.images.crop.height = 240;
+      c->source.framerate = kFrameRates[1];
+      c->encoder.implementation = CodecImpl::kSoftware;
+      c->encoder.codec = Codec::kVideoCodecVP9;
+    });
+    s.RunFor(kRunTime);
+  }
+  std::vector<int> expected_counts;
+  for (int fps : kFrameRates)
+    expected_counts.push_back(
+        static_cast<int>(kRunTime.seconds<double>() * fps * 0.8));
+
+  EXPECT_GE(frame_counts[0], expected_counts[0]);
+  EXPECT_GE(frame_counts[1], expected_counts[1]);
+}
+
+// TODO(srte): Enable this after resolving flakiness issues.
 TEST(VideoStreamTest, DISABLED_RecievesVp8SimulcastFrames) {
   TimeDelta kRunTime = TimeDelta::ms(500);
   int kFrameRate = 30;
