16289f64777c8a49b5c00deb642b3f0a8dce831f
Mirko Bonadei <mbonadei@webrtc.org>
2019-03-18 18:53:53 +0100

    Remove NTDDI_WIN10_RS2 check.
    This check is already present here [1] so when WebRTC will be built
    together with Chromium, NTDDI_WIN10_RS2 have to be defined even if WebRTC
    doesn't check for it.
    
    [1] - https://cs.chromium.org/chromium/src/base/win/windows_version.cc?l=24&rcl=091d55df46abb677d565d084064f837f819e92d5
    
    Bug: None
    Change-Id: I8bbda7b412b5904418f0e400f231dd816e6cbda3
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/128500
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#27199}

--- rtc_base/win/windows_version.cc
+++ rtc_base/win/windows_version.cc
@@ -20,14 +20,6 @@
 #error VS 2017 Update 3.2 or higher is required
 #endif
 
-#if !defined(NTDDI_WIN10_RS2)
-// Windows 10 Creators Update SDK is required to build Chrome. It is important
-// to install the 10.0.15063.468 version, released June 2017, because earlier
-// versions had bugs and could not build Chrome. See this link for details:
-// https://developercommunity.visualstudio.com/content/problem/42961/15063-sdk-is-broken-bitsh-indirectly-references-no.html
-#error Creators Update SDK (10.0.15063.468) required.
-#endif
-
 #if !defined(WINUWP)
 
 namespace {
