61e27535006615c97481029f5243b029b7819e19
Niels Möller <nisse@webrtc.org>
2019-04-08 12:57:16 +0200

    Delete placeholder code for Windows Media Foundation capturer.
    Any new capture implementation for windows should use VideoSourceInterface,
    like current capturers for android, mac and ios.
    
    Bug: None
    Change-Id: I0dffa9fde5ba069aab6273bfef826d17f03877d2
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/131337
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Commit-Queue: Tommi <tommi@webrtc.org>
    Reviewed-by: Tommi <tommi@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#27483}

--- modules/video_capture/windows/video_capture_factory_windows.cc
+++ modules/video_capture/windows/video_capture_factory_windows.cc
@@ -10,8 +10,6 @@
 
 #include "api/scoped_refptr.h"
 #include "modules/video_capture/windows/video_capture_ds.h"
-#include "modules/video_capture/windows/video_capture_mf.h"
-#include "rtc_base/ref_count.h"
 #include "rtc_base/ref_counted_object.h"
 
 namespace webrtc {
