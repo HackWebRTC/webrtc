28de7cfd643ca33d867ed7f5da5df1cbf51f2392
Danil Chapovalov <danilchap@webrtc.org>
2019-04-12 19:25:06 +0200

    Use DefaultTaskQueueFactory instead of Global in test RealTimeController
    Bug: webrtc:10284
    Change-Id: I1c285078a9aae42ca811144f42066980a7b155fd
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/132782
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#27672}

--- test/time_controller/real_time_controller.cc
+++ test/time_controller/real_time_controller.cc
@@ -9,19 +9,20 @@
  */
 #include "test/time_controller/real_time_controller.h"
 
-#include "api/task_queue/global_task_queue_factory.h"
-#include "rtc_base/event.h"
-#include "rtc_base/task_utils/to_queued_task.h"
+#include "api/task_queue/default_task_queue_factory.h"
 #include "system_wrappers/include/sleep.h"
 
 namespace webrtc {
+
+RealTimeController::RealTimeController()
+    : task_queue_factory_(CreateDefaultTaskQueueFactory()) {}
 
 Clock* RealTimeController::GetClock() {
   return Clock::GetRealTimeClock();
 }
 
 TaskQueueFactory* RealTimeController::GetTaskQueueFactory() {
-  return &GlobalTaskQueueFactory();
+  return task_queue_factory_.get();
 }
 
 std::unique_ptr<ProcessThread> RealTimeController::CreateProcessThread(
--- test/time_controller/real_time_controller.h
+++ test/time_controller/real_time_controller.h
@@ -10,19 +10,29 @@
 #ifndef TEST_TIME_CONTROLLER_REAL_TIME_CONTROLLER_H_
 #define TEST_TIME_CONTROLLER_REAL_TIME_CONTROLLER_H_
 
+#include <functional>
 #include <memory>
 
+#include "api/task_queue/task_queue_factory.h"
+#include "api/units/time_delta.h"
+#include "modules/utility/include/process_thread.h"
+#include "system_wrappers/include/clock.h"
 #include "test/time_controller/time_controller.h"
 
 namespace webrtc {
 class RealTimeController : public TimeController {
  public:
+  RealTimeController();
+
   Clock* GetClock() override;
   TaskQueueFactory* GetTaskQueueFactory() override;
   std::unique_ptr<ProcessThread> CreateProcessThread(
       const char* thread_name) override;
   void Sleep(TimeDelta duration) override;
   void InvokeWithControlledYield(std::function<void()> closure) override;
+
+ private:
+  std::unique_ptr<TaskQueueFactory> task_queue_factory_;
 };
 
 RealTimeController* GlobalRealTimeController();
