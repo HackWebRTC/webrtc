d8bf2d498692fc1a276edaeee2ce1f43475bd70b
Danil Chapovalov <danilchap@webrtc.org>
2019-04-23 12:52:02 +0000

    Revert "Delete deprecated variant of VideoDecoder::Decode"
    This reverts commit 3a86d9520c9692abe4c6d2a73faf4fb7a0cb80b2.
    
    Reason for revert: breaks downstream project
    
    Original change's description:
    > Delete deprecated variant of VideoDecoder::Decode
    > 
    > Bug: webrtc:10379
    > Change-Id: I4dd8b503625a9ea2a71177165238e128ac3e49bb
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/132554
    > Reviewed-by: Sami Kalliomäki <sakal@webrtc.org>
    > Reviewed-by: Rasmus Brandt <brandtr@webrtc.org>
    > Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    > Reviewed-by: Erik Språng <sprang@webrtc.org>
    > Commit-Queue: Niels Moller <nisse@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#27712}
    
    TBR=brandtr@webrtc.org,sakal@webrtc.org,nisse@webrtc.org,kthelgason@webrtc.org,sprang@webrtc.org
    
    Change-Id: Ie971fd821f4de9e4b68e1608d7074835bdf2ed16
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:10379
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/133907
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#27713}

--- api/video_codecs/video_decoder.h
+++ api/video_codecs/video_decoder.h
@@ -22,6 +22,10 @@
 
 namespace webrtc {
 
+// TODO(pbos): Expose these through a public (root) header or change these APIs.
+struct CodecSpecificInfo;
+class VideoCodec;
+
 class RTC_EXPORT DecodedImageCallback {
  public:
   virtual ~DecodedImageCallback() {}
@@ -51,7 +55,14 @@ class RTC_EXPORT VideoDecoder {
 
   virtual int32_t Decode(const EncodedImage& input_image,
                          bool missing_frames,
-                         int64_t render_time_ms) = 0;
+                         int64_t render_time_ms);
+
+  // TODO(bugs.webrtc.org/10379): Deprecated. Delete, and make above method pure
+  // virtual, as soon as downstream applications are updated.
+  virtual int32_t Decode(const EncodedImage& input_image,
+                         bool missing_frames,
+                         const CodecSpecificInfo* codec_specific_info,
+                         int64_t render_time_ms);
 
   virtual int32_t RegisterDecodeCompleteCallback(
       DecodedImageCallback* callback) = 0;
