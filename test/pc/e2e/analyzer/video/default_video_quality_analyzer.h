e680c83a412bad3edbdf4abe14da1d39b9c05506
Artem Titov <titovartem@webrtc.org>
2019-04-25 13:38:49 +0000

    Revert "Add Video Bwe stats collection to DefaultVideoQualityAnalyzer."
    This reverts commit 8848229234aae01ec19582ece7b748d557119d66.
    
    Reason for revert: break chromium compilation on iOS
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8915214519549611184/+/steps/compile/0/stdout
    
    Original change's description:
    > Add Video Bwe stats collection to DefaultVideoQualityAnalyzer.
    > 
    > This CL adds the possibility to collect the following Video BWE stats:
    > - available_send_bandwidth
    > - transmission_bitrate
    > - retransmission_bitrate
    > - actual_encode_bitrate
    > - target_encode_bitrate
    > 
    > Example of the output:
    > 
    > RESULT available_send_bandwidth: smoke_test/alice= {487754.33,87583.093} bytesPerSecond
    > RESULT transmission_bitrate: smoke_test/alice= {465779.17,212075.5} bytesPerSecond
    > RESULT retransmission_bitrate: smoke_test/alice= {20036,26326.751} bytesPerSecond
    > RESULT actual_encode_bitrate: smoke_test/alice= {418779.33,200486.03} bytesPerSecond
    > RESULT target_encode_bitrate: smoke_test/alice= {469491.17,77866.909} bytesPerSecond
    > RESULT available_send_bandwidth: smoke_test/bob= {642924.83,168842.34} bytesPerSecond
    > RESULT transmission_bitrate: smoke_test/bob= {626115.5,294783.56} bytesPerSecond
    > RESULT retransmission_bitrate: smoke_test/bob= {0,0} bytesPerSecond
    > RESULT actual_encode_bitrate: smoke_test/bob= {594235.33,297289.54} bytesPerSecond
    > RESULT target_encode_bitrate: smoke_test/bob= {640463.5,167676.66} bytesPerSecond
    > 
    > Bug: webrtc:10138
    > Change-Id: I0414055af0010b8fb4d909297e6da86d398157c2
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/132703
    > Commit-Queue: Artem Titov <titovartem@webrtc.org>
    > Reviewed-by: Tommi <tommi@webrtc.org>
    > Reviewed-by: Artem Titov <titovartem@webrtc.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@google.com>
    > Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#27760}
    
    TBR=mbonadei@webrtc.org,mbonadei@google.com,ilnik@webrtc.org,tommi@webrtc.org,titovartem@webrtc.org
    
    Change-Id: Ib0ef94331410d9b22b6425e4da412b75360fa2d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: webrtc:10138
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/134210
    Reviewed-by: Artem Titov <titovartem@webrtc.org>
    Commit-Queue: Artem Titov <titovartem@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#27771}

--- test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
+++ test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
@@ -19,7 +19,6 @@
 #include <string>
 #include <vector>
 
-#include "absl/container/flat_hash_map.h"
 #include "api/test/video_quality_analyzer_interface.h"
 #include "api/units/timestamp.h"
 #include "api/video/encoded_image.h"
@@ -69,6 +68,7 @@ struct FrameCounters {
 };
 
 struct StreamStats {
+ public:
   SamplesStatsCounter psnr;
   SamplesStatsCounter ssim;
   // Time from frame encoded (time point on exit from encoder) to the
@@ -100,6 +100,7 @@ struct StreamStats {
 };
 
 struct AnalyzerStats {
+ public:
   // Size of analyzer internal comparisons queue, measured when new element
   // id added to the queue.
   SamplesStatsCounter comparisons_queue_size;
@@ -111,14 +112,6 @@ struct AnalyzerStats {
   // comparison doesn't include metrics, that require heavy computations like
   // SSIM and PSNR.
   int64_t overloaded_comparisons_done = 0;
-};
-
-struct VideoBweStats {
-  SamplesStatsCounter available_send_bandwidth;
-  SamplesStatsCounter transmission_bitrate;
-  SamplesStatsCounter retransmission_bitrate;
-  SamplesStatsCounter actual_encode_bitrate;
-  SamplesStatsCounter target_encode_bitrate;
 };
 
 class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
@@ -155,12 +148,8 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
   std::map<std::string, StreamStats> GetStats() const;
   AnalyzerStats GetAnalyzerStats() const;
 
-  // Will be called everytime new stats reports are available for the
-  // Peer Connection identified by |pc_label|.
-  void OnStatsReports(absl::string_view pc_label,
-                      const StatsReports& stats_reports) override;
-
-  absl::flat_hash_map<std::string, VideoBweStats> GetVideoBweStats() const;
+  // TODO(bugs.webrtc.org/10138): Provide a real implementation for
+  // OnStatsReport.
 
  private:
   struct FrameStats {
@@ -243,11 +232,9 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
   void ProcessComparison(const FrameComparison& comparison);
   // Report results for all metrics for all streams.
   void ReportResults();
-  static void ReportVideoBweResults(const std::string& test_case_name,
-                                    const VideoBweStats& video_bwe_stats);
-  static void ReportResults(const std::string& test_case_name,
-                            const StreamStats& stats,
-                            const FrameCounters& frame_counters);
+  static void ReportResults(std::string test_case_name,
+                            StreamStats stats,
+                            FrameCounters frame_counters);
   // Report result for single metric for specified stream.
   static void ReportResult(const std::string& metric_name,
                            const std::string& test_case_name,
@@ -283,12 +270,6 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
       RTC_GUARDED_BY(comparison_lock_);
   std::deque<FrameComparison> comparisons_ RTC_GUARDED_BY(comparison_lock_);
   AnalyzerStats analyzer_stats_ RTC_GUARDED_BY(comparison_lock_);
-
-  rtc::CriticalSection video_bwe_stats_lock_;
-  // Map between a peer connection label (provided by the framework) and
-  // its video BWE stats.
-  absl::flat_hash_map<std::string, VideoBweStats> video_bwe_stats_
-      RTC_GUARDED_BY(video_bwe_stats_lock_);
 
   std::vector<std::unique_ptr<rtc::PlatformThread>> thread_pool_;
   rtc::Event comparison_available_event_;
