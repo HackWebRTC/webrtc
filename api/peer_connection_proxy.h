74e63b8abbbfdc25df05c90a6f4bc40cead73304
Tim Haloun <thaloun@google.com>
2019-06-04 11:23:32 -0700

    Add missing proxy function for overloaded StartRtcEventLog peer connection function.
    TBR=kwiberg@webrtc.org
    
    Bug: webrtc:6463, webrtc:10716
    Change-Id: I1cdfb87e30a9aef5ecc297339721397591542646
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/140164
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Reviewed-by: Niels Moller <nisse@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#28164}

--- api/peer_connection_proxy.h
+++ api/peer_connection_proxy.h
@@ -135,6 +135,7 @@ PROXY_METHOD2(bool,
               StartRtcEventLog,
               std::unique_ptr<RtcEventLogOutput>,
               int64_t)
+PROXY_METHOD1(bool, StartRtcEventLog, std::unique_ptr<RtcEventLogOutput>)
 PROXY_METHOD0(void, StopRtcEventLog)
 PROXY_METHOD0(void, Close)
 END_PROXY_MAP()
