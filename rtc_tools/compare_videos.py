f97f342fd352633ef1b2ced5cf1b340f994be515
Mirko Bonadei <mbonadei@webrtc.org>
2019-06-25 15:00:54 +0200

    Remove deprecated flags from compare_videos.py.
    Bug: None
    Change-Id: If581ce5b949dab8143b35ad4cbfb7fadf6f4c83e
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/143788
    Reviewed-by: Magnus Jedvert <magjed@webrtc.org>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#28377}

--- rtc_tools/compare_videos.py
+++ rtc_tools/compare_videos.py
@@ -45,18 +45,6 @@ def _ParseArgs():
                     help='Path to VMAF model.')
   parser.add_option('--vmaf_phone_model', action='store_true',
                     help='Whether to use phone model in VMAF.')
-  parser.add_option('--barcode_decoder', type='string',
-                    help=('DEPRECATED'))
-  parser.add_option('--ffmpeg_path', type='string',
-                    help=('DEPRECATED'))
-  parser.add_option('--zxing_path', type='string',
-                    help=('DEPRECATED'))
-  parser.add_option('--stats_file_ref', type='string', default='stats_ref.txt',
-                    help=('DEPRECATED'))
-  parser.add_option('--stats_file_test', type='string',
-                    help=('DEPRECATED'))
-  parser.add_option('--stats_file', type='string',
-                    help=('DEPRECATED'))
   parser.add_option('--yuv_frame_width', type='int', default=640,
                     help='Width of the YUV file\'s frames. Default: %default')
   parser.add_option('--yuv_frame_height', type='int', default=480,
