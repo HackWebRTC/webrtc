e8df48220589004f434c5aa99090d992b590290d
Elad Alon <eladalon@webrtc.org>
2019-06-26 11:51:04 +0200

    Clean-up after CL #140941
    Bug: webrtc:10720
    Change-Id: Ie41083ea89be23e183eaff6ac1bdf661160b70cb
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/140942
    Commit-Queue: Elad Alon <eladalon@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#28388}

--- api/video_codecs/vp8_frame_buffer_controller.h
+++ api/video_codecs/vp8_frame_buffer_controller.h
@@ -177,23 +177,12 @@ class Vp8FrameBufferControllerFactory {
   virtual ~Vp8FrameBufferControllerFactory() = default;
 
   // Clones oneself. (Avoids Vp8FrameBufferControllerFactoryFactory.)
-  // TODO(bugs.webrtc.org/10720): Update downstream and make pure-virtual.
-  virtual std::unique_ptr<Vp8FrameBufferControllerFactory> Clone() const {
-    return nullptr;
-  }
+  virtual std::unique_ptr<Vp8FrameBufferControllerFactory> Clone() const = 0;
 
-  // TODO(bugs.webrtc.org/10720): Update downstream and remove.
-  virtual std::unique_ptr<Vp8FrameBufferController> Create(
-      const VideoCodec& codec) {
-    return nullptr;
-  }
-
-  // TODO(bugs.webrtc.org/10720): Update downstream and make pure-virtual.
+  // Create a Vp8FrameBufferController instance.
   virtual std::unique_ptr<Vp8FrameBufferController> Create(
       const VideoCodec& codec,
-      const VideoEncoder::Settings& settings) {
-    return nullptr;
-  }
+      const VideoEncoder::Settings& settings) = 0;
 };
 
 }  // namespace webrtc
