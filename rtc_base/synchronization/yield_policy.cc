fefa77c42687417bf18405ed66ecffa0e978a943
Steve Anton <steveanton@webrtc.org>
2019-07-19 07:03:03 -0700

    Add pthread thread-local storage support for ScopedYieldPolicy
    Emscripten does not support C++11 thread_local but does support
    the pthread TLS API.
    
    Bug: None
    Change-Id: Ia21895148d1df7652579d086d9e1c0c53d7a85f4
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/145441
    Commit-Queue: Steve Anton <steveanton@webrtc.org>
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#28621}

--- rtc_base/synchronization/yield_policy.cc
+++ rtc_base/synchronization/yield_policy.cc
@@ -10,23 +10,73 @@
 #include "rtc_base/synchronization/yield_policy.h"
 
 #include "absl/base/attributes.h"
+#include "absl/base/config.h"
+#include "rtc_base/checks.h"
+#if !defined(ABSL_HAVE_THREAD_LOCAL) && defined(WEBRTC_POSIX)
+#include <pthread.h>
+#endif
 
 namespace rtc {
 namespace {
+
+#if defined(ABSL_HAVE_THREAD_LOCAL)
+
 ABSL_CONST_INIT thread_local YieldInterface* current_yield_policy = nullptr;
+
+YieldInterface* GetCurrentYieldPolicy() {
+  return current_yield_policy;
 }
 
+void SetCurrentYieldPolicy(YieldInterface* ptr) {
+  current_yield_policy = ptr;
+}
+
+#elif defined(WEBRTC_POSIX)
+
+// Emscripten does not support the C++11 thread_local keyword but does support
+// the pthread thread-local storage API.
+// https://github.com/emscripten-core/emscripten/issues/3502
+
+ABSL_CONST_INIT pthread_key_t g_current_yield_policy_tls = 0;
+
+void InitializeTls() {
+  RTC_CHECK_EQ(pthread_key_create(&g_current_yield_policy_tls, nullptr), 0);
+}
+
+pthread_key_t GetCurrentYieldPolicyTls() {
+  static pthread_once_t init_once = PTHREAD_ONCE_INIT;
+  RTC_CHECK_EQ(pthread_once(&init_once, &InitializeTls), 0);
+  return g_current_yield_policy_tls;
+}
+
+YieldInterface* GetCurrentYieldPolicy() {
+  return static_cast<YieldInterface*>(
+      pthread_getspecific(GetCurrentYieldPolicyTls()));
+}
+
+void SetCurrentYieldPolicy(YieldInterface* ptr) {
+  pthread_setspecific(GetCurrentYieldPolicyTls(), ptr);
+}
+
+#else
+#error Unsupported platform
+#endif
+
+}  // namespace
+
 ScopedYieldPolicy::ScopedYieldPolicy(YieldInterface* policy)
-    : previous_(current_yield_policy) {
-  current_yield_policy = policy;
+    : previous_(GetCurrentYieldPolicy()) {
+  SetCurrentYieldPolicy(policy);
 }
 
 ScopedYieldPolicy::~ScopedYieldPolicy() {
-  current_yield_policy = previous_;
+  SetCurrentYieldPolicy(previous_);
 }
 
 void ScopedYieldPolicy::YieldExecution() {
-  if (current_yield_policy)
-    current_yield_policy->YieldExecution();
+  YieldInterface* current = GetCurrentYieldPolicy();
+  if (current)
+    current->YieldExecution();
 }
+
 }  // namespace rtc
