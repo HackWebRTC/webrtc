ada8e17125d2124f5bcdc1558182ce95d6311d93
Niels Möller <nisse@webrtc.org>
2019-08-08 16:29:33 +0200

    Delete mac_utils.h and mac_utils.cc
    They defined two functions: ToUtf16 and ToUtf8. The former was unused,
    and the latter is moved to
    modules/desktop_capture/mac/window_list_utils.cc, the only user.
    
    Tbr: sergeyu@chromium.org
    Bug: None
    Change-Id: Ib8a513da42e43ba8d41a2de4c1645b3f48448dc9
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/148531
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Reviewed-by: Sergey Ulanov <sergeyu@google.com>
    Cr-Commit-Position: refs/heads/master@{#28913}

--- modules/desktop_capture/mouse_cursor_monitor_mac.mm
+++ modules/desktop_capture/mouse_cursor_monitor_mac.mm
@@ -27,7 +27,6 @@
 #include "modules/desktop_capture/mac/full_screen_chrome_window_detector.h"
 #include "modules/desktop_capture/mac/window_list_utils.h"
 #include "modules/desktop_capture/mouse_cursor.h"
-#include "rtc_base/mac_utils.h"
 
 namespace webrtc {
 
