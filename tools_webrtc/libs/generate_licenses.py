10b636138d77e7d89497d5f7fde848a9bc33c648
Oleksandr Iakovenko <iakovenko@webrtc.org>
2019-08-28 09:41:26 +0200

    Add license for android_ndk
    Bug: webrtc:10923
    Change-Id: Ic2f40329964d0d3f65bd85e7d00ab1e49fb71123
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/150643
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#28990}

--- tools_webrtc/libs/generate_licenses.py
+++ tools_webrtc/libs/generate_licenses.py
@@ -34,6 +34,7 @@ import subprocess
 # (right after the `third_party/` prefix)
 LIB_TO_LICENSES_DICT = {
     'abseil-cpp': ['third_party/abseil-cpp/LICENSE'],
+    'android_ndk': ['third_party/android_ndk/NOTICE'],
     'android_sdk': ['third_party/android_sdk/LICENSE'],
     'auto': ['third_party/auto/src/LICENSE.txt'],
     'bazel': ['third_party/bazel/LICENSE'],
