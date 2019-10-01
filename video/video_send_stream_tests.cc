24d251f796b4a64e4fb457ad1170410e10cb892b
Björn Terelius <terelius@webrtc.org>
2019-10-02 00:37:36 +0200

    Add 100 ms network delay to the SupportsFlexFEC* tests.
    Some of the tests are currently flaky because FEC is disabled if the
    RTT is <200 ms, and the simulated network is configured to use 100 ms
    for the send transport, but nothing is configured for the receive
    transport. This CL configures the receive transport to 100 ms delay.
    
    Bug: webrtc:10920
    Change-Id: I79995693ba73683406fa9ced92a7918e6c05473f
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/154571
    Commit-Queue: Björn Terelius <terelius@webrtc.org>
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Reviewed-by: Rasmus Brandt <brandtr@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#29394}

--- video/video_send_stream_tests.cc
+++ video/video_send_stream_tests.cc
@@ -769,6 +769,21 @@ class FlexfecObserver : public test::EndToEndTest {
             std::make_unique<SimulatedNetwork>(config)));
   }
 
+  std::unique_ptr<test::PacketTransport> CreateReceiveTransport(
+      TaskQueueBase* task_queue) override {
+    // We need the RTT to be >200 ms to send FEC and the network delay for the
+    // send transport is 100 ms, so add 100 ms (but no loss) on the return link.
+    BuiltInNetworkBehaviorConfig config;
+    config.loss_percent = 0;
+    config.queue_delay_ms = 100;
+    return std::make_unique<test::PacketTransport>(
+        task_queue, nullptr, this, test::PacketTransport::kReceiver,
+        VideoSendStreamTest::payload_type_map_,
+        std::make_unique<FakeNetworkPipe>(
+            Clock::GetRealTimeClock(),
+            std::make_unique<SimulatedNetwork>(config)));
+  }
+
   void ModifyVideoConfigs(
       VideoSendStream::Config* send_config,
       std::vector<VideoReceiveStream::Config>* receive_configs,
