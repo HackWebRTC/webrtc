d6b9b0a1f4132474c737b5e673e380c3d8e12e2c
Danil Chapovalov <danilchap@webrtc.org>
2019-11-08 09:40:26 +0100

    Cleanup MultiStreamTester
    Instead of taking TaskQueue from outside create one internally.
    Detach MultiStreamTests from test::CallTest since that inheritance
    only used for constants and (now unneeded) task_queue object.
    
    Bug: webrtc:10933
    Change-Id: I7e30ddcf6faaa134ebcd9d53b578b40fdedf2a3c
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/159034
    Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#29744}

--- video/end_to_end_tests/multi_stream_tester.cc
+++ video/end_to_end_tests/multi_stream_tester.cc
@@ -31,23 +31,20 @@
 
 namespace webrtc {
 
-MultiStreamTester::MultiStreamTester(TaskQueueBase* task_queue)
-    : task_queue_(task_queue) {
+MultiStreamTester::MultiStreamTester() {
   // TODO(sprang): Cleanup when msvc supports explicit initializers for array.
   codec_settings[0] = {1, 640, 480};
   codec_settings[1] = {2, 320, 240};
   codec_settings[2] = {3, 240, 160};
-  class multi_stream_test {
-   public:
-    multi_stream_test();
-  };
 }
 
-MultiStreamTester::~MultiStreamTester() {}
+MultiStreamTester::~MultiStreamTester() = default;
 
 void MultiStreamTester::RunTest() {
   webrtc::RtcEventLogNull event_log;
   auto task_queue_factory = CreateDefaultTaskQueueFactory();
+  auto task_queue = task_queue_factory->CreateTaskQueue(
+      "TaskQueue", TaskQueueFactory::Priority::NORMAL);
   Call::Config config(&event_log);
   config.task_queue_factory = task_queue_factory.get();
   std::unique_ptr<Call> sender_call;
@@ -64,12 +61,12 @@ void MultiStreamTester::RunTest() {
       CreateBuiltinVideoBitrateAllocatorFactory();
   InternalDecoderFactory decoder_factory;
 
-  SendTask(RTC_FROM_HERE, task_queue_, [&]() {
+  SendTask(RTC_FROM_HERE, task_queue.get(), [&]() {
     sender_call = absl::WrapUnique(Call::Create(config));
     receiver_call = absl::WrapUnique(Call::Create(config));
-    sender_transport = CreateSendTransport(task_queue_, sender_call.get());
+    sender_transport = CreateSendTransport(task_queue.get(), sender_call.get());
     receiver_transport =
-        CreateReceiveTransport(task_queue_, receiver_call.get());
+        CreateReceiveTransport(task_queue.get(), receiver_call.get());
 
     sender_transport->SetReceiver(receiver_call->Receiver());
     receiver_transport->SetReceiver(sender_call->Receiver());
@@ -125,7 +122,7 @@ void MultiStreamTester::RunTest() {
 
   Wait();
 
-  SendTask(RTC_FROM_HERE, task_queue_, [&]() {
+  SendTask(RTC_FROM_HERE, task_queue.get(), [&]() {
     for (size_t i = 0; i < kNumStreams; ++i) {
       frame_generators[i]->Stop();
       sender_call->DestroyVideoSendStream(send_streams[i]);
