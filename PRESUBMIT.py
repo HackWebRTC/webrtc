0b3a6e383e17c80e5f3adce826cce7bc202a01cf
Joe Chen <jsphchn@google.com>
2019-12-26 23:01:42 -0800

    Make RTCAudioSession accessible to Swift.
    This is done by:
    1. removing the <vector> include from RTCAudioSession+Private,
    2. creating a audio_session_objc package that excludes the RTCNativeAudioSessionDelegateAdapter class.
    
    Bug: webrtc:11237
    Change-Id: I36c86542a19e3244456fd164d908563b1435de29
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/163900
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Reviewed-by: Chuck Hays <haysc@webrtc.org>
    Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
    Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#30177}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -521,7 +521,7 @@ def CheckPublicDepsIsNotUsed(gn_files, input_api, output_api):
   """Checks that public_deps is not used without a good reason."""
   result = []
   no_presubmit_check_re = input_api.re.compile(
-      r'# no-presubmit-check TODO\(webrtc:8603\)')
+      r'# no-presubmit-check TODO\(webrtc:\d+\)')
   error_msg = ('public_deps is not recommended in WebRTC BUILD.gn files '
                'because it doesn\'t map well to downstream build systems.\n'
                'Used in: %s (line %d).\n'
