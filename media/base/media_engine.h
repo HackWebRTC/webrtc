c0f25cf762a6946666c812f7a3df3f0a7f98b38d
Johannes Kron <kron@webrtc.org>
2020-01-16 15:00:56 +0100

    Distinguish between send and receive codecs
    Even though send and receive codecs may be the same, they might have
    different support in HW. Distinguish between send and receive codecs
    to be able to keep track of which codecs have HW support.
    
    Bug: chromium:1029737
    Change-Id: Id119560becadfe0aaf861c892a6485f1c2eb378d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/165763
    Commit-Queue: Johannes Kron <kron@webrtc.org>
    Reviewed-by: Steve Anton <steveanton@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#30284}

--- media/base/media_engine.h
+++ media/base/media_engine.h
@@ -99,7 +99,9 @@ class VideoEngineInterface {
       webrtc::VideoBitrateAllocatorFactory*
           video_bitrate_allocator_factory) = 0;
 
-  virtual std::vector<VideoCodec> codecs() const = 0;
+  virtual std::vector<VideoCodec> send_codecs() const = 0;
+  virtual std::vector<VideoCodec> recv_codecs() const = 0;
+
   virtual RtpCapabilities GetCapabilities() const = 0;
 };
 
--- pc/media_session.h
+++ pc/media_session.h
@@ -151,8 +151,11 @@ class MediaSessionDescriptionFactory {
     audio_rtp_extensions_ = extensions;
   }
   RtpHeaderExtensions audio_rtp_header_extensions() const;
-  const VideoCodecs& video_codecs() const { return video_codecs_; }
-  void set_video_codecs(const VideoCodecs& codecs) { video_codecs_ = codecs; }
+  const VideoCodecs& video_sendrecv_codecs() const;
+  const VideoCodecs& video_send_codecs() const;
+  const VideoCodecs& video_recv_codecs() const;
+  void set_video_codecs(const VideoCodecs& send_codecs,
+                        const VideoCodecs& recv_codecs);
   void set_video_rtp_header_extensions(const RtpHeaderExtensions& extensions) {
     video_rtp_extensions_ = extensions;
   }
@@ -184,6 +187,11 @@ class MediaSessionDescriptionFactory {
   const AudioCodecs& GetAudioCodecsForOffer(
       const webrtc::RtpTransceiverDirection& direction) const;
   const AudioCodecs& GetAudioCodecsForAnswer(
+      const webrtc::RtpTransceiverDirection& offer,
+      const webrtc::RtpTransceiverDirection& answer) const;
+  const VideoCodecs& GetVideoCodecsForOffer(
+      const webrtc::RtpTransceiverDirection& direction) const;
+  const VideoCodecs& GetVideoCodecsForAnswer(
       const webrtc::RtpTransceiverDirection& offer,
       const webrtc::RtpTransceiverDirection& answer) const;
   void GetCodecsForOffer(
@@ -317,6 +325,8 @@ class MediaSessionDescriptionFactory {
 
   void ComputeAudioCodecsIntersectionAndUnion();
 
+  void ComputeVideoCodecsIntersectionAndUnion();
+
   bool is_unified_plan_ = false;
   AudioCodecs audio_send_codecs_;
   AudioCodecs audio_recv_codecs_;
@@ -325,7 +335,12 @@ class MediaSessionDescriptionFactory {
   // Union of send and recv.
   AudioCodecs all_audio_codecs_;
   RtpHeaderExtensions audio_rtp_extensions_;
-  VideoCodecs video_codecs_;
+  VideoCodecs video_send_codecs_;
+  VideoCodecs video_recv_codecs_;
+  // Intersection of send and recv.
+  VideoCodecs video_sendrecv_codecs_;
+  // Union of send and recv.
+  VideoCodecs all_video_codecs_;
   RtpHeaderExtensions video_rtp_extensions_;
   RtpDataCodecs rtp_data_codecs_;
   // This object is not owned by the channel so it must outlive it.
