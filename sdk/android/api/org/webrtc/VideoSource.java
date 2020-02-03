0f6bcd18b2ef109850c6430fa0d474688571da5f
Sami Kalliomäki <sakal@webrtc.org>
2020-02-03 16:09:45 +0100

    Hold a reference to AndroidVideoTrackSource while calling onFrameCaptured.
    This makes it safe to deliver frames to the sink from VideoProcessor
    even after setSink has been called with null reference without danger
    of use after free.
    
    Bug: b/148063550
    Change-Id: Ib78f75ac49fc6117f744c55da1a4e671bbdcdf22
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/168160
    Reviewed-by: Paulina Hensman <phensman@webrtc.org>
    Commit-Queue: Sami Kalliomäki <sakal@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#30455}

--- sdk/android/api/org/webrtc/VideoSource.java
+++ sdk/android/api/org/webrtc/VideoSource.java
@@ -135,7 +135,9 @@ public class VideoSource extends MediaSource {
       }
       videoProcessor = newVideoProcessor;
       if (newVideoProcessor != null) {
-        newVideoProcessor.setSink(nativeAndroidVideoTrackSource::onFrameCaptured);
+        newVideoProcessor.setSink(
+            (frame)
+                -> runWithReference(() -> nativeAndroidVideoTrackSource.onFrameCaptured(frame)));
         if (isCapturerRunning) {
           newVideoProcessor.onCapturerStarted(/* success= */ true);
         }
