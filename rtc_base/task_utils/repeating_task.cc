29a5fe8a4feb1eb1ae44278dbfff52e2469d0f30
Tommi <tommi@webrtc.org>
2020-05-15 10:12:36 +0200

    Reland "Revert back to using the task_queue_ for guarding access."
    This reverts commit af1b9ceb62dce3462083f9a44e26ee6d79639cef.
    
    Reason for revert: Speculative reland after looking into downstream
    failures. It's possible that carryover state from unrelated tests
    running in parallel was causing failures.
    
    Original change's description:
    > Revert "Revert back to using the task_queue_ for guarding access."
    > 
    > This reverts commit 475006d4a30f8bc47f82eb540a6a066da2829095.
    > 
    > Reason for revert: Speculative revert. Breaks downstream project
    > 
    > Original change's description:
    > > Revert back to using the task_queue_ for guarding access.
    > > 
    > > This removes the SequenceChecker that was temporarily used while
    > > the rtc::Thread TQ implementation was being fixed.
    > > 
    > > Bug: none
    > > Change-Id: Iaa46e47371211ac0a97b2dcaf23cef12b43ee8ea
    > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/175081
    > > Commit-Queue: Tommi <tommi@webrtc.org>
    > > Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    > > Cr-Commit-Position: refs/heads/master@{#31256}
    > 
    > TBR=tommi@webrtc.org,srte@webrtc.org
    > 
    > Change-Id: I17a12bdca888a63f2fd161da30c0def5b9c3d04e
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: none
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/175103
    > Reviewed-by: Artem Titov <titovartem@webrtc.org>
    > Commit-Queue: Artem Titov <titovartem@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#31258}
    
    TBR=tommi@webrtc.org,srte@webrtc.org,titovartem@webrtc.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: none
    Change-Id: I23992643126d7d6dae63da1bb14420b2b8794fd9
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/175135
    Reviewed-by: Tommi <tommi@webrtc.org>
    Reviewed-by: Sebastian Jansson <srte@webrtc.org>
    Reviewed-by: Artem Titov <titovartem@webrtc.org>
    Commit-Queue: Tommi <tommi@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31283}

--- rtc_base/task_utils/repeating_task.cc
+++ rtc_base/task_utils/repeating_task.cc
@@ -21,13 +21,12 @@ RepeatingTaskBase::RepeatingTaskBase(TaskQueueBase* task_queue,
                                      TimeDelta first_delay)
     : task_queue_(task_queue),
       next_run_time_(Timestamp::Micros(rtc::TimeMicros()) + first_delay) {
-  sequence_checker_.Detach();
 }
 
 RepeatingTaskBase::~RepeatingTaskBase() = default;
 
 bool RepeatingTaskBase::Run() {
-  RTC_DCHECK_RUN_ON(&sequence_checker_);
+  RTC_DCHECK_RUN_ON(task_queue_);
   // Return true to tell the TaskQueue to destruct this object.
   if (next_run_time_.IsPlusInfinity())
     return true;
@@ -53,7 +52,7 @@ bool RepeatingTaskBase::Run() {
 }
 
 void RepeatingTaskBase::Stop() {
-  RTC_DCHECK_RUN_ON(&sequence_checker_);
+  RTC_DCHECK_RUN_ON(task_queue_);
   RTC_DCHECK(next_run_time_.IsFinite());
   next_run_time_ = Timestamp::PlusInfinity();
 }
