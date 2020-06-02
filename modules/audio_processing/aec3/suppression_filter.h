43ccfecdb7681f8bfc160b70462f70c532a62269
Per Åhgren <peah@webrtc.org>
2020-06-02 23:00:36 +0200

    Remove unused instance of Ooura FFT
    Bug: b/155316201
    Change-Id: I29a2dcf620091665b6d48820022fac3a3b672c04
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/176405
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Commit-Queue: Per Åhgren <peah@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31414}

--- modules/audio_processing/aec3/suppression_filter.h
+++ modules/audio_processing/aec3/suppression_filter.h
@@ -14,7 +14,6 @@
 #include <array>
 #include <vector>
 
-#include "common_audio/third_party/ooura/fft_size_128/ooura_fft.h"
 #include "modules/audio_processing/aec3/aec3_common.h"
 #include "modules/audio_processing/aec3/aec3_fft.h"
 #include "modules/audio_processing/aec3/fft_data.h"
@@ -39,7 +38,6 @@ class SuppressionFilter {
   const Aec3Optimization optimization_;
   const int sample_rate_hz_;
   const size_t num_capture_channels_;
-  const OouraFft ooura_fft_;
   const Aec3Fft fft_;
   std::vector<std::vector<std::array<float, kFftLengthBy2>>> e_output_old_;
   RTC_DISALLOW_COPY_AND_ASSIGN(SuppressionFilter);
