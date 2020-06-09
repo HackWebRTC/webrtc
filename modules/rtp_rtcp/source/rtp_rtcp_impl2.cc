8d4d671e9be98a090921006cc988cc1fc4fe1947
Tomas Gunnarsson <tommi@webrtc.org>
2020-06-09 07:39:02 +0200

    Remove dead code from ModuleRtpRtcpImpl2
    Bug: webrtc:11581
    Change-Id: I70e1df2724801c1bc7fb5cfb52cb6d1d44920b0d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/176570
    Commit-Queue: Tommi <tommi@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31467}

--- modules/rtp_rtcp/source/rtp_rtcp_impl2.cc
+++ modules/rtp_rtcp/source/rtp_rtcp_impl2.cc
@@ -812,15 +812,4 @@ const RTPSender* ModuleRtpRtcpImpl2::RtpSender() const {
   return rtp_sender_ ? &rtp_sender_->packet_generator : nullptr;
 }
 
-DataRate ModuleRtpRtcpImpl2::SendRate() const {
-  RTC_DCHECK(rtp_sender_);
-  return rtp_sender_->packet_sender.GetSendRates().Sum();
-}
-
-DataRate ModuleRtpRtcpImpl2::NackOverheadRate() const {
-  RTC_DCHECK(rtp_sender_);
-  return rtp_sender_->packet_sender
-      .GetSendRates()[RtpPacketMediaType::kRetransmission];
-}
-
 }  // namespace webrtc
