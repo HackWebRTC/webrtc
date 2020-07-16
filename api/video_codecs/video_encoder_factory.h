3592839896082c72c8e92f9ebde788ef77b88043
Niels Möller <nisse@webrtc.org>
2020-07-16 12:08:44 +0200

    Add default values for VideoEncoderFactory::CodecInfo
    So that applications can construct a default struct without naming the
    members.
    
    Bug: None
    Change-Id: Idd9028bee9016670e776f17a62077eb9c34d6f2a
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/179485
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Reviewed-by: Mirta Dvornicic <mirtad@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31742}

--- api/video_codecs/video_encoder_factory.h
+++ api/video_codecs/video_encoder_factory.h
@@ -30,13 +30,13 @@ class VideoEncoderFactory {
   struct CodecInfo {
     // |is_hardware_accelerated| is true if the encoders created by this factory
     // of the given codec will use hardware support.
-    bool is_hardware_accelerated;
+    bool is_hardware_accelerated = false;
     // |has_internal_source| is true if encoders created by this factory of the
     // given codec will use internal camera sources, meaning that they don't
     // require/expect frames to be delivered via webrtc::VideoEncoder::Encode.
     // This flag is used as the internal_source parameter to
     // webrtc::ViEExternalCodec::RegisterExternalSendCodec.
-    bool has_internal_source;
+    bool has_internal_source = false;
   };
 
   // An injectable class that is continuously updated with encoding conditions
