e6542f21127d5d1fd365b5d823deb95978d904ab
philipel <philipel@webrtc.org>
2020-07-17 15:19:40 +0200

    Removed unused include from encoded_image.h.
    Bug: webrtc:9378
    Change-Id: Ie26ab4d30d62ec109a8be638661789399821c162
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/179525
    Reviewed-by: Niels Moller <nisse@webrtc.org>
    Commit-Queue: Philip Eliasson <philipel@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31758}

--- api/video/encoded_image.h
+++ api/video/encoded_image.h
@@ -21,7 +21,6 @@
 #include "api/scoped_refptr.h"
 #include "api/video/color_space.h"
 #include "api/video/video_codec_constants.h"
-#include "api/video/video_codec_type.h"
 #include "api/video/video_content_type.h"
 #include "api/video/video_frame_type.h"
 #include "api/video/video_rotation.h"
