9d9d10c7ed964cfcfc8abe74059571c7166e70cc
Sami Kalliomäki <sakal@webrtc.org>
2020-08-14 13:46:04 +0200

    Log a warning in STHelper if a frame is already pending.
    Bug: b/163785724
    Change-Id: Ic2cf1e13f5ccc8115b3132b21c60577f9fb8994f
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/181660
    Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
    Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31934}

--- sdk/android/api/org/webrtc/SurfaceTextureHelper.java
+++ sdk/android/api/org/webrtc/SurfaceTextureHelper.java
@@ -199,6 +199,10 @@ public class SurfaceTextureHelper {
     oesTextureId = GlUtil.generateTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES);
     surfaceTexture = new SurfaceTexture(oesTextureId);
     setOnFrameAvailableListener(surfaceTexture, (SurfaceTexture st) -> {
+      if (hasPendingTexture) {
+        Logging.d(TAG, "A frame is already pending, dropping frame.");
+      }
+
       hasPendingTexture = true;
       tryDeliverTextureFrame();
     }, handler);
