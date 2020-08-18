876a3dc88ac44768cab57dd6f23c8758bfb6b5bd
Ivo Creusen <ivoc@webrtc.org>
2020-08-18 17:08:18 +0200

    Fix for NetEq simulations containing large gaps and multiple SSRCs.
    This CL fixes 2 issues that affect NetEq simulations.
    - When using event logs with multiple SSRCs, it does not make sense to
      use more than a single SSRC. If the user does not provide an SSRC
      filter, we should use the first SSRC we find and no others.
    - It is possible for event logs to have a gap in the middle, and
      sometimes we don't store/mark the gap properly. If is possible to
      detect gaps by looking at the wallclock time delta between getAudio
      events. These should be 10 ms nominally, so values greater than 1000
      should never happen and indicate an error.
    
    Bug: webrtc:11855
    Change-Id: Idc3b8a7902be4159da48b063ef5c5c82fd484071
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/181940
    Reviewed-by: Jakob Ivarsson <jakobi@webrtc.org>
    Commit-Queue: Ivo Creusen <ivoc@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#31960}

--- modules/audio_coding/neteq/tools/neteq_test.cc
+++ modules/audio_coding/neteq/tools/neteq_test.cc
@@ -267,8 +267,12 @@ NetEqTest::SimulationStepResult NetEqTest::RunToNextGetAudio() {
       prev_lifetime_stats_ = lifetime_stats;
       const bool no_more_packets_to_decode =
           !input_->NextPacketTime() && !operations_state.next_packet_available;
-      result.is_simulation_finished =
-          no_more_packets_to_decode || input_->ended();
+      // End the simulation if the gap is too large. This indicates an issue
+      // with the event log file.
+      const bool simulation_step_too_large = result.simulation_step_ms > 1000;
+      result.is_simulation_finished = simulation_step_too_large ||
+                                      no_more_packets_to_decode ||
+                                      input_->ended();
       prev_ops_state_ = operations_state;
       return result;
     }
