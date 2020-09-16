818be15e4d11e0e8a249b64d21cc5f5a41239c72
Mirko Bonadei <mbonadei@webrtc.org>
2020-09-16 13:05:07 +0200

    Clobber iOS builders.
    TBR=titovartem@webrtc.org
    
    Bug: webrtc:11937
    Change-Id: Ic3ad56df532fac0ccf254e3e98d003ae0fefbf3c
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/184342
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#32115}

--- tools_webrtc/get_landmines.py
+++ tools_webrtc/get_landmines.py
@@ -56,6 +56,7 @@ def print_landmines():  # pylint: disable=invalid-name
     print 'Another landmine for low_bandwidth_audio_test (webrtc:7430)'
     print 'Clobber to change neteq_rtpplay type to executable'
     print 'Clobber to remove .xctest files.'
+    print 'Clobber to remove .xctest files (take 2).'
 
 
 def main():
