8408c9938cb543a7cd5e7127927f3b4d0834ad59
Tomas Gunnarsson <tommi@webrtc.org>
2021-02-14 14:19:12 +0100

    Remove 'secondary sink' concept from webrtc::VideoReceiveStream.
    In practice, support for multiple sinks is not needed and supporting
    the API that allows for dynamically adding/removing sinks at runtime,
    adds to the complexity of the implementation.
    
    This CL removes that Add/Remove methods for secondary sinks as well
    as vectors of callback pointers (which were either of size 0 or 1).
    Instead, an optional callback pointer is added to the config struct
    for VideoReceiveStream, that an implementation can consider to be
    const and there's not a need to do thread synchronization for that
    pointer for every network packet.
    
    As part of webrtc:11993, this simplifies the work towards keeping
    the processing of network packets on the network thread. The secondary
    sinks, currently operate on the worker thread.
    
    Bug: webrtc:11993
    Change-Id: I10c473e57d3809527a1b689f4352e903a4c78168
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/207421
    Commit-Queue: Tommi <tommi@webrtc.org>
    Reviewed-by: Rasmus Brandt <brandtr@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#33272}

--- video/rtp_video_stream_receiver2.h
+++ video/rtp_video_stream_receiver2.h
@@ -175,13 +175,6 @@ class RtpVideoStreamReceiver2 : public LossNotificationSender,
   absl::optional<int64_t> LastReceivedPacketMs() const;
   absl::optional<int64_t> LastReceivedKeyframePacketMs() const;
 
-  // RtpDemuxer only forwards a given RTP packet to one sink. However, some
-  // sinks, such as FlexFEC, might wish to be informed of all of the packets
-  // a given sink receives (or any set of sinks). They may do so by registering
-  // themselves as secondary sinks.
-  void AddSecondarySink(RtpPacketSinkInterface* sink);
-  void RemoveSecondarySink(const RtpPacketSinkInterface* sink);
-
  private:
   // Implements RtpVideoFrameReceiver.
   void ManageFrame(
@@ -338,9 +331,6 @@ class RtpVideoStreamReceiver2 : public LossNotificationSender,
   int16_t last_payload_type_ RTC_GUARDED_BY(worker_task_checker_) = -1;
 
   bool has_received_frame_ RTC_GUARDED_BY(worker_task_checker_);
-
-  std::vector<RtpPacketSinkInterface*> secondary_sinks_
-      RTC_GUARDED_BY(worker_task_checker_);
 
   absl::optional<uint32_t> last_received_rtp_timestamp_
       RTC_GUARDED_BY(worker_task_checker_);
