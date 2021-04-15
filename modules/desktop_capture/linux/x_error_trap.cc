0f506780aa844e8fc3db5aaf29db73f86dbb648b
Byoungchan Lee <daniel.l@hpcnt.com>
2021-04-16 07:03:36 +0900

    Remove usage of TOOLKIT_GTK define.
    This is not defined anywhere, including chromium.
    
    Bug: None
    Change-Id: If5e89880570a80dd5720e48ebaefb0eb2c37fab3
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/215360
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#34105}

--- modules/desktop_capture/linux/x_error_trap.cc
+++ modules/desktop_capture/linux/x_error_trap.cc
@@ -13,15 +13,9 @@
 #include <assert.h>
 #include <stddef.h>
 
-#if defined(TOOLKIT_GTK)
-#include <gdk/gdk.h>
-#endif  // !defined(TOOLKIT_GTK)
-
 namespace webrtc {
 
 namespace {
-
-#if !defined(TOOLKIT_GTK)
 
 // TODO(sergeyu): This code is not thread safe. Fix it. Bug 2202.
 static bool g_xserver_error_trap_enabled = false;
@@ -33,32 +27,22 @@ int XServerErrorHandler(Display* display, XErrorEvent* error_event) {
   return 0;
 }
 
-#endif  // !defined(TOOLKIT_GTK)
-
 }  // namespace
 
 XErrorTrap::XErrorTrap(Display* display)
     : original_error_handler_(NULL), enabled_(true) {
-#if defined(TOOLKIT_GTK)
-  gdk_error_trap_push();
-#else   // !defined(TOOLKIT_GTK)
   assert(!g_xserver_error_trap_enabled);
   original_error_handler_ = XSetErrorHandler(&XServerErrorHandler);
   g_xserver_error_trap_enabled = true;
   g_last_xserver_error_code = 0;
-#endif  // !defined(TOOLKIT_GTK)
 }
 
 int XErrorTrap::GetLastErrorAndDisable() {
   enabled_ = false;
-#if defined(TOOLKIT_GTK)
-  return gdk_error_trap_push();
-#else   // !defined(TOOLKIT_GTK)
   assert(g_xserver_error_trap_enabled);
   XSetErrorHandler(original_error_handler_);
   g_xserver_error_trap_enabled = false;
   return g_last_xserver_error_code;
-#endif  // !defined(TOOLKIT_GTK)
 }
 
 XErrorTrap::~XErrorTrap() {
