63b3095d2ba163eb189c8af798d89f9a75dfad4f
Minyue Li <minyue@webrtc.org>
2021-05-19 14:38:25 +0200

    Make local to capturer clock offset a separate entry in PacketInfo.
    This also changes the meaning of |estimated_capture_clock_offset| in
    |absolute_capture_time_| to become a remote to capturer clock offset.
    
    Bug: chromium:1056230, webrtc:10739
    Change-Id: Id658590e027bbe77ae0834ea224e1dc977a305f2
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219163
    Commit-Queue: Minyue Li <minyue@webrtc.org>
    Reviewed-by: Åsa Persson <asapersson@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Reviewed-by: Alessio Bazzica <alessiob@webrtc.org>
    Reviewed-by: Christoffer Rodbro <crodbro@webrtc.org>
    Reviewed-by: Gustaf Ullberg <gustaf@webrtc.org>
    Reviewed-by: Chen Xing <chxg@google.com>
    Cr-Commit-Position: refs/heads/master@{#34067}

--- video/rtp_video_stream_receiver2.cc
+++ video/rtp_video_stream_receiver2.cc
@@ -253,7 +253,7 @@ RtpVideoStreamReceiver2::RtpVideoStreamReceiver2(
       packet_buffer_(kPacketBufferStartSize, PacketBufferMaxSize()),
       has_received_frame_(false),
       frames_decryptable_(false),
-      absolute_capture_time_receiver_(clock) {
+      absolute_capture_time_interpolator_(clock) {
   constexpr bool remb_candidate = true;
   if (packet_router_)
     packet_router_->AddReceiveRtpModule(rtp_rtcp_.get(), remb_candidate);
@@ -492,9 +492,9 @@ void RtpVideoStreamReceiver2::OnReceivedPayloadData(
 
   // Try to extrapolate absolute capture time if it is missing.
   packet_info.set_absolute_capture_time(
-      absolute_capture_time_receiver_.OnReceivePacket(
-          AbsoluteCaptureTimeReceiver::GetSource(packet_info.ssrc(),
-                                                 packet_info.csrcs()),
+      absolute_capture_time_interpolator_.OnReceivePacket(
+          AbsoluteCaptureTimeInterpolator::GetSource(packet_info.ssrc(),
+                                                     packet_info.csrcs()),
           packet_info.rtp_timestamp(),
           // Assume frequency is the same one for all video frames.
           kVideoPayloadTypeFrequency, packet_info.absolute_capture_time()));
@@ -1036,7 +1036,7 @@ bool RtpVideoStreamReceiver2::DeliverRtcp(const uint8_t* rtcp_packet,
     absl::optional<int64_t> remote_to_local_clock_offset_ms =
         ntp_estimator_.EstimateRemoteToLocalClockOffsetMs();
     if (remote_to_local_clock_offset_ms.has_value()) {
-      absolute_capture_time_receiver_.SetRemoteToLocalClockOffset(
+      capture_clock_offset_updater_.SetRemoteToLocalClockOffset(
           Int64MsToQ32x32(*remote_to_local_clock_offset_ms));
     }
   }
