504fc192d09c51c21777eeefe883827b9f6ed8ab
Vojin Ilic <vilic@nvidia.com>
2021-05-31 14:02:28 +0200

    Add ability to pass factory for RtpTransportControllerSend to PeerConnectionFactoryDependencies.
    This way we can have custom implementation of RtpTransportControllerSendInterface and pass it properly to Call.
    Call relies on RtpTransportControllerSendInterface already so this is natural way to customize RTP related classes.
    
    If there is custom factory present in dependencies it will be used, otherwise default factory will be used.
    
    Intention behind this change is to have ability to have custom QoS with custom parameters.
    
    Bug: webrtc:12778
    Change-Id: I5b88957025621ef4bcd63eaa98c218ad213da9c8
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/217769
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Niels Moller <nisse@webrtc.org>
    Commit-Queue: Philipp Hancke <phancke@nvidia.com>
    Cr-Commit-Position: refs/heads/master@{#34181}

--- call/call.h
+++ call/call.h
@@ -84,11 +84,14 @@ class Call {
 
   static Call* Create(const Call::Config& config);
   static Call* Create(const Call::Config& config,
-                      rtc::scoped_refptr<SharedModuleThread> call_thread);
-  static Call* Create(const Call::Config& config,
                       Clock* clock,
                       rtc::scoped_refptr<SharedModuleThread> call_thread,
                       std::unique_ptr<ProcessThread> pacer_thread);
+  static Call* Create(const Call::Config& config,
+                      Clock* clock,
+                      rtc::scoped_refptr<SharedModuleThread> call_thread,
+                      std::unique_ptr<RtpTransportControllerSendInterface>
+                          transportControllerSend);
 
   virtual AudioSendStream* CreateAudioSendStream(
       const AudioSendStream::Config& config) = 0;
