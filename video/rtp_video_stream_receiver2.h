06a2bf09a462a514ba1a19faac617a5a15d034bb
Markus Handell <handellm@webrtc.org>
2021-07-22 15:09:39 +0200

    NackModule2: Rename to NackRequester.
    The alternative new name proposed, NackTracker, is already in
    use in audio_coding.
    
    Fixed: webrtc:11594
    Change-Id: I6a05fafc05fa7ddb18ea4f64886a135e5ef59f7e
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/226744
    Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Markus Handell <handellm@webrtc.org>
    Cr-Commit-Position: refs/heads/master@{#34539}

--- video/rtp_video_stream_receiver2.h
+++ video/rtp_video_stream_receiver2.h
@@ -39,7 +39,7 @@
 #include "modules/rtp_rtcp/source/video_rtp_depacketizer.h"
 #include "modules/video_coding/h264_sps_pps_tracker.h"
 #include "modules/video_coding/loss_notification_controller.h"
-#include "modules/video_coding/nack_module2.h"
+#include "modules/video_coding/nack_requester.h"
 #include "modules/video_coding/packet_buffer.h"
 #include "modules/video_coding/rtp_frame_reference_finder.h"
 #include "modules/video_coding/unique_timestamp_counter.h"
@@ -53,7 +53,7 @@
 
 namespace webrtc {
 
-class NackModule2;
+class NackRequester;
 class PacketRouter;
 class ReceiveStatistics;
 class RtcpRttStats;
@@ -316,7 +316,7 @@ class RtpVideoStreamReceiver2 : public LossNotificationSender,
   KeyFrameRequestSender* const keyframe_request_sender_;
 
   RtcpFeedbackBuffer rtcp_feedback_buffer_;
-  const std::unique_ptr<NackModule2> nack_module_;
+  const std::unique_ptr<NackRequester> nack_module_;
   std::unique_ptr<LossNotificationController> loss_notification_controller_;
 
   video_coding::PacketBuffer packet_buffer_
