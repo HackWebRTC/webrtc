807328fec755723eedb01f3ee3fb5b12ff83522a
Niels Möller <nisse@webrtc.org>
2022-05-12 16:16:39 +0200

    Move frame drop config to VideoCodec and VideoEncoderConfig.
    Intend to delete corresponding codec-specific settings in a followup.
    
    Bug: webrtc:6883
    Change-Id: I78ab07729a5aee1055f80d39d8f7289beb6721e1
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/262244
    Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
    Commit-Queue: Niels Moller <nisse@webrtc.org>
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#36882}

--- api/video_codecs/video_codec.h
+++ api/video_codecs/video_codec.h
@@ -116,6 +116,9 @@ class RTC_EXPORT VideoCodec {
   VideoCodecComplexity GetVideoEncoderComplexity() const;
   void SetVideoEncoderComplexity(VideoCodecComplexity complexity_setting);
 
+  bool GetFrameDropEnabled() const;
+  void SetFrameDropEnabled(bool enabled);
+
   // Public variables. TODO(hta): Make them private with accessors.
   VideoCodecType codecType;
 
@@ -179,6 +182,9 @@ class RTC_EXPORT VideoCodec {
   // 'complexity_' indicates the CPU capability of the client. It's used to
   // determine encoder CPU complexity (e.g., cpu_used for VP8, VP9. and AV1).
   absl::optional<VideoCodecComplexity> complexity_;
+  // TODO(bugs.webrtc.org/6883): When unset, GetEnableFrameDrop checks the
+  // codec-specific settings.
+  absl::optional<bool> frame_drop_enabled_;
 };
 
 }  // namespace webrtc
