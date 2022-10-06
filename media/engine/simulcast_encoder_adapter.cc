4424f4bb8eb5614acb06b13d503fc5639545da5a
Åsa Persson <asapersson@webrtc.org>
2022-10-06 16:50:00 +0000

    Revert "Only set apply_alignment_to_all_simulcast_layers if set by any encoder."
    This reverts commit 379a0b49d2bcfbabf62a9d1c5a7db0f7e46e8798.
    
    Reason for revert: Waiting with this change.
    
    Original change's description:
    > Only set apply_alignment_to_all_simulcast_layers if set by any encoder.
    >
    > Bug: none
    > Change-Id: I16448b71c54536d88e6e5326586219f833daf174
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/278044
    > Commit-Queue: Åsa Persson <asapersson@webrtc.org>
    > Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    > Cr-Commit-Position: refs/heads/main@{#38305}
    
    Bug: none
    Change-Id: I84a88655cc073da92aef5fd0bc2c46d31d735911
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/278280
    Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#38311}

--- media/engine/simulcast_encoder_adapter.cc
+++ media/engine/simulcast_encoder_adapter.cc
@@ -928,7 +928,12 @@ VideoEncoder::EncoderInfo SimulcastEncoderAdapter::GetEncoderInfo() const {
     encoder_info.requested_resolution_alignment = cricket::LeastCommonMultiple(
         encoder_info.requested_resolution_alignment,
         encoder_impl_info.requested_resolution_alignment);
-    if (encoder_impl_info.apply_alignment_to_all_simulcast_layers) {
+    // request alignment on all layers if any of the encoders may need it, or
+    // if any non-top layer encoder requests a non-trivial alignment.
+    if (encoder_impl_info.apply_alignment_to_all_simulcast_layers ||
+        (encoder_impl_info.requested_resolution_alignment > 1 &&
+         (codec_.simulcastStream[i].height < codec_.height ||
+          codec_.simulcastStream[i].width < codec_.width))) {
       encoder_info.apply_alignment_to_all_simulcast_layers = true;
     }
   }
