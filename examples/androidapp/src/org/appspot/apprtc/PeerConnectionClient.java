74af9d1fc727e7d5b35d89bc48948c3c92c34c19
Piasy <xz4215@gmail.com>
2023-06-01 22:35:31 +0800

    demo add H265 option

--- examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
+++ examples/androidapp/src/org/appspot/apprtc/PeerConnectionClient.java
@@ -96,6 +96,7 @@ public class PeerConnectionClient {
   private static final String VIDEO_CODEC_H264 = "H264";
   private static final String VIDEO_CODEC_H264_BASELINE = "H264 Baseline";
   private static final String VIDEO_CODEC_H264_HIGH = "H264 High";
+  private static final String VIDEO_CODEC_H265 = "H265";
   private static final String VIDEO_CODEC_AV1 = "AV1";
   private static final String AUDIO_CODEC_OPUS = "opus";
   private static final String AUDIO_CODEC_ISAC = "ISAC";
@@ -991,6 +992,8 @@ public class PeerConnectionClient {
       case VIDEO_CODEC_H264_HIGH:
       case VIDEO_CODEC_H264_BASELINE:
         return VIDEO_CODEC_H264;
+      case VIDEO_CODEC_H265:
+        return VIDEO_CODEC_H265;
       default:
         return VIDEO_CODEC_VP8;
     }
